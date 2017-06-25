3336f167c2b28442617336e30cb7e1dad9105d02
Francesco Cogno <francesco.cogno@gmail.com>
2017-06-25 16:31:25 +0200

    First example completed

--- examples/collection.rs
+++ examples/collection.rs
@@ -9,6 +9,7 @@ extern crate hyper_tls;
 use std::error::Error;
 
 use futures::future::*;
+use futures::Stream;
 use tokio_core::reactor::Core;
 
 use azure_sdk_for_rust::azure::cosmos::authorization_token::{AuthorizationToken, TokenType};
@@ -30,21 +31,21 @@ fn code() -> Result<(), Box<Error>> {
     let account = std::env::var("COSMOS_ACCOUNT").expect("Set env variable COSMOS_ACCOUNT first!");
 
     // let's create a tokio-core reactor.
-    // It will drive our request. Remember, util run, futures do absolutely
+    // It will drive our request. Remember, until run, futures do absolutely
     // nothing. So, run them. Also note that, in order to avoid cloning the authorization_token at
     // each request this library constructs the request **before** the future. This means the date
     // sent to the server will be the one at Future creation time, not the execution time.
     // Azure calls will block requests with time too much in the past (in order to prevent reply
-    // attacks) so make sure to execute the Future as soon as possibile after having it created.
+    // attacks) so make sure to execute the Future as soon as possible after having it created.
     // * This is something worth discussing *
     let mut core = Core::new()?;
 
 
     // This is how you construct an authorization token.
-    // Remeber to pick the correct token type.
+    // Remember to pick the correct token type.
     // Here we assume master.
     // Most methods return a ```Result<_, AzureError>```.
-    // ```AzureError``` is an enum union of all the possibile undelying
+    // ```AzureError``` is an enum union of all the possible underlying
     // errors, plus Azure specific ones. For example if a REST call returns the
     // unexpected result (ie NotFound instead of Ok) we return a Err telling
     // you that.
@@ -61,35 +62,29 @@ fn code() -> Result<(), Box<Error>> {
     let client = Client::new(&core.handle(), authorization_token)?;
 
     // The Cosmos' client exposes a lot of methods. This one lists the databases in the specified
-    // account. Database do not implement Display but defef to &str so you can pass it to methods
+    // account. Database do not implement Display but deref to &str so you can pass it to methods
     // both as struct or id.
     println!("before client.list_databases");
-    let future = client
-        .list_databases()
-        .and_then(move |databases| {
-            println!("Account {} has {} databases", account, databases.len());
-            ok(databases)
-        })
-        .for_each(|db| println!("ddd"))
-        .map(move |collections| {
-            println!("Database xxx has {} collections", collections.len());
-        });
-    println!("after client.list_databases");
+    let future = client.list_databases().and_then(move |databases| {
+        println!("Account {} has {} database(s)", account, databases.len());
 
-    println!("before core.run");
+        let mut v = Vec::new();
+
+        // Each Cosmos' database contains so or more collections. We can enumerate them using the
+        // list_collection method.
+        for db in databases {
+            v.push(client.list_collections(&db).map(move |collections| {
+                println!("database {} has {} collection(s)", db.id, collections.len());
+
+                for collection in collections {
+                    println!("\tcollection {}", collection.id);
+                }
+            }));
+        }
+
+        futures::future::join_all(v)
+    });
+
     core.run(future)?;
-    println!("after core.run");
-
-    //// Each Cosmos' database contains zo or more collections. We can enumerate them using the
-    //// list_collection method.
-    //for db in &databases {
-    //    let collections = client.list_clections(db)?;
-    //    println!("*** {} *** ({} collections)", db as &str, collections.len());
-    //    for coll in &collections {
-    //        // Collection does not implement Display but Deref to &str so this print works as
-    //        // expected.
-    //        println!("\t{}", coll as &str);
-    //    }
-    //}
     Ok(())
 }
