4bd63b980a9e3e690f851caa7b4ba71046882b95
Francesco Cogno <francesco.cogno@outlook.com>
2019-08-11 15:52:17 +0200

    added publish script

---
+++ scripts/publish_all.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/env bash
+
+cd azure_sdk_core
+cargo publish
+cd ..
+
+cd azure_sdk_storage_core
+cargo publish
+cd ..
+
+cd azure_sdk_storage_account
+cargo publish
+cd ..
+
+cd azure_sdk_storage_blob
+cargo publish
+cd ..
+
+cd azure_sdk_storage_table
+cargo publish
+cd ..
+
+cd azure_sdk_auth_aad
+cargo publish
+cd ..
+
+cd azure_sdk_cosmos
+cargo publish
+cd ..
+
+cd azure_sdk_service_bus
+cargo publish
+cd ..
