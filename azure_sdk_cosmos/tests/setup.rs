8bc99ceade9c909e92270a5d269a63b14401a576
Francesco Cogno <francesco.cogno@gmail.com>
2020-04-19 16:52:59 +0200

    release core_0.43.0

--- azure_sdk_cosmos/tests/setup.rs
+++ azure_sdk_cosmos/tests/setup.rs
@@ -4,7 +4,8 @@ use azure_sdk_cosmos::AuthorizationToken;
 
 pub fn initialize() -> Result<Client<impl CosmosUriBuilder + Clone>, AzureError> {
     let account = std::env::var("COSMOS_ACCOUNT").expect("Set env variable COSMOS_ACCOUNT first!");
-    let key = std::env::var("COSMOS_MASTER_KEY").expect("Set env variable COSMOS_KEY first!");
+    let key =
+        std::env::var("COSMOS_MASTER_KEY").expect("Set env variable COSMOS_MASTER_KEY first!");
 
     let authorization_token = AuthorizationToken::new_master(&key)?;
     let client = ClientBuilder::new(account, authorization_token)?;
