c2650b4a05319bc10061de37ae3f484630b7267b
Cameron Taggart <cameron.taggart@gmail.com>
2020-10-08 07:13:30 -0600

    headers now generated

--- rest/azure_mgmt_storage_v2019_06_01/src/lib.rs
+++ rest/azure_mgmt_storage_v2019_06_01/src/lib.rs
@@ -5,6 +5,15 @@ pub use self::{models::*, client::*};
 
 // these are all TODO
 pub type UnknownType = serde_json::Value;
+pub type SkuName = serde_json::Value;
+pub type Tier = serde_json::Value;
+pub type SKUCapability = serde_json::Value;
+pub type Sku = serde_json::Value;
+pub type IPRule = serde_json::Value;
+// in client
+pub type PrivateLinkResourceListResult = serde_json::Value;
+pub type PrivateEndpointConnection = serde_json::Value;
+pub type PrivateEndpointConnectionListResult = serde_json::Value;
 
 pub struct Configuration {
     pub base_path: String,
