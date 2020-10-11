73bd8c4f38a17d075447cb9105c2023eea7e46ba
Cameron Taggart <cameron.taggart@gmail.com>
2020-10-10 21:46:24 -0600

    default Configuration

--- rest/mgmt_storage/2019-06-01/src/lib.rs
+++ rest/mgmt_storage/2019-06-01/src/lib.rs
@@ -4,7 +4,24 @@ mod models;
 pub use self::{client::*, models::*};
 
 pub struct Configuration {
-    pub base_path: String,
+    pub api_version: String,
     pub client: reqwest::Client,
+    pub base_path: String,
     pub bearer_access_token: Option<String>,
+}
+
+impl Default for Configuration {
+    fn default() -> Self {
+        {
+            let api_version = "2019-06-01".to_owned();
+            let client = reqwest::Client::new();
+            let base_path = "https://management.azure.com".to_owned();
+            Self {
+                api_version,
+                client,
+                base_path,
+                bearer_access_token: None,
+            }
+        }
+    }
 }
