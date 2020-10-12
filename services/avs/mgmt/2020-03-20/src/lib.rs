d3bebaadfa71875404c1a101c93373f64e212a2d
Cameron Taggart <cameron.taggart@gmail.com>
2020-10-12 14:59:23 -0600

    add avs_mgmt

---
+++ services/avs/mgmt/2020-03-20/src/lib.rs
@@ -0,0 +1,29 @@
+mod models;
+mod operations;
+
+pub use self::{models::*, operations::*};
+
+pub const API_VERSION: &str = "2020-03-20";
+
+pub type Error = Box<dyn std::error::Error + Send + Sync>;
+pub type Result<T> = std::result::Result<T, Error>;
+
+pub struct Configuration {
+    pub api_version: String,
+    pub client: reqwest::Client,
+    pub base_path: String,
+    pub bearer_access_token: Option<String>,
+}
+
+impl Default for Configuration {
+    fn default() -> Self {
+        {
+            Self {
+                api_version: API_VERSION.to_owned(),
+                client: reqwest::Client::new(),
+                base_path: "https://management.azure.com".to_owned(),
+                bearer_access_token: None,
+            }
+        }
+    }
+}
