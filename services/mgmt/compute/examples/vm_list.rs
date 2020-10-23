5664edf013fdcfe7a39026a8a73bbf1486b9c058
Cameron Taggart <cameron.taggart@gmail.com>
2020-10-22 18:24:00 -0600

    update examples

--- services/mgmt/compute/examples/vm_list.rs
+++ services/mgmt/compute/examples/vm_list.rs
@@ -2,32 +2,23 @@
 Lists the virtual , similar to:
 az vm list --query [].id
 
-export SUBSCRIPTION_ID=$(az account show --query id --output tsv)
-export ACCESS_TOKEN=$(az account get-access-token --query accessToken --output tsv)
 cargo run --example vm_list
 */
 
+use azure_identity::token_credentials::AzureCliCredential;
 use azure_mgmt_compute::operations::virtual_machines;
 
 type Result<T> = std::result::Result<T, Box<dyn std::error::Error>>;
 
 #[tokio::main]
 async fn main() -> Result<()> {
-    let subscription_id = &get_subscription_id()?;
-    let access_token = &get_access_token()?;
-    let config = &azure_mgmt_compute::OperationConfig::new(access_token);
+    let token_credential = AzureCliCredential {};
+    let subscription_id = &AzureCliCredential::get_subscription()?;
+    let config = &azure_mgmt_compute::OperationConfig::new(Box::new(token_credential));
     let vms = virtual_machines::list_all(config, subscription_id, None).await?;
     println!("# of virtual machines {}", vms.value.len());
     for vm in &vms.value {
         println!("{:?}", &vm.resource.id);
     }
     Ok(())
-}
-
-fn get_subscription_id() -> Result<String> {
-    Ok(std::env::var("SUBSCRIPTION_ID").map_err(|_| "SUBSCRIPTION_ID required")?)
-}
-
-fn get_access_token() -> Result<String> {
-    Ok(std::env::var("ACCESS_TOKEN").map_err(|_| "ACCESS_TOKEN required")?)
 }
