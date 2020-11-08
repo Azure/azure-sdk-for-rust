7419b8f85d645dd422ea690437c3db922bd80261
Jeffrey Shen <jeffrey.shen@displayr.com>
2020-11-08 15:38:51 +1100

    Fix invalid resource uri in REST call
    thread 'main' panicked at 'called `Result::unwrap()` on an `Err` value: Azure("AuthorizationError(Failed to authenticate to Azure Active Directory
    Caused by:
       Generic error: AADSTS70011: The provided request must include a \'scope\' input parameter. The provided value for the input parameter \'scope\' is not valid. 
       The scope https://vault.azure.net.default is not valid

--- sdk/key_vault/src/client.rs
+++ sdk/key_vault/src/client.rs
@@ -75,7 +75,7 @@ impl<'a, T: TokenCredential> KeyVaultClient<'a, T> {
             // Token is valid, return it.
             return Ok(());
         }
-        let resource = format!("https://{}", &self.endpoint_suffix);
+        let resource = format!("https://{}/", &self.endpoint_suffix);
         let token = self
             .token_credential
             .get_token(&resource)
