ad84c56f8e05473dd487ade8469c1da1dcb27d71
Alexei Barnes <alexei.barnes@aveva.com>
2020-11-22 19:20:35 +0000

    Implemented Queue Peek Messages
    Fixed Get and Peek Messages on Empty Queue

---
+++ sdk/storage/examples/peek_messages.rs
@@ -0,0 +1,34 @@
+#[macro_use]
+extern crate log;
+use azure_storage::core::prelude::*;
+use azure_storage::queue::prelude::*;
+use std::error::Error;
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn Error>> {
+    // First we retrieve the account name and master key from environment variables.
+    let account =
+        std::env::var("STORAGE_ACCOUNT").expect("Set env variable STORAGE_ACCOUNT first!");
+    let master_key =
+        std::env::var("STORAGE_MASTER_KEY").expect("Set env variable STORAGE_MASTER_KEY first!");
+
+    let queue_name = std::env::args()
+        .nth(1)
+        .expect("Please pass the queue name as first parameter");
+
+    let queue = client::with_access_key(&account, &master_key)
+        .into_queue_service_client()
+        .into_queue_name_client(&queue_name);
+
+    trace!("peeking messages");
+
+    let response = queue
+        .peek_messages()
+        .with_number_of_messages(2)
+        .execute()
+        .await?;
+
+    println!("response == {:#?}", response);
+
+    Ok(())
+}
--- sdk/storage/src/queue/responses/peek_messages_response.rs
+++ sdk/storage/src/queue/responses/peek_messages_response.rs
@@ -5,47 +5,41 @@ use hyper::header::HeaderMap;
 use std::convert::TryInto;
 
 #[derive(Debug, Clone)]
-pub struct GetMessagesResponse {
+pub struct PeekMessagesResponse {
     pub common_storage_response_headers: CommonStorageResponseHeaders,
-    pub messages: Vec<Message>,
-}
-
-#[derive(Debug, Clone)]
-pub struct Message {
-    pub message_id: String,
-    pub insertion_time: DateTime<Utc>,
-    pub expiration_time: DateTime<Utc>,
-    pub pop_receipt: String,
-    pub time_next_visible: DateTime<Utc>,
-    pub dequeue_count: u64,
-    pub message_text: String,
+    pub messages: Vec<PeekMessage>,
 }
 
 #[derive(Debug, Clone, Serialize, Deserialize)]
-struct MessageInternal {
+struct PeekMessageInternal {
     #[serde(rename = "MessageId")]
     pub message_id: String,
     #[serde(rename = "InsertionTime")]
     pub insertion_time: String,
     #[serde(rename = "ExpirationTime")]
     pub expiration_time: String,
-    #[serde(rename = "PopReceipt")]
-    pub pop_receipt: String,
-    #[serde(rename = "TimeNextVisible")]
-    pub time_next_visible: String,
     #[serde(rename = "DequeueCount")]
     pub dequeue_count: u64,
     #[serde(rename = "MessageText")]
     pub message_text: String,
 }
 
-#[derive(Debug, Clone, Serialize, Deserialize)]
-struct MessagesInternal {
-    #[serde(rename = "QueueMessage")]
-    pub messages: Option<Vec<MessageInternal>>,
+#[derive(Debug, Clone)]
+pub struct PeekMessage {
+    pub message_id: String,
+    pub insertion_time: DateTime<Utc>,
+    pub expiration_time: DateTime<Utc>,
+    pub dequeue_count: u64,
+    pub message_text: String,
 }
 
-impl std::convert::TryFrom<(&HeaderMap, &[u8])> for GetMessagesResponse {
+#[derive(Debug, Clone, Serialize, Deserialize)]
+struct PeekMessagesInternal {
+    #[serde(rename = "QueueMessage")]
+    pub messages: Option<Vec<PeekMessageInternal>>,
+}
+
+impl std::convert::TryFrom<(&HeaderMap, &[u8])> for PeekMessagesResponse {
     type Error = AzureError;
     fn try_from(value: (&HeaderMap, &[u8])) -> Result<Self, Self::Error> {
         let headers = value.0;
@@ -55,23 +49,21 @@ impl std::convert::TryFrom<(&HeaderMap, &[u8])> for GetMessagesResponse {
 
         let received = &std::str::from_utf8(body)?[3..];
         debug!("receieved == {:#?}", received);
-        let response: MessagesInternal = serde_xml_rs::from_reader(&body[3..])?;
+        let response: PeekMessagesInternal = serde_xml_rs::from_reader(&body[3..])?;
         debug!("response == {:?}", response);
 
         let mut messages = Vec::new();
         for message in response.messages.unwrap_or(Vec::new()).into_iter() {
-            messages.push(Message {
+            messages.push(PeekMessage {
                 message_id: message.message_id,
                 insertion_time: utc_date_from_rfc2822(&message.insertion_time)?,
                 expiration_time: utc_date_from_rfc2822(&message.expiration_time)?,
-                pop_receipt: message.pop_receipt,
-                time_next_visible: utc_date_from_rfc2822(&message.time_next_visible)?,
                 dequeue_count: message.dequeue_count,
                 message_text: message.message_text,
             })
         }
 
-        Ok(GetMessagesResponse {
+        Ok(PeekMessagesResponse {
             common_storage_response_headers: headers.try_into()?,
             messages,
         })
