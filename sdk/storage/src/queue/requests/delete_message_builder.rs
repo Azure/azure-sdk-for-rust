0883860b96f31829a090221b7930bb6a3bfe2ca2
Francesco Cogno <francesco.cogno@gmail.com>
2020-11-23 09:34:42 +0100

    merged from master

--- sdk/storage/src/queue/requests/delete_message_builder.rs
+++ sdk/storage/src/queue/requests/delete_message_builder.rs
@@ -163,7 +163,7 @@ where
 
         debug!("uri == {}", uri);
 
-        let future_response = self.queue_name_service.storage_client().perform_request(
+        let (_, future_response) = self.queue_name_service.storage_client().perform_request(
             &uri,
             &http::Method::DELETE,
             &|mut request| {
