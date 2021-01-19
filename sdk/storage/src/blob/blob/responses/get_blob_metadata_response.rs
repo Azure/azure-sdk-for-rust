4132e40fb182812d5f8d14b07be9e9718c0e8f60
Francesco Cogno <francesco.cogno@outlook.com>
2021-01-19 12:19:08 +0100

    first attempt

---
+++ sdk/storage/src/blob/blob/responses/get_blob_metadata_response.rs
@@ -0,0 +1,51 @@
+use azure_core::errors::AzureError;
+use azure_core::headers::{
+    date_from_headers, etag_from_headers, request_id_from_headers, server_from_headers,
+};
+use azure_core::RequestId;
+use bytes::Bytes;
+use chrono::{DateTime, Utc};
+use http::HeaderMap;
+use std::collections::HashMap;
+use std::convert::TryFrom;
+
+#[derive(Debug, Clone)]
+pub struct GetBlobMetadataResponse {
+    pub request_id: RequestId,
+    pub etag: String,
+    pub server: String,
+    pub date: DateTime<Utc>,
+    pub metadata: HashMap<String, Bytes>,
+}
+
+impl TryFrom<&HeaderMap> for GetBlobMetadataResponse {
+    type Error = AzureError;
+
+    fn try_from(headers: &HeaderMap) -> Result<Self, Self::Error> {
+        println!("headers == {:#?}", headers);
+
+        let mut metadata = HashMap::new();
+        headers
+            .iter()
+            .filter(|header| header.0.as_str().starts_with("x-ms-meta-"))
+            .for_each(|header| {
+                metadata.insert(
+                    header
+                        .0
+                        .as_str()
+                        .strip_prefix("x-ms-meta-")
+                        .unwrap()
+                        .to_owned(),
+                    Bytes::from(header.1.as_bytes().to_owned()),
+                );
+            });
+
+        Ok(GetBlobMetadataResponse {
+            request_id: request_id_from_headers(headers)?,
+            etag: etag_from_headers(headers)?,
+            server: server_from_headers(headers)?.to_owned(),
+            date: date_from_headers(headers)?,
+            metadata,
+        })
+    }
+}
