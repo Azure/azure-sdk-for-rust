94883c9ff4165444597325ea47022e05dbb86094
Francesco Cogno <francesco.cogno@outlook.com>
2021-01-20 09:54:10 +0100

    simplified Metadata parsing code and implemented as From<&HeadeRMap>

--- sdk/core/src/metadata.rs
+++ sdk/core/src/metadata.rs
@@ -1,6 +1,7 @@
 use crate::AddAsHeader;
 use bytes::Bytes;
 use http::request::Builder;
+use http::HeaderMap;
 use std::collections::HashMap;
 
 #[derive(Debug, Clone)]
@@ -41,5 +42,23 @@ impl AddAsHeader for &Metadata {
         }
 
         builder
+    }
+}
+
+impl From<&HeaderMap> for Metadata {
+    fn from(header_map: &HeaderMap) -> Self {
+        let mut metadata = Metadata::new();
+        header_map
+            .iter()
+            .map(|header| (header.0.as_str(), header.1.as_bytes()))
+            .filter(|(key, _)| key.starts_with("x-ms-meta-"))
+            .for_each(|(key, value)| {
+                metadata.insert(
+                    key.strip_prefix("x-ms-meta-").unwrap().to_owned(),
+                    value.to_owned(),
+                );
+            });
+
+        metadata
     }
 }
--- sdk/storage/src/blob/blob/responses/get_blob_metadata_response.rs
+++ sdk/storage/src/blob/blob/responses/get_blob_metadata_response.rs
@@ -3,7 +3,6 @@ use azure_core::headers::{
     date_from_headers, etag_from_headers, request_id_from_headers, server_from_headers,
 };
 use azure_core::{Metadata, RequestId};
-use bytes::Bytes;
 use chrono::{DateTime, Utc};
 use http::HeaderMap;
 use std::convert::TryFrom;
@@ -23,28 +22,12 @@ impl TryFrom<&HeaderMap> for GetBlobMetadataResponse {
     fn try_from(headers: &HeaderMap) -> Result<Self, Self::Error> {
         debug!("headers == {:#?}", headers);
 
-        let mut metadata = Metadata::new();
-        headers
-            .iter()
-            .filter(|header| header.0.as_str().starts_with("x-ms-meta-"))
-            .for_each(|header| {
-                metadata.insert(
-                    header
-                        .0
-                        .as_str()
-                        .strip_prefix("x-ms-meta-")
-                        .unwrap()
-                        .to_owned(),
-                    Bytes::from(header.1.as_bytes().to_owned()),
-                );
-            });
-
         Ok(GetBlobMetadataResponse {
             request_id: request_id_from_headers(headers)?,
             etag: etag_from_headers(headers)?,
             server: server_from_headers(headers)?.to_owned(),
             date: date_from_headers(headers)?,
-            metadata,
+            metadata: headers.into(),
         })
     }
 }
