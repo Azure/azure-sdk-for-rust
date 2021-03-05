1a3118db432ee7c58bcd680b30da83976ab28639
Francesco Cogno <francesco.cogno@outlook.com>
2021-03-05 11:47:10 +0100

    fixed get_blob

--- sdk/storage/src/blob/blob/responses/get_blob_response.rs
+++ sdk/storage/src/blob/blob/responses/get_blob_response.rs
@@ -2,37 +2,35 @@ use crate::blob::blob::Blob;
 use azure_core::errors::AzureError;
 use azure_core::headers::{date_from_headers, request_id_from_headers};
 use azure_core::RequestId;
+use bytes::Bytes;
 use chrono::{DateTime, Utc};
-use http::HeaderMap;
+use http::Response;
+use std::convert::TryFrom;
 
 #[derive(Debug, Clone)]
 pub struct GetBlobResponse {
-    pub blob: Blob,
     pub request_id: RequestId,
-    pub data: Vec<u8>,
+    pub data: Bytes,
     pub date: DateTime<Utc>,
     pub content_range: Option<String>,
 }
 
-impl GetBlobResponse {
-    pub(crate) fn from_response(
-        headers: &HeaderMap,
-        blob: Blob,
-        body: &[u8],
-    ) -> Result<GetBlobResponse, AzureError> {
-        debug!("headers == {:#?}", headers);
+impl TryFrom<Response<Bytes>> for GetBlobResponse {
+    type Error = AzureError;
+    fn try_from(response: Response<Bytes>) -> Result<Self, Self::Error> {
+        println!("response.headers() == {:#?}", response.headers());
 
-        let request_id = request_id_from_headers(headers)?;
-        let date = date_from_headers(headers)?;
+        let request_id = request_id_from_headers(response.headers())?;
+        let date = date_from_headers(response.headers())?;
 
-        let content_range = headers
+        let content_range = response
+            .headers()
             .get(http::header::CONTENT_RANGE)
             .map(|h| h.to_str().unwrap().to_owned());
 
         Ok(GetBlobResponse {
-            blob,
             request_id,
-            data: body.to_vec(),
+            data: response.into_body(),
             date,
             content_range,
         })
