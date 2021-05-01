83f4a2a765ecd14600d4c6449b7817973721ef4c
Radu M <root@radu.sh>
2021-04-30 18:58:21 -0700

    Remove conditional compilation statements and add async_trait
    Signed-off-by: Radu M <root@radu.sh>

--- sdk/core/src/http_client.rs
+++ sdk/core/src/http_client.rs
@@ -9,8 +9,7 @@ use serde::Serialize;
 #[cfg(feature = "enable_wasi_experimental_http")]
 use wasi_experimental_http;
 
-#[cfg_attr(target_arch = "wasm32", async_trait(?Send))]
-#[cfg_attr(not(target_arch = "wasm32"), async_trait)]
+#[async_trait]
 pub trait HttpClient: Send + Sync + std::fmt::Debug {
     async fn execute_request(
         &self,
@@ -65,75 +64,73 @@ pub trait HttpClient: Send + Sync + std::fmt::Debug {
     }
 }
 
-// #[cfg(feature = "enable_hyper")]
-// #[cfg(not(feature = "enable_wasi_experimental_http"))]
-// // #[cfg_attr(target_arch = "wasm32", async_trait(?Send))]
-// #[cfg_attr(not(target_arch = "wasm32"), async_trait)]
-// impl HttpClient for hyper::Client<HttpsConnector<hyper::client::HttpConnector>> {
-//     async fn execute_request(
-//         &self,
-//         request: Request<Bytes>,
-//     ) -> Result<Response<Bytes>, Box<dyn std::error::Error + Sync + Send>> {
-//         let mut hyper_request = hyper::Request::builder()
-//             .uri(request.uri())
-//             .method(request.method());
+#[cfg(feature = "enable_hyper")]
+#[cfg(not(feature = "enable_wasi_experimental_http"))]
+#[async_trait]
+impl HttpClient for hyper::Client<HttpsConnector<hyper::client::HttpConnector>> {
+    async fn execute_request(
+        &self,
+        request: Request<Bytes>,
+    ) -> Result<Response<Bytes>, Box<dyn std::error::Error + Sync + Send>> {
+        let mut hyper_request = hyper::Request::builder()
+            .uri(request.uri())
+            .method(request.method());
 
-//         for header in request.headers() {
-//             hyper_request = hyper_request.header(header.0, header.1);
-//         }
+        for header in request.headers() {
+            hyper_request = hyper_request.header(header.0, header.1);
+        }
 
-//         let hyper_request = hyper_request.body(hyper::Body::from(request.into_body()))?;
+        let hyper_request = hyper_request.body(hyper::Body::from(request.into_body()))?;
 
-//         let hyper_response = self.request(hyper_request).await?;
+        let hyper_response = self.request(hyper_request).await?;
 
-//         let mut response = Response::builder()
-//             .status(hyper_response.status())
-//             .version(hyper_response.version());
+        let mut response = Response::builder()
+            .status(hyper_response.status())
+            .version(hyper_response.version());
 
-//         for (key, value) in hyper_response.headers() {
-//             response = response.header(key, value);
-//         }
+        for (key, value) in hyper_response.headers() {
+            response = response.header(key, value);
+        }
 
-//         let response = response.body(hyper::body::to_bytes(hyper_response.into_body()).await?)?;
+        let response = response.body(hyper::body::to_bytes(hyper_response.into_body()).await?)?;
 
-//         Ok(response)
-//     }
-// }
+        Ok(response)
+    }
+}
 
-// #[cfg(feature = "enable_reqwest")]
-// // #[cfg_attr(target_arch = "wasm32", async_trait(?Send))]
-// #[cfg_attr(not(target_arch = "wasm32"), async_trait)]
-// #[cfg(not(feature = "enable_wasi_experimental_http"))]
-// impl HttpClient for reqwest::Client {
-//     async fn execute_request(
-//         &self,
-//         request: Request<Bytes>,
-//     ) -> Result<Response<Bytes>, Box<dyn std::error::Error + Sync + Send>> {
-//         let mut reqwest_request =
-//             self.request(request.method().clone(), &request.uri().to_string());
-//         for header in request.headers() {
-//             reqwest_request = reqwest_request.header(header.0, header.1);
-//         }
+#[cfg(feature = "enable_reqwest")]
+#[cfg(not(feature = "enable_wasi_experimental_http"))]
+#[async_trait]
+impl HttpClient for reqwest::Client {
+    async fn execute_request(
+        &self,
+        request: Request<Bytes>,
+    ) -> Result<Response<Bytes>, Box<dyn std::error::Error + Sync + Send>> {
+        let mut reqwest_request =
+            self.request(request.method().clone(), &request.uri().to_string());
+        for header in request.headers() {
+            reqwest_request = reqwest_request.header(header.0, header.1);
+        }
 
-//         let reqwest_request = reqwest_request.body(request.into_body()).build()?;
+        let reqwest_request = reqwest_request.body(request.into_body()).build()?;
 
-//         let reqwest_response = self.execute(reqwest_request).await?;
+        let reqwest_response = self.execute(reqwest_request).await?;
 
-//         let mut response = Response::builder().status(reqwest_response.status());
+        let mut response = Response::builder().status(reqwest_response.status());
 
-//         if let Some(version) = get_version(&reqwest_response) {
-//             response = response.version(version);
-//         }
+        if let Some(version) = get_version(&reqwest_response) {
+            response = response.version(version);
+        }
 
-//         for (key, value) in reqwest_response.headers() {
-//             response = response.header(key, value);
-//         }
+        for (key, value) in reqwest_response.headers() {
+            response = response.header(key, value);
+        }
 
-//         let response = response.body(reqwest_response.bytes().await?)?;
+        let response = response.body(reqwest_response.bytes().await?)?;
 
-//         Ok(response)
-//     }
-// }
+        Ok(response)
+    }
+}
 
 #[cfg(feature = "enable_wasi_experimental_http")]
 #[cfg(all(target_arch = "wasm32", target_os = "wasi"))]
@@ -142,12 +139,13 @@ pub struct WasiHttpClient {}
 
 #[cfg(feature = "enable_wasi_experimental_http")]
 #[cfg(all(target_arch = "wasm32", target_os = "wasi"))]
-#[cfg_attr(target_os = "wasi", async_trait(?Send))]
+#[async_trait]
 impl HttpClient for WasiHttpClient {
     async fn execute_request(
         &self,
         request: Request<Bytes>,
     ) -> Result<Response<Bytes>, Box<dyn std::error::Error + Sync + Send>> {
+        println!("trying to make request to: {}", request.uri());
         let mut r = http::request::Builder::new()
             .uri(request.uri())
             .method(request.method().clone());
