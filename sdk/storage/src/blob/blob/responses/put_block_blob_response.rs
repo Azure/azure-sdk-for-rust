741d03b865ec420d15246cd192022cf98113d4f5
Francesco Cogno <francesco.cogno@outlook.com>
2021-05-10 18:09:40 +0200

    Revert "Fix consistency response (#236)"
    This reverts commit f55c49fd2519aaac0a6687ca54be9f9defcbb396.

--- sdk/storage/src/blob/blob/responses/put_block_blob_response.rs
+++ sdk/storage/src/blob/blob/responses/put_block_blob_response.rs
@@ -3,7 +3,7 @@ use azure_core::headers::{
     consistency_from_headers, date_from_headers, etag_from_headers, last_modified_from_headers,
     request_id_from_headers, request_server_encrypted_from_headers,
 };
-use azure_core::{ConsistencyCRC64, ConsistencyMD5, RequestId};
+use azure_core::{Consistency, RequestId};
 use chrono::{DateTime, Utc};
 use http::HeaderMap;
 
@@ -11,8 +11,7 @@ use http::HeaderMap;
 pub struct PutBlockBlobResponse {
     pub etag: String,
     pub last_modified: DateTime<Utc>,
-    pub content_md5: ConsistencyMD5,
-    pub content_crc64: Option<ConsistencyCRC64>,
+    pub consistency: Consistency,
     pub request_id: RequestId,
     pub date: DateTime<Utc>,
     pub request_server_encrypted: bool,
@@ -24,7 +23,7 @@ impl PutBlockBlobResponse {
 
         let etag = etag_from_headers(headers)?;
         let last_modified = last_modified_from_headers(headers)?;
-        let (content_md5, content_crc64) = consistency_from_headers(headers)?;
+        let consistency = consistency_from_headers(headers)?;
         let request_id = request_id_from_headers(headers)?;
         let date = date_from_headers(headers)?;
         let request_server_encrypted = request_server_encrypted_from_headers(headers)?;
@@ -32,8 +31,7 @@ impl PutBlockBlobResponse {
         Ok(PutBlockBlobResponse {
             etag,
             last_modified,
-            content_md5,
-            content_crc64,
+            consistency,
             request_id,
             date,
             request_server_encrypted,
--- sdk/storage/src/blob/blob/responses/put_block_response.rs
+++ sdk/storage/src/blob/blob/responses/put_block_response.rs
@@ -3,14 +3,13 @@ use azure_core::headers::{
     consistency_from_headers, date_from_headers, request_id_from_headers,
     request_server_encrypted_from_headers,
 };
-use azure_core::{ConsistencyCRC64, ConsistencyMD5, RequestId};
+use azure_core::{Consistency, RequestId};
 use chrono::{DateTime, Utc};
 use http::HeaderMap;
 
 #[derive(Debug, Clone, PartialEq)]
 pub struct PutBlockResponse {
-    pub content_md5: ConsistencyMD5,
-    pub content_crc64: Option<ConsistencyCRC64>,
+    pub consistency: Consistency,
     pub request_id: RequestId,
     pub date: DateTime<Utc>,
     pub request_server_encrypted: bool,
@@ -20,14 +19,13 @@ impl PutBlockResponse {
     pub(crate) fn from_headers(headers: &HeaderMap) -> Result<PutBlockResponse, AzureError> {
         debug!("{:#?}", headers);
 
-        let (content_md5, content_crc64) = consistency_from_headers(headers)?;
+        let consistency = consistency_from_headers(headers)?;
         let request_id = request_id_from_headers(headers)?;
         let date = date_from_headers(headers)?;
         let request_server_encrypted = request_server_encrypted_from_headers(headers)?;
 
         Ok(PutBlockResponse {
-            content_md5,
-            content_crc64,
+            consistency,
             request_id,
             date,
             request_server_encrypted,
