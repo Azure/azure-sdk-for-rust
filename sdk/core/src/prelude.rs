eb52988af8257c58a009d2f014ddbfd6b49ff65b
Francesco Cogno <francesco.cogno@gmail.com>
2021-05-10 19:30:24 +0200

    Revert "Fix consistency response (#236)" (#244)
    This reverts commit f55c49fd2519aaac0a6687ca54be9f9defcbb396.

--- sdk/core/src/prelude.rs
+++ sdk/core/src/prelude.rs
@@ -2,6 +2,6 @@ pub use crate::errors::AzureError;
 pub use crate::etag::Etag;
 pub use crate::request_options::*;
 pub use crate::{
-    AddAsHeader, AppendToUrlQuery, ConsistencyCRC64, ConsistencyMD5, HttpClient, RequestId,
-    SessionToken, StoredAccessPolicy, StoredAccessPolicyList, EMPTY_BODY,
+    AddAsHeader, AppendToUrlQuery, Consistency, HttpClient, RequestId, SessionToken,
+    StoredAccessPolicy, StoredAccessPolicyList, EMPTY_BODY,
 };
