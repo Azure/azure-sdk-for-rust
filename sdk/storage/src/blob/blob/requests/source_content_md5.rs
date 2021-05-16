79ea02c7d4b8baeec65ee6d83f00ee7de82089e8
Cameron Taggart <cameron.taggart@gmail.com>
2021-05-16 08:39:14 -0700

    remove md5 from core (#248)

--- sdk/storage/src/blob/blob/requests/source_content_md5.rs
+++ sdk/storage/src/blob/blob/requests/source_content_md5.rs
@@ -1,4 +1,4 @@
-use crate::AddAsHeader;
+use azure_core::AddAsHeader;
 use http::request::Builder;
 
 #[derive(Debug, Clone, PartialEq, PartialOrd, Eq, Ord)]
@@ -6,7 +6,7 @@ pub struct SourceContentMD5([u8; 16]);
 
 impl AddAsHeader for SourceContentMD5 {
     fn add_as_header(&self, builder: Builder) -> Builder {
-        builder.header(crate::SOURCE_CONTENT_MD5, base64::encode(self.0))
+        builder.header("x-ms-source-content-md5", base64::encode(self.0))
     }
 }
 
