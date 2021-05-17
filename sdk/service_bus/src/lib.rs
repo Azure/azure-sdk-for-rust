4d01253190a2f4fa71572c64caeba96fade49af3
Cameron Taggart <cameron.taggart@gmail.com>
2021-05-17 10:59:20 -0700

    comment warning

--- sdk/service_bus/src/lib.rs
+++ sdk/service_bus/src/lib.rs
@@ -1,7 +1,7 @@
 #![recursion_limit = "128"]
 
-#[macro_use]
-extern crate log;
+// #[macro_use]
+// extern crate log;
 
 // // TODO update service_bus to use HttpClient https://github.com/Azure/azure-sdk-for-rust/issues/255
 // pub mod event_hub;
