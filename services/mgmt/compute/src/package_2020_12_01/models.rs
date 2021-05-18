906bc9acc0a1ea7d2dc386971871679d04873833
Cameron Taggart <cameron.taggart@gmail.com>
2021-05-18 10:52:30 -0700

    VirtualMachineExtension should have SubResource

--- services/mgmt/compute/src/package_2020_12_01/models.rs
+++ services/mgmt/compute/src/package_2020_12_01/models.rs
@@ -420,7 +420,7 @@ pub struct VirtualMachineExtensionUpdateProperties {
 #[derive(Clone, Debug, PartialEq, Serialize, Deserialize)]
 pub struct VirtualMachineExtension {
     #[serde(flatten)]
-    pub resource: Resource,
+    pub resource: SubResource,
     #[serde(default, skip_serializing_if = "Option::is_none")]
     pub properties: Option<VirtualMachineExtensionProperties>,
 }
