f1c320baa4d076bbca7729cbfb7b0d1a4e7a9601
Cameron Taggart <cameron.taggart@gmail.com>
2021-05-26 16:29:46 -0700

    addition cases for identity::Error

--- sdk/identity/src/errors.rs
+++ sdk/identity/src/errors.rs
@@ -3,14 +3,22 @@ use serde::{Deserialize, Serialize};
 
 #[non_exhaustive]
 #[derive(Debug, thiserror::Error)]
-/// An unrecognized error response from an identity service.
 pub enum Error {
     #[error("Error getting token credentials from Azure CLI: {0}")]
     AzureCliCredentialError(#[from] crate::token_credentials::AzureCliCredentialError),
     #[error("Client secret credentials error: {0}")]
     ClientSecretCredentialError(#[from] crate::token_credentials::ClientSecretCredentialError),
+    #[error("Error getting environment credential: {0}")]
+    EnvironmentCredentialError(#[from] crate::token_credentials::EnvironmentCredentialError),
+    #[error("Error getting managed identity credential: {0}")]
+    ManagedIdentityCredentialError(
+        #[from] crate::token_credentials::ManagedIdentityCredentialError,
+    ),
+    #[error("Error getting default credential: {0}")]
+    DefaultCredentialError(#[from] crate::token_credentials::DefaultCredentialError),
     #[error("Error refreshing token: {0}")]
     RefreshTokenError(#[from] crate::refresh_token::Error),
+    /// An unrecognized error response from an identity service.
     #[error("Error response from service: {0}")]
     ErrorResponse(String),
 
