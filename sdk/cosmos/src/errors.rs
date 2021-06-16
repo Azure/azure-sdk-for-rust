2b80706de48a028f716ba2b8bd18d0023e9baf68
Ryan Levick <me@ryanlevick.com>
2021-06-16 18:20:04 +0200

    Do a specific parsing error

--- sdk/cosmos/src/errors.rs
+++ sdk/cosmos/src/errors.rs
@@ -5,28 +5,18 @@
 pub enum Error {
     #[error(transparent)]
     AzureCoreError(#[from] azure_core::Error),
-    #[error("Resource quota parsing error: {0}")]
-    ResourceQuotaParsingError(#[from] crate::resource_quota::ResourceQuotaParsingError),
+    #[error(transparent)]
+    ParsingError(ParsingError),
     #[error("Header not found: {0}")]
     HeaderNotFound(String),
     #[error("To str error: {0}")]
     ToStrError(#[from] http::header::ToStrError),
-    #[error("Parsing error: {0}")]
-    ParsingError(#[from] azure_core::ParsingError),
     #[error("http error: {0}")]
     AzureHttpError(#[from] azure_core::HttpError),
     #[error("stream error: {0}")]
     StreamError(#[from] azure_core::StreamError),
     #[error("http error: {0}")]
     HttpError(#[from] http::Error),
-    #[error("Parse int error: {0}")]
-    ParseIntError(#[from] std::num::ParseIntError),
-    #[error("uuid error: {0}")]
-    ParseUuidError(#[from] uuid::Error),
-    #[error("Date time parse error: {0}")]
-    DateTimeParseError(#[from] chrono::format::ParseError),
-    #[error("Parse float error: {0}")]
-    ParseFloatError(#[from] std::num::ParseFloatError),
     #[error("JSON error: {0}")]
     JsonError(#[from] serde_json::Error),
     #[error("UTF-8 conversion error: {0}")]
@@ -36,4 +26,20 @@ pub enum Error {
     /// Other errors that can happen but are unlikely to be matched against
     #[error(transparent)]
     Other(#[from] Box<dyn std::error::Error + Send + Sync>),
+}
+
+#[derive(Debug, thiserror::Error)]
+pub enum ParsingError {
+    #[error("Parse int error: {0}")]
+    ParseIntError(#[from] std::num::ParseIntError),
+    #[error("uuid error: {0}")]
+    ParseUuidError(#[from] uuid::Error),
+    #[error("Date time parse error: {0}")]
+    ParseDateTimeError(#[from] chrono::format::ParseError),
+    #[error("Parse float error: {0}")]
+    ParseFloatError(#[from] std::num::ParseFloatError),
+    #[error("Resource quota parsing error: {0}")]
+    ParseResourceQuotaError(#[from] crate::resource_quota::ResourceQuotaParsingError),
+    #[error("parsing error: {0}")]
+    Other(#[from] azure_core::ParsingError),
 }
