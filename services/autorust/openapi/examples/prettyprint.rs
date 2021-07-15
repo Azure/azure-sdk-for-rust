32fb3c96e851bfa098e06622fab37b535e7dd0b4
Cameron Taggart <cameron.taggart@gmail.com>
2021-07-15 16:10:47 -0600

    copy of https://github.com/ctaggart/autorust_openapi/commit/fc2f579f27b92e0cebbf75abfec3e2cef9353e11

---
+++ services/autorust/openapi/examples/prettyprint.rs
@@ -0,0 +1,45 @@
+use autorust_openapi::OpenAPI;
+use std::{
+    fs::{self, create_dir_all, File},
+    io::Write,
+    path::{Path, PathBuf},
+    process::exit,
+};
+
+pub type Error = Box<dyn std::error::Error>;
+pub type Result<T> = std::result::Result<T, Error>;
+
+// Pass in a output directory as the second parameter if you want to write the file instead of printing it to stdout.
+// cargo run --example prettyprint -- ../azure-rest-api-specs/specification/security/resource-manager/common/v1/types.json
+// cargo run --example prettyprint -- ../OpenAPI-Specification/examples/v2.0/json/petstore.json tmp
+fn main() -> Result<()> {
+    match std::env::args().nth(1) {
+        None => {
+            eprintln!("Please pass in the spec path.");
+            exit(1);
+        }
+        Some(file_in) => {
+            let file_in = Path::new(&file_in);
+            // reading the whole file upfront is much faster than using a BufReader
+            // https://github.com/serde-rs/json/issues/160
+            let bytes = fs::read(file_in)?;
+            let spec: OpenAPI = serde_json::from_slice(&bytes)?;
+            let json = serde_json::to_string_pretty(&spec)?;
+
+            match std::env::args().nth(2) {
+                Some(dir_out) => {
+                    create_dir_all(&dir_out)?;
+                    let mut file_out = PathBuf::new();
+                    file_out.push(&dir_out);
+                    file_out.push(&file_in.file_name().unwrap());
+                    let mut file = File::create(file_out)?;
+                    file.write_all(json.as_bytes())?;
+                }
+                None => {
+                    println!("{}", json);
+                }
+            }
+        }
+    }
+    Ok(())
+}
