3d1183878b790f93c5f4f0105dfd122c230be94f
Cameron Taggart <cameron.taggart@gmail.com>
2021-07-15 20:36:35 -0600

    use separate paths for repositories

--- services/autorust/codegen/tests/azure_rest_api_specs.rs
+++ services/autorust/codegen/tests/azure_rest_api_specs.rs
@@ -9,23 +9,13 @@ use std::path::PathBuf;
 
 type Result<T> = std::result::Result<T, Box<dyn std::error::Error>>;
 
-const COMMON_TYPES_SPEC: &str = "azure-rest-api-specs/specification/security/resource-manager/common/v1/types.json";
-const VMWARE_SPEC: &str = "azure-rest-api-specs/specification/vmware/resource-manager/Microsoft.AVS/stable/2020-03-20/vmware.json";
-
-/// Get the relative base test directory.
-/// CI uses a ../, otherwise ../../
-/// https://docs.github.com/en/actions/reference/environment-variables
-fn base_dir(path: &str) -> String {
-    if std::env::var("CI").is_ok() {
-        "../../../".to_owned() + path
-    } else {
-        "../../../../".to_owned() + path
-    }
-}
+const COMMON_TYPES_SPEC: &str = "../../../../azure-rest-api-specs/specification/security/resource-manager/common/v1/types.json";
+const VMWARE_SPEC: &str =
+    "../../../../azure-rest-api-specs/specification/vmware/resource-manager/Microsoft.AVS/stable/2020-03-20/vmware.json";
 
 #[test]
 fn refs_count_security_common() -> Result<()> {
-    let api = &spec::openapi::parse(&base_dir(COMMON_TYPES_SPEC))?;
+    let api = &spec::openapi::parse(COMMON_TYPES_SPEC)?;
     let refs = spec::openapi::get_references(api);
     assert_eq!(15, refs.len());
     Ok(())
@@ -33,7 +23,7 @@ fn refs_count_security_common() -> Result<()> {
 
 #[test]
 fn refs_count_avs() -> Result<()> {
-    let api = &spec::openapi::parse(&base_dir(VMWARE_SPEC))?;
+    let api = &spec::openapi::parse(VMWARE_SPEC)?;
     let refs = spec::openapi::get_references(api);
     assert_eq!(199, refs.len());
     Ok(())
@@ -41,53 +31,49 @@ fn refs_count_avs() -> Result<()> {
 
 #[test]
 fn ref_files() -> Result<()> {
-    let api = &spec::openapi::parse(&base_dir(VMWARE_SPEC))?;
+    let api = &spec::openapi::parse(VMWARE_SPEC)?;
     let files = spec::openapi::get_reference_file_paths(api);
     assert_eq!(1, files.len());
-    assert!(files.contains(&base_dir("../common-types/resource-management/v1/types.json")));
+    assert!(files.contains("../../../../../common-types/resource-management/v1/types.json"));
     Ok(())
 }
 
 #[test]
 fn read_spec_avs() -> Result<()> {
-    let spec = &Spec::read_files(&[&base_dir(VMWARE_SPEC)])?;
+    let spec = &Spec::read_files(&[VMWARE_SPEC])?;
     assert_eq!(2, spec.docs().len());
-    assert!(spec.docs().contains_key(std::path::Path::new(&base_dir(
-        "azure-rest-api-specs/specification/common-types/resource-management/v1/types.json"
-    ))));
+    assert!(spec.docs().contains_key(std::path::Path::new(
+        "../../../../azure-rest-api-specs/specification/common-types/resource-management/v1/types.json"
+    )));
     Ok(())
 }
 
 #[test]
 fn test_resolve_schema_ref() -> Result<()> {
-    let file = PathBuf::from(&base_dir(VMWARE_SPEC));
+    let file = PathBuf::from(VMWARE_SPEC);
     let spec = &Spec::read_files(&[&file])?;
-    spec.resolve_schema_ref(&file, Reference::parse("#/definitions/OperationList")?)?;
+    spec.resolve_schema_ref(&file, Reference::parse("#/definitions/OperationList").unwrap())?;
     spec.resolve_schema_ref(
         &file,
-        Reference::parse(&base_dir(
-            "../common-types/resource-management/v1/types.json#/definitions/ErrorResponse",
-        ))?,
+        Reference::parse("../../../../../common-types/resource-management/v1/types.json#/definitions/ErrorResponse").unwrap(),
     )?;
     Ok(())
 }
 
 #[test]
 fn test_resolve_parameter_ref() -> Result<()> {
-    let file = PathBuf::from(&base_dir(VMWARE_SPEC));
+    let file = PathBuf::from(VMWARE_SPEC);
     let spec = &Spec::read_files(&[&file])?;
     spec.resolve_parameter_ref(
         &file,
-        Reference::parse(&base_dir(
-            "../common-types/resource-management/v1/types.json#/parameters/ApiVersionParameter",
-        ))?,
+        Reference::parse("../../../../../common-types/resource-management/v1/types.json#/parameters/ApiVersionParameter").unwrap(),
     )?;
     Ok(())
 }
 
 #[test]
 fn test_resolve_all_refs() -> Result<()> {
-    let doc_file = PathBuf::from(&base_dir(VMWARE_SPEC));
+    let doc_file = PathBuf::from(VMWARE_SPEC);
     let spec = &Spec::read_files(&[&doc_file])?;
     for (doc_file, doc) in spec.docs() {
         let refs = spec::openapi::get_references(doc);
--- services/autorust/openapi/tests/azure_rest_api_specs.rs
+++ services/autorust/openapi/tests/azure_rest_api_specs.rs
@@ -6,15 +6,15 @@ mod common;
 use common::*;
 
 const PATHS: &[&str] = &[
-    "../azure-rest-api-specs/specification/vmware/resource-manager/Microsoft.AVS/stable/2020-03-20/vmware.json",
-    "../azure-rest-api-specs/specification/batch/data-plane/Microsoft.Batch/stable/2020-03-01.11.0/BatchService.json",
-    "../azure-rest-api-specs/specification/security/resource-manager/common/v1/types.json",
-    "../azure-rest-api-specs/specification/cosmos-db/resource-manager/Microsoft.DocumentDB/stable/2020-04-01/cosmos-db.json",
-    "../azure-rest-api-specs/specification/alertsmanagement/resource-manager/Microsoft.AlertsManagement/preview/2019-05-05-preview/ActionRules.json",
+    "../../../../azure-rest-api-specs/specification/vmware/resource-manager/Microsoft.AVS/stable/2020-03-20/vmware.json",
+    "../../../../azure-rest-api-specs/specification/batch/data-plane/Microsoft.Batch/stable/2020-03-01.11.0/BatchService.json",
+    "../../../../azure-rest-api-specs/specification/security/resource-manager/common/v1/types.json",
+    "../../../../azure-rest-api-specs/specification/cosmos-db/resource-manager/Microsoft.DocumentDB/stable/2020-04-01/cosmos-db.json",
+    "../../../../azure-rest-api-specs/specification/alertsmanagement/resource-manager/Microsoft.AlertsManagement/preview/2019-05-05-preview/ActionRules.json",
     // https://github.com/ctaggart/autorust_openapi/issues/20
-    // "../azure-rest-api-specs/specification/apimanagement/resource-manager/Microsoft.ApiManagement/stable/2019-12-01/apimapis.json",
-    "../azure-rest-api-specs/specification/communication/data-plane/Chat/stable/2021-03-07/communicationserviceschat.json",
-    "../azure-rest-api-specs/specification/eventgrid/data-plane/Microsoft.EventGrid/stable/2018-01-01/EventGrid.json",
+    // "../../../../azure-rest-api-specs/specification/apimanagement/resource-manager/Microsoft.ApiManagement/stable/2019-12-01/apimapis.json",
+    "../../../../azure-rest-api-specs/specification/communication/data-plane/Chat/stable/2021-03-07/communicationserviceschat.json",
+    "../../../../azure-rest-api-specs/specification/eventgrid/data-plane/Microsoft.EventGrid/stable/2018-01-01/EventGrid.json",
 ];
 
 #[test]
