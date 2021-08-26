5350f09e80c4222c7a9288c905507f6d3a55c09b
Joshua Gendein <joshua@gendein.com>
2021-08-26 04:02:09 -0700

    Move (create, replace, get, delete)_permission to pipeline architecture (#349)
    Co-authored-by: Joshua Gendein <jgendein@microsoft.com>

---
+++ sdk/cosmos/src/resources/permission/permission_response.rs
@@ -0,0 +1,34 @@
+use crate::headers::from_headers::*;
+
+use azure_core::headers::{etag_from_headers, session_token_from_headers};
+use azure_core::{collect_pinned_stream, Response as HttpResponse};
+
+use super::Permission;
+
+#[derive(Debug, Clone, PartialEq)]
+pub struct PermissionResponse<'a> {
+    pub permission: Permission<'a>,
+    pub charge: f64,
+    pub activity_id: uuid::Uuid,
+    pub etag: String,
+    pub session_token: String,
+    pub content_path: String,
+    pub alt_content_path: String,
+}
+
+impl<'a> PermissionResponse<'a> {
+    pub async fn try_from(response: HttpResponse) -> Result<PermissionResponse<'a>, crate::Error> {
+        let (_status_code, headers, pinned_stream) = response.deconstruct();
+        let body = collect_pinned_stream(pinned_stream).await?;
+
+        Ok(Self {
+            permission: serde_json::from_slice(&body)?,
+            charge: request_charge_from_headers(&headers)?,
+            activity_id: activity_id_from_headers(&headers)?,
+            session_token: session_token_from_headers(&headers)?,
+            etag: etag_from_headers(&headers)?,
+            content_path: String::from(content_path_from_headers(&headers)?),
+            alt_content_path: String::from(alt_content_path_from_headers(&headers)?),
+        })
+    }
+}
