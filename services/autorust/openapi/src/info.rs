1f2162a9e70c5aee2773abf7cd181d0b167a22c3
Cameron Taggart <cameron.taggart@gmail.com>
2021-08-30 16:55:45 -0500

    import autorust_openapi (#328)
    * copy of https://github.com/ctaggart/autorust_openapi/commit/fc2f579f27b92e0cebbf75abfec3e2cef9353e11
    
    * build autorust
    
    * cd services/autorust
    
    * use base_dir
    
    * remove custom path
    
    * $GITHUB_WORKSPACE
    
    * echo pwd
    
    * echo pwd
    
    * echo $PWD
    
    * specify paths
    
    * use separate paths for repositories
    
    * test openapi_spec_examples too
    
    * Copyright (c) 2020 Cameron Taggart
    
    * set major.minor versions
    
    * versions for dev-dependencies
    
    * name: test AutoRust
    
    * add NOTICE.txt for
    https://github.com/softprops/openapi/blob/master/LICENSE
    https://github.com/glademiller/openapiv3/blob/master/LICENSE-MIT

---
+++ services/autorust/openapi/src/info.rs
@@ -0,0 +1,30 @@
+use crate::{Contact, License, MsCodeGenerationSetting};
+use indexmap::IndexMap;
+use serde::{Deserialize, Serialize};
+
+/// General information about the API.
+/// https://github.com/OAI/OpenAPI-Specification/blob/master/versions/2.0.md#info-object
+#[derive(Clone, Debug, Deserialize, Serialize, PartialEq, Default)]
+#[serde(rename_all = "lowercase")]
+pub struct Info {
+    /// A unique and precise title of the API.
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub title: Option<String>,
+    /// A semantic version number of the API.
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub description: Option<String>,
+    #[serde(rename = "termsOfService", skip_serializing_if = "Option::is_none")]
+    pub terms_of_service: Option<String>,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub contact: Option<Contact>,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub license: Option<License>,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub version: Option<String>,
+
+    /// enables passing code generation settings via OpenAPI definition
+    /// (deprecated! Please use configuration files instead.)
+    /// https://github.com/Azure/autorest/blob/master/docs/extensions/readme.md#x-ms-code-generation-settings
+    #[serde(rename = "x-ms-code-generation-settings", default, skip_serializing_if = "IndexMap::is_empty")]
+    pub x_ms_code_generation_settings: IndexMap<String, MsCodeGenerationSetting>,
+}
---
+++ services/autorust/openapi/src/openapi.rs
@@ -0,0 +1,54 @@
+use crate::*;
+use indexmap::IndexMap;
+use serde::{Deserialize, Serialize};
+
+/// This is the root document object for the API specification.
+/// It combines what previously was the Resource Listing and API Declaration (version 1.2 and earlier) together into one document.
+/// https://github.com/OAI/OpenAPI-Specification/blob/master/versions/2.0.md#swagger-object
+#[derive(Clone, Debug, Deserialize, Serialize, PartialEq, Default)]
+#[serde(rename_all = "camelCase")]
+pub struct OpenAPI {
+    /// The Swagger version of this document.
+    pub swagger: String,
+    pub info: Info,
+    /// The host (name or ip) of the API. Example: 'swagger.io'
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub host: Option<String>,
+    /// The base path to the API. Example: '/api'.
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub base_path: Option<String>,
+    #[serde(default, skip_serializing_if = "Vec::is_empty")]
+    pub schemes: Vec<Scheme>,
+    /// A list of MIME types accepted by the API.
+    #[serde(default, skip_serializing_if = "Vec::is_empty")]
+    pub consumes: Vec<String>,
+    /// A list of MIME types the API can produce.
+    #[serde(default, skip_serializing_if = "Vec::is_empty")]
+    pub produces: Vec<String>,
+    /// Relative paths to the individual endpoints. They must be relative to the 'basePath'.
+    // #[serde(default, skip_serializing_if = "IndexMap::is_empty")] // do not skip
+    pub paths: IndexMap<String, ReferenceOr<PathItem>>,
+    /// Relative paths to the individual endpoints. They must be relative to the 'basePath'.
+    #[serde(default, rename = "x-ms-paths", skip_serializing_if = "IndexMap::is_empty")]
+    pub x_ms_paths: IndexMap<String, ReferenceOr<PathItem>>,
+    #[serde(default, skip_serializing_if = "IndexMap::is_empty")]
+    pub definitions: IndexMap<String, ReferenceOr<Schema>>,
+    #[serde(default, skip_serializing_if = "IndexMap::is_empty")]
+    pub parameters: IndexMap<String, Parameter>,
+    /// mappings to http response codes or "default"
+    #[serde(default, skip_serializing_if = "IndexMap::is_empty")]
+    pub responses: IndexMap<StatusCode, ReferenceOr<Response>>,
+    #[serde(default, skip_serializing_if = "IndexMap::is_empty")]
+    pub security_definitions: IndexMap<String, Security>,
+    #[serde(default, skip_serializing_if = "Vec::is_empty")]
+    pub security: Vec<IndexMap<String, Vec<String>>>,
+    #[serde(default, skip_serializing_if = "Vec::is_empty")]
+    pub tags: Vec<Tag>,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub external_docs: Option<ExternalDocumentation>,
+
+    /// replaces the fixed host with a host template that can be replaced with variable parameters
+    /// https://github.com/Azure/autorest/blob/master/docs/extensions/readme.md#x-ms-parameterized-host
+    #[serde(rename = "x-ms-parameterized-host", skip_serializing_if = "Option::is_none")]
+    pub x_ms_parameterized_host: Option<MsParameterizedHost>,
+}
