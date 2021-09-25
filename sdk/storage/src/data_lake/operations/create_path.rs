515d15b77ad3c2044ed4eb102b8509dd251c8e73
Thomas Vollmer <34384788+thovoll@users.noreply.github.com>
2021-09-25 09:38:11 -0700

    Add missing content length header.

--- sdk/storage/src/data_lake/operations/create_path.rs
+++ sdk/storage/src/data_lake/operations/create_path.rs
@@ -1,4 +1,5 @@
 use azure_core::prelude::IfMatchCondition;
+use azure_core::prelude::ContentLength;
 use http::StatusCode;
 
 use azure_core::{Request as HttpRequest, Response as HttpResponse};
@@ -21,6 +22,7 @@ impl<'a> CreatePathOptions<'a> {
 
     pub(crate) fn decorate_request(&self, req: &mut HttpRequest) -> Result<(), crate::Error> {
         azure_core::headers::add_optional_header2(&self.if_match_condition, req)?;
+        azure_core::headers::add_mandatory_header2(&ContentLength::new(0), req)?; // Length is required for creating files
 
         req.set_body(bytes::Bytes::new().into()); // TODO: needed?
 
