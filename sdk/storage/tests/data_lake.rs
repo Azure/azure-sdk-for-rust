5439e4c26d6d9125ba6aae746fbe21e8938aa818
Thomas Vollmer <34384788+thovoll@users.noreply.github.com>
2021-09-28 10:05:27 -0700

    Use actual bearer token.

--- sdk/storage/tests/data_lake.rs
+++ sdk/storage/tests/data_lake.rs
@@ -6,6 +6,8 @@ use chrono::Utc;
 use futures::stream::StreamExt;
 use std::error::Error;
 use std::num::NonZeroU32;
+use azure_identity::token_credentials::DefaultCredential;
+use azure_identity::token_credentials::TokenCredential;
 
 #[tokio::test]
 async fn test_data_lake_file_system_functions() -> Result<(), Box<dyn Error + Send + Sync>> {
@@ -23,9 +25,14 @@ async fn test_data_lake_file_system_functions() -> Result<(), Box<dyn Error + Se
     let storage_account_client =
         StorageAccountClient::new_access_key(http_client.clone(), &account, &master_key);
 
+    println!("getting bearer token...");
+    let bearer_token = DefaultCredential::default()
+        .get_token("https://storage.azure.com/")
+        .await?;
+
     let data_lake = storage_account_client
         .as_storage_client()
-        .as_data_lake_client(account, "bearertoken".to_owned())?;
+        .as_data_lake_client(account, bearer_token.token.secret().to_owned())?;
 
     let file_system = data_lake.as_file_system_client(&file_system_name)?;
 
