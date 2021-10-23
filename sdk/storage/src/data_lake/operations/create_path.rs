8f4829150569cf4acd46a776d186fcbd3a4ee489
Thomas Vollmer <34384788+thovoll@users.noreply.github.com>
2021-10-23 06:45:26 -0700

    Improve create_path response headers.

--- sdk/storage/src/data_lake/operations/create_path.rs
+++ sdk/storage/src/data_lake/operations/create_path.rs
@@ -1,6 +1,11 @@
+use azure_core::headers::{
+    etag_from_headers, last_modified_from_headers, CommonStorageResponseHeaders,
+};
 use azure_core::prelude::ContentLength;
 use azure_core::prelude::IfMatchCondition;
-use http::StatusCode;
+use azure_core::RequestId;
+use chrono::{DateTime, Utc};
+use std::convert::TryInto;
 
 use azure_core::{Request as HttpRequest, Response as HttpResponse};
 
@@ -30,13 +35,23 @@ impl<'a> CreatePathOptions<'a> {
 
 #[derive(Debug, Clone)]
 pub struct CreatePathResponse {
-    pub status_code: StatusCode,
+    pub common_storage_response_headers: CommonStorageResponseHeaders,
+    pub etag: String,
+    pub last_modified: DateTime<Utc>,
 }
 
 impl CreatePathResponse {
     pub async fn try_from(response: HttpResponse) -> Result<Self, crate::Error> {
-        let (status_code, _headers, _pinned_stream) = response.deconstruct();
+        let (_status_code, headers, _pinned_stream) = response.deconstruct();
 
-        Ok(CreatePathResponse { status_code })
+        let common_storage_response_headers = (&headers).try_into()?;
+        let etag = etag_from_headers(&headers)?;
+        let last_modified = last_modified_from_headers(&headers)?;
+
+        Ok(Self {
+            common_storage_response_headers,
+            etag,
+            last_modified,
+        })
     }
 }
