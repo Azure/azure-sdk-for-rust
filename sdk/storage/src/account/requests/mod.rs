4f1b399bdfba0c14ab50a3b9105371dd4ecc0e13
Jens Walter <jens@apimeister.com>
2021-10-24 14:23:24 +0200

    support find blobs by tags operation

--- sdk/storage/src/account/requests/mod.rs
+++ sdk/storage/src/account/requests/mod.rs
@@ -1,2 +1,4 @@
+mod find_blobs_by_tags_builder;
 mod get_account_information_builder;
+pub use self::find_blobs_by_tags_builder::FindBlobsByTagsBuilder;
 pub use self::get_account_information_builder::GetAccountInformationBuilder;
