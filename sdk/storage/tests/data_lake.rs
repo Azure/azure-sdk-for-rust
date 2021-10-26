ec9cc5923efb61a505415a4647f8da1d1a63acd2
Thomas Vollmer <34384788+thovoll@users.noreply.github.com>
2021-10-26 06:50:14 -0700

    add "create path" operation (files only for now) to Azure Storage data_lake (using Pipeline architecture) (#358)
    * Ignore IDEA files.
    
    * Very rough draft.
    
    * Fix compiler error: align types of pipeline and pipeline_context.
    
    * Fix compiler error: align types of pipeline and pipeline_context.
    
    * Fix compiler error around request builder.
    
    * Match create_collection implementation.
    
    * Fix error conversion.
    
    * Fix data lake example.
    
    * Format code.
    
    * Fix typo.
    
    * Test path create. Failing...
    
    * Add missing content length header.
    
    * Clean up comment.
    
    * Remove commented code. Add timestamp to file system name. Rename variables. Fix newlines. Don't panic when create path fails.
    
    * WIP
    
    * Fail e2e test if create path did not work, while still deleting file system.
    
    * Use actual bearer token in auth header.
    
    * Use actual bearer token.
    
    * Code format.
    
    * Remove unneeded bearer token reference from DataLakeClient.
    
    * Print debug info.
    
    * Send proper auth header.
    
    * Code format.
    
    * Code format.
    
    * Adopt new way to do options.
    
    * Remove TODO comment.
    
    * No need to set body to empty byte array.
    
    * Remove comment.
    
    * Fix env var names.
    
    * Improve create_path response headers.
    
    * Ignore batch files.
    
    * Add assertions to e2e test.
    
    * Remove unused.
    
    * Format code. Fix bug.
    
    * Consolidate two examples into one. Align example with e2e test.
    
    * Add step to e2e test and example that fails to create file that already exists.
    
    * Add mock testing framework support code. But can't switch e2e test until all operations are using pipeline architecture.
    
    * Introduce actual DataLakeContext.

--- sdk/storage/tests/data_lake.rs
+++ sdk/storage/tests/data_lake.rs
@@ -1,65 +1,158 @@
 #![cfg(all(test, feature = "test_e2e"))]
+// #![cfg(feature = "mock_transport_framework")]
+
 use azure_core::prelude::*;
+use azure_identity::token_credentials::DefaultCredential;
+use azure_identity::token_credentials::TokenCredential;
 use azure_storage::core::prelude::*;
 use azure_storage::data_lake::prelude::*;
+use chrono::Utc;
 use futures::stream::StreamExt;
 use std::error::Error;
 use std::num::NonZeroU32;
 
 #[tokio::test]
 async fn test_data_lake_file_system_functions() -> Result<(), Box<dyn Error + Send + Sync>> {
-    // First we retrieve the account name and master key from environment variables.
-    let account = std::env::var("ADSL_STORAGE_ACCOUNT")
-        .expect("Set env variable ADSL_STORAGE_ACCOUNT first!");
-    let master_key = std::env::var("ADSL_STORAGE_MASTER_KEY")
-        .expect("Set env variable ADSL_STORAGE_MASTER_KEY first!");
+    let account = std::env::var("ADLSGEN2_STORAGE_ACCOUNT")
+        .expect("Set env variable ADLSGEN2_STORAGE_ACCOUNT first!");
+    let master_key = std::env::var("ADLSGEN2_STORAGE_MASTER_KEY")
+        .expect("Set env variable ADLSGEN2_STORAGE_MASTER_KEY first!");
 
-    let file_system_name = "azuresdke2etest";
+    let now = Utc::now();
+    let file_system_name = format!("azurerustsdk-datalake-e2etest-{}", now.timestamp());
 
     let http_client = new_http_client();
 
     let storage_account_client =
         StorageAccountClient::new_access_key(http_client.clone(), &account, &master_key);
 
-    let data_lake = storage_account_client
+    let resource_id = "https://storage.azure.com/";
+    println!("getting bearer token for '{}'...", resource_id);
+    let bearer_token = DefaultCredential::default().get_token(resource_id).await?;
+    println!("token expires on {}", bearer_token.expires_on);
+    println!();
+
+    let data_lake_client = storage_account_client
         .as_storage_client()
-        .as_data_lake_client(account)?;
+        // This test won't work during replay in CI until all operations are converted to pipeline architecture
+        // .as_data_lake_client_with_transaction(account, bearer_token.token.secret().to_owned(), "test_data_lake_file_system_functions")?;
+        .as_data_lake_client(account, bearer_token.token.secret().to_owned())?;
 
-    let file_system = data_lake.as_file_system_client(file_system_name)?;
+    let file_system_client = data_lake_client.as_file_system_client(&file_system_name)?;
 
-    let mut properties = Properties::new();
-    properties.insert("AddedVia", "Azure SDK for Rust");
-    properties.insert("CreatedAt", chrono::Utc::now().to_string());
-    let response = file_system
+    let mut fs_properties = Properties::new();
+    fs_properties.insert("AddedVia", "Azure SDK for Rust");
+
+    println!("creating file system '{}'...", &file_system_name);
+    let create_fs_response = file_system_client
         .create()
-        .properties(&properties)
+        .properties(&fs_properties)
         .execute()
         .await?;
-    println!("repsonse == {:?}", response);
+    println!("create file system response == {:?}", create_fs_response);
+    assert!(
+        create_fs_response.namespace_enabled,
+        "namespace should be enabled"
+    );
+    println!("namespace is enabled");
+    println!();
 
+    println!("listing file systems...");
     let mut stream = Box::pin(
-        data_lake
+        data_lake_client
             .list()
             .max_results(NonZeroU32::new(3).unwrap())
             .stream(),
     );
-
-    while let Some(response) = stream.next().await {
-        println!("response == {:?}\n\n", response);
+    let mut found = false;
+    while let Some(list_fs_response) = stream.next().await {
+        for fs in list_fs_response.unwrap().file_systems {
+            if fs.name == file_system_name {
+                found = true;
+            }
+        }
     }
+    assert!(found, "did not find created file system");
+    println!("found created file system");
+    println!();
 
-    properties.insert("ModifiedBy", "Iota");
-    let response = file_system
-        .set_properties(Some(&properties))
+    println!("getting file system properties...");
+    let get_fs_props_response = file_system_client.get_properties().execute().await?;
+    let properties_hashmap = get_fs_props_response.properties.hash_map();
+    let added_via_option = properties_hashmap.get("AddedVia");
+    assert!(
+        added_via_option.is_some(),
+        "did not find expected property: AddedVia"
+    );
+    assert_eq!(
+        added_via_option.unwrap().to_string(),
+        "Azure SDK for Rust",
+        "did not find expected property value for: AddedVia"
+    );
+    println!("found expected file system property: AddedVia");
+    println!();
+
+    let file_name = "e2etest-file.txt";
+
+    println!("creating path '{}'...", file_name);
+    let create_path_response = file_system_client
+        .create_path(Context::default(), file_name, CreatePathOptions::default())
+        .await?;
+    println!("create path response == {:?}", create_path_response);
+    println!();
+
+    println!("creating path '{}' (overwrite)...", file_name);
+    let create_path_response = file_system_client
+        .create_path(Context::default(), file_name, CreatePathOptions::default())
+        .await?;
+    println!("create path response == {:?}", create_path_response);
+    println!();
+
+    println!("creating path '{}' (do not overwrite)...", file_name);
+    let do_not_overwrite =
+        CreatePathOptions::new().if_match_condition(IfMatchCondition::NotMatch("*"));
+    let create_path_result = file_system_client
+        .create_path(Context::default(), file_name, do_not_overwrite)
+        .await;
+    assert!(create_path_result.is_err());
+    println!(
+        "create path result (should fail) == {:?}",
+        create_path_result
+    );
+    println!();
+
+    println!("setting file system properties...");
+    fs_properties.insert("ModifiedBy", "Iota");
+    let set_fs_props_response = file_system_client
+        .set_properties(Some(&fs_properties))
         .execute()
         .await?;
-    println!("response == {:?}\n\n", response);
+    println!(
+        "set file system properties response == {:?}",
+        set_fs_props_response
+    );
+    println!();
 
-    let response = file_system.get_properties().execute().await?;
-    println!("response == {:?}\n\n", response);
+    println!("getting file system properties...");
+    let get_fs_props_response = file_system_client.get_properties().execute().await?;
+    let properties_hashmap = get_fs_props_response.properties.hash_map();
+    let modified_by_option = properties_hashmap.get("ModifiedBy");
+    assert!(
+        modified_by_option.is_some(),
+        "did not find expected property: ModifiedBy"
+    );
+    assert_eq!(
+        modified_by_option.unwrap().to_string(),
+        "Iota",
+        "did not find expected property value for: ModifiedBy"
+    );
+    println!("found expected file system property: ModifiedBy");
+    println!();
 
-    let response = file_system.delete().execute().await?;
-    println!("response == {:?}\n\n", response);
+    println!("deleting file system...");
+    let delete_fs_response = file_system_client.delete().execute().await?;
+    println!("delete file system response == {:?}", delete_fs_response);
+    println!();
 
     Ok(())
 }
