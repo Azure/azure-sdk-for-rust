20c8f145d6cf7279ca5a169a54ad299d24c138b6
Ryan Levick <me@ryanlevick.com>
2021-10-28 14:50:34 +0200

    Delete database in the test

--- sdk/cosmos/tests/create_database_and_collection.rs
+++ sdk/cosmos/tests/create_database_and_collection.rs
@@ -17,6 +17,7 @@ async fn create_database_and_collection() -> Result<(), BoxedError> {
     let database_name = "sample";
     let context = Context::new();
 
+    // create database!
     log::info!("Creating a database with name '{}'...", database_name);
     let db = client
         .create_database(
@@ -32,7 +33,6 @@ async fn create_database_and_collection() -> Result<(), BoxedError> {
 
     // create collection!
     let db_client = client.clone().into_database_client(database_name.clone());
-
     let collection_name = "panzadoro";
     log::info!("Creating a collection with name '{}'...", collection_name);
     let collection = db_client
@@ -42,19 +42,28 @@ async fn create_database_and_collection() -> Result<(), BoxedError> {
             CreateCollectionOptions::new("/id"),
         )
         .await?;
-
     assert_eq!(collection.collection.id, collection_name);
-
     log::info!("Successfully created a collection");
     log::debug!("The create_collection response: {:#?}", collection);
 
-    let collections = Box::pin(db_client.list_collections(context, ListCollectionsOptions::new()))
-        .next()
-        .await
-        .expect("No collection page")?;
+    // list collections!
+    log::info!("Listing all collections...");
+    let collections =
+        Box::pin(db_client.list_collections(context.clone(), ListCollectionsOptions::new()))
+            .next()
+            .await
+            .expect("No collection page")?;
     assert_eq!(collections.count, 1);
     log::info!("Successfully listed collections");
     log::debug!("The list_collection response: {:#?}", collections);
+
+    // delete database
+    log::info!("Deleting the database...");
+    let deleted_database = db_client
+        .delete_database(context.clone(), DeleteDatabaseOptions::new())
+        .await?;
+    log::info!("Successfully deleted database");
+    log::debug!("The delete_database response: {:#?}", deleted_database);
 
     Ok(())
 }
