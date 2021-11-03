5e235d49a6e465cfeadb191662682d6fef9d73a0
Joshua Gendein <jgendein@microsoft.com>
2021-11-02 17:32:25 -0700

    [Identity] Aggregate errors for get_token

--- sdk/identity/src/token_credentials/default_credentials.rs
+++ sdk/identity/src/token_credentials/default_credentials.rs
@@ -2,7 +2,6 @@ use super::{
     AzureCliCredential, EnvironmentCredential, ManagedIdentityCredential, TokenCredential,
 };
 use azure_core::TokenResponse;
-use log::debug;
 
 #[derive(Debug, Default)]
 /// Provides a mechanism of selectively disabling credentials used for a `DefaultCredential` instance
@@ -67,8 +66,11 @@ pub enum DefaultCredentialError {
     EnvironmentCredentialError(#[from] super::EnvironmentCredentialError),
     #[error("Error getting managed identity credential: {0}")]
     ManagedIdentityCredentialError(#[from] super::ManagedIdentityCredentialError),
-    #[error("End of default list")]
-    EndOfDefaultList,
+    #[error(
+        "Multiple errors were encountered while attempting to authenticate:{}",
+        format_aggregate_error(.0)
+    )]
+    EndOfDefaultList(Vec<DefaultCredentialError>),
 }
 
 /// Types of TokenCredential supported by DefaultCredential
@@ -134,16 +136,17 @@ impl TokenCredential for DefaultCredential {
     type Error = DefaultCredentialError;
     /// Try to fetch a token using each of the credential sources until one succeeds
     async fn get_token(&self, resource: &str) -> Result<TokenResponse, Self::Error> {
+        let mut errors = Vec::new();
         for source in &self.sources {
             let token_res = source.get_token(resource).await;
 
             if let Ok(token) = token_res {
                 return Ok(token);
             } else {
-                debug!("Failed to get credentials: {:?}", token_res.err().unwrap());
+                errors.push(token_res.err().unwrap());
             }
         }
-        Err(DefaultCredentialError::EndOfDefaultList)
+        Err(DefaultCredentialError::EndOfDefaultList(errors))
     }
 }
 
@@ -157,4 +160,8 @@ impl azure_core::TokenCredential for DefaultCredential {
             .await
             .map_err(|error| azure_core::Error::GetTokenError(Box::new(error)))
     }
+}
+
+fn format_aggregate_error(errors: &Vec<DefaultCredentialError>) -> String {
+    errors.iter().map(|error| format!("\n{}", error)).collect()
 }
