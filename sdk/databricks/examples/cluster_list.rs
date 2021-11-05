86d00f993bb18d39562fdd84d40428845d71c67f
duysqubix <duys@qubixat.com>
2021-11-05 04:32:25 +0000

    added example to show all cluster information within given databricks instance

--- sdk/databricks/examples/cluster_list.rs
+++ sdk/databricks/examples/cluster_list.rs
@@ -1,25 +1,25 @@
-use azure_core::Body;
 use azure_databricks::client::*;
-use std::error::Error;
-use http;
-use bytes::Bytes;
+use azure_databricks::clusters::ClusterDriver;
+use azure_databricks::prelude::*;
 
 #[tokio::main]
-async fn main() -> Result<(), Box<dyn Error + Send + Sync>>{
+async fn main() -> DatabricksResult<()> {
     // to connect to databricks api, we need at least, the host and personal access token
 
     // get host_name from environment variables (includes the https://)
-    let host_name: String = std::env::var("DATABRICKS_INSTANCE").expect("Set env variable DATABRICKS_INSTANCE first!");
+    let host_name: String =
+        std::env::var("DATABRICKS_INSTANCE").expect("Set env variable DATABRICKS_INSTANCE first!");
 
     // get personal access token to databricks workspace
-    let token: String = std::env::var("DATABRICKS_TOKEN").expect("Set env variable DATABRICKS_TOKEN first!");
+    let token: String =
+        std::env::var("DATABRICKS_TOKEN").expect("Set env variable DATABRICKS_TOKEN first!");
 
-    let creds = DatabricksCredentials::BearerToken(token);
+    let client = DatabricksClient::new(&token, &host_name)?;
 
-    let client = DatabricksClient::new(creds, &host_name, None);
+    let cluster_driver = ClusterDriver::new(client.clone())?;
 
-    let request = client.prepare_request(&http::Method::GET, Body::Bytes(Bytes::from("")));
-    println!("{:#?}", request);
+    let clusters_list = cluster_driver.get_list().await?;
 
+    println!("{:#?}", clusters_list);
     Ok(())
 }
