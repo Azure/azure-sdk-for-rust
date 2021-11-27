799c0f047cd4437469c77b90698e859bdf0017e1
Cameron Taggart <cameron.taggart@gmail.com>
2021-11-27 12:20:31 -0800

    pub(crate) internal Client operations

--- services/autorust/codegen/src/codegen_operations.rs
+++ services/autorust/codegen/src/codegen_operations.rs
@@ -94,13 +94,13 @@ pub fn create_client(modules: &[String]) -> Result<TokenStream, Error> {
         }
 
         impl Client {
-            pub fn endpoint(&self) -> &str {
+            pub(crate) fn endpoint(&self) -> &str {
                 self.endpoint.as_str()
             }
-            pub fn token_credential(&self) -> &dyn azure_core::TokenCredential {
+            pub(crate) fn token_credential(&self) -> &dyn azure_core::TokenCredential {
                 self.credential.as_ref()
             }
-            pub fn scopes(&self) -> Vec<&str> {
+            pub(crate) fn scopes(&self) -> Vec<&str> {
                 self.scopes.iter().map(String::as_str).collect()
             }
             pub(crate) async fn send(&self, request: impl Into<azure_core::Request>) -> Result<azure_core::Response, azure_core::Error> {
