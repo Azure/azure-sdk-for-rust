9497deefed4cf436ce17f0388e5b5f294e6730b6
ShayMusachanov-dev <shaymusachanov@gmail.com>
2021-12-10 16:02:39 +0200

    add table sas

---
+++ sdk/storage/src/table/authorization/sas_token/options/table_account_sas_permission.rs
@@ -0,0 +1,85 @@
+use azure_core::{SasError, TableSasParsingError};
+
+use super::constants::*;
+use std::str::FromStr;
+
+/// Specifies the signed permissions for the account SAS.
+/// Permissions are only valid if they match the specified signed resource type; otherwise they are ignored.
+#[derive(Copy, Clone, Debug, PartialEq)]
+pub enum TableAccountSasPermission {
+    /// Indicates that Read is permitted.
+    Read,
+    /// Indicates that Write is permitted.
+    Write,
+    /// Indicates that Delete is permitted.
+    Delete,
+    /// Indicates that List is permitted.
+    List,
+    /// Indicates that Add is permitted.
+    Add,
+    /// Indicates that Update is permitted.
+    Update,
+}
+
+#[derive(Copy, Clone, Debug, PartialEq, Default)]
+pub struct TableAccountSasPermissions([char; 6]);
+
+impl TableAccountSasPermissions {
+    pub fn new() -> Self {
+        Default::default()
+    }
+
+    pub fn all() -> Self {
+        Self(['r', 'w', 'd', 'l', 'a', 'u'])
+    }
+
+    pub fn add_permission(mut self, permission: TableAccountSasPermission) -> Self {
+        match permission {
+            TableAccountSasPermission::Read => self.0[0] = READ,
+            TableAccountSasPermission::Write => self.0[1] = WRITE,
+            TableAccountSasPermission::Delete => self.0[2] = DELETE,
+            TableAccountSasPermission::List => self.0[3] = LIST,
+            TableAccountSasPermission::Add => self.0[4] = ADD,
+            TableAccountSasPermission::Update => self.0[5] = UPDATE,
+        };
+        self
+    }
+}
+
+impl From<TableAccountSasPermissions> for String {
+    fn from(builder: TableAccountSasPermissions) -> Self {
+        let mut permissions = String::with_capacity(builder.0.len());
+        for permission in builder.0 {
+            if permission != char::default() {
+                permissions.push(permission);
+            }
+        }
+        permissions
+    }
+}
+
+impl FromStr for TableAccountSasPermissions {
+    type Err = SasError;
+
+    fn from_str(slice: &str) -> Result<Self, Self::Err> {
+        let permissions = TableAccountSasPermissions::new();
+        for permissions_char in slice.chars() {
+            let permission = match permissions_char {
+                READ => Ok(TableAccountSasPermission::Read),
+                WRITE => Ok(TableAccountSasPermission::Write),
+                DELETE => Ok(TableAccountSasPermission::Delete),
+                LIST => Ok(TableAccountSasPermission::List),
+                ADD => Ok(TableAccountSasPermission::Add),
+                UPDATE => Ok(TableAccountSasPermission::Update),
+                _ => {
+                    return Err(SasError::SasParsingError {
+                        field: "".into(),
+                        input: "".into(),
+                    })
+                }
+            }?;
+            permissions.add_permission(permission);
+        }
+        Ok(permissions)
+    }
+}
---
+++ sdk/storage/src/table/authorization/sas_token/table_account_sas_builder.rs
@@ -0,0 +1,112 @@
+use crate::authorization::AccountCredential;
+
+use super::{
+    options::{
+        constants, table_account_sas_optional_options::TableAccountSasOptionalOptions,
+        table_account_sas_permission::TableAccountSasPermissions,
+        table_account_sas_resource_type::TableAccountSasResourceTypes,
+    },
+    table_sas_query_parameters::TableSasQueryParameters,
+};
+use azure_core::SasError;
+use chrono::{DateTime, SecondsFormat, Utc};
+use std::{ops::Add, str::FromStr};
+
+pub struct TableAccountSasBuilder {
+    /// The time at which the shared access signature becomes invalid
+    expires_on: DateTime<Utc>,
+
+    /// Gets the permissions associated with the shared access signature.
+    /// The user is restricted to operations allowed by the permissions.
+    /// This field must be omitted if it has been specified in an
+    /// associated stored access policy.
+    permissions: TableAccountSasPermissions,
+
+    /// Gets which resources are accessible via the shared access signature.
+    resource_types: TableAccountSasResourceTypes,
+}
+
+impl TableAccountSasBuilder {
+    /// * expires_on - The time at which the shared access signature becomes invalid
+    /// * permissions -
+    /// * resource_types -
+    pub fn new(
+        expires_on: DateTime<Utc>,
+        permissions: TableAccountSasPermissions,
+        resource_types: TableAccountSasResourceTypes,
+    ) -> Self {
+        TableAccountSasBuilder {
+            expires_on,
+            permissions,
+            resource_types,
+        }
+    }
+
+    pub fn new_from_raw_permissions(
+        expires_on: DateTime<Utc>,
+        raw_permissions: impl Into<String>,
+        resource_types: TableAccountSasResourceTypes,
+    ) -> Result<Self, SasError> {
+        Ok(TableAccountSasBuilder {
+            expires_on,
+            resource_types,
+            permissions: TableAccountSasPermissions::from_str(&raw_permissions.into())?,
+        })
+    }
+
+    pub fn new_from_sas_uri(sas_uri: http::Uri) -> Result<Self, SasError> {
+        todo!()
+    }
+
+    pub fn sign(
+        &self,
+        credential: &AccountCredential,
+        options: &TableAccountSasOptionalOptions,
+    ) -> Result<TableSasQueryParameters, SasError> {
+        let permissions: String = self.permissions.into();
+        let resource_types: String = self.resource_types.into();
+        let expires_on = self
+            .expires_on
+            .to_rfc3339_opts(chrono::SecondsFormat::Secs, true);
+
+        let mut parts_to_sign = vec![
+            credential.account(),
+            permissions.as_str(),
+            constants::TABLE_ACCOUNT_SERVICES_IDENTIFIER,
+            resource_types.as_str(),
+        ];
+
+        let start_on = options
+            .start_time
+            .map(|dt| dt.to_rfc3339_opts(SecondsFormat::Secs, true))
+            .unwrap_or_default();
+        parts_to_sign.push(&start_on);
+
+        parts_to_sign.push(&expires_on);
+
+        let ip: String = options.ip.map(|ip| ip.into()).unwrap_or_default();
+        parts_to_sign.push(&ip);
+
+        let protocol: String = options
+            .protocol
+            .map(|protocol| protocol.into())
+            .unwrap_or_default();
+        parts_to_sign.push(&protocol);
+
+        parts_to_sign.push(&constants::SAS_VERSION);
+
+        let to_sign = parts_to_sign.join("\n");
+        println!("{:#?}", to_sign);
+
+        Ok(TableSasQueryParameters::new(
+            constants::SAS_VERSION.into(),
+            credential.sign(to_sign),
+            expires_on,
+            permissions,
+            resource_types,
+            Some(ip),
+            Some(protocol),
+            Some(start_on),
+        ))
+    }
+}
