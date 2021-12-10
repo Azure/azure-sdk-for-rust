02811e6ee8f889241b7a280bae57c7d9f1d60fd5
Yoshua Wuyts <yoshuawuyts@gmail.com>
2021-12-10 14:09:55 +0100

    move `response_from_headers!` to the Storage SDK

--- sdk/storage/src/blob/container/responses/break_lease_response.rs
+++ sdk/storage/src/blob/container/responses/break_lease_response.rs
@@ -2,7 +2,7 @@ use azure_core::headers::*;
 use azure_core::RequestId;
 use chrono::{DateTime, Utc};
 
-response_from_headers!(BreakLeaseResponse,
+crate::response_from_headers!(BreakLeaseResponse,
                etag_from_headers => etag: String,
                last_modified_from_headers => last_modified: DateTime<Utc>,
                lease_time_from_headers => lease_time: u8,
