f1d3316e5e9495cd2f36dbbbc85153a6d29c5137
John Batty <john.batty@metaswitch.com>
2021-12-19 23:00:48 +0000

    Create azure_data_tables crate from azure_storage

---
+++ sdk/data_tables/src/prelude.rs
@@ -0,0 +1,5 @@
+pub use crate::clients::{
+    AsEntityClient, AsPartitionKeyClient, AsTableClient, AsTableServiceClient, EntityClient,
+    PartitionKeyClient, TableClient, TableServiceClient,
+};
+pub use crate::{Filter, IfMatchCondition, ReturnEntity, Select, Table, Top, Transaction};
--- sdk/storage/src/table/prelude.rs
+++
@@ -1,5 +0,0 @@
-pub use crate::table::clients::{
-    AsEntityClient, AsPartitionKeyClient, AsTableClient, AsTableServiceClient, EntityClient,
-    PartitionKeyClient, TableClient, TableServiceClient,
-};
-pub use crate::table::{Filter, IfMatchCondition, ReturnEntity, Select, Table, Top, Transaction};
