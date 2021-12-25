d5aee5b651be3a00fffd9b5232bfe40680743174
ShayMusachanov-dev <shaymusachanov@gmail.com>
2021-12-25 15:05:28 +0200

    [Data Tables] Migrate table service to pipeline architecture

--- sdk/data_tables/src/authorization/authorization_policy.rs
+++ sdk/data_tables/src/authorization/authorization_policy.rs
@@ -1,6 +1,6 @@
 use super::AuthorizationToken;
-use azure_core::{headers::Headers, Context, Policy, PolicyResult, Request};
-use http::{HeaderValue, Method, Uri};
+use azure_core::{Context, PipelineError, Policy, PolicyResult, Request, Response};
+use http::{HeaderMap, HeaderValue, Method, Uri};
 use log::trace;
 use ring::hmac;
 use std::sync::Arc;
@@ -25,13 +25,14 @@ impl Policy for AuthorizationPolicy {
         ctx: &Context,
         request: &mut Request,
         next: &[Arc<dyn Policy>],
-    ) -> PolicyResult {
+    ) -> PolicyResult<Response> {
         trace!("called AuthorizationPolicy::send. self == {:#?}", self);
 
-        assert!(
-            !next.is_empty(),
-            "Authorization policies cannot be the last policy of a pipeline"
-        );
+        if next.is_empty() {
+            return Err(Box::new(PipelineError::InvalidTailPolicy(
+                "Authorization policies cannot be the last policy of a pipeline".to_owned(),
+            )));
+        }
 
         match &self.authorization_token {
             AuthorizationToken::SASToken {} => todo!(),
@@ -46,7 +47,7 @@ impl Policy for AuthorizationPolicy {
                 );
                 request
                     .headers_mut()
-                    .insert("authorization", HeaderValue::from_str(&token)?);
+                    .append("authorization", HeaderValue::from_str(&token).unwrap());
             }
         }
 
@@ -63,7 +64,7 @@ impl Policy for AuthorizationPolicy {
 /// * canonicalized resource (for example, /devstoreaccount1/devstoreaccount1/Tables)
 /// log example:
 fn shared_key_token(
-    headers: &Headers,
+    headers: &HeaderMap,
     uri: &Uri,
     method: &Method,
     account: &str,
