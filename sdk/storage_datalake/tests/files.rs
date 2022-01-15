270c5368853400f9a983404da633b7588b5f1d2a
Robert Pack <robstar.pack@gmail.com>
2022-01-15 08:17:02 +0100

    split tests

--- sdk/storage_datalake/tests/files.rs
+++ sdk/storage_datalake/tests/files.rs
@@ -1,91 +1,10 @@
 #![cfg(feature = "mock_transport_framework")]
-
-use azure_storage_datalake::prelude::*;
-use futures::stream::StreamExt;
 use std::error::Error;
-use std::num::NonZeroU32;
 
 mod setup;
 
 #[tokio::test]
-async fn test_data_lake_file_system_functions() -> Result<(), Box<dyn Error + Send + Sync>> {
-    let data_lake_client = setup::create_data_lake_client("datalake_file_system")
-        .await
-        .unwrap();
-
-    let file_system_name = "azurerustsdk-datalake-file-system";
-    let file_system_client = data_lake_client
-        .clone()
-        .into_file_system_client(file_system_name.to_string());
-
-    let mut fs_properties = Properties::new();
-    fs_properties.insert("AddedVia", "Azure SDK for Rust");
-
-    let create_fs_response = file_system_client
-        .create()
-        .properties(fs_properties.clone())
-        .into_future()
-        .await?;
-    assert!(
-        create_fs_response.namespace_enabled,
-        "namespace should be enabled"
-    );
-
-    let mut stream = data_lake_client
-        .list_file_systems()
-        .max_results(NonZeroU32::new(3).unwrap())
-        .prefix("azurerustsdk")
-        .into_stream();
-    let mut found = false;
-    while let Some(list_fs_response) = stream.next().await {
-        for fs in list_fs_response.unwrap().file_systems {
-            if fs.name == file_system_name {
-                found = true;
-                break;
-            }
-        }
-    }
-    assert!(found, "did not find created file system");
-
-    let get_fs_props_response = file_system_client.get_properties().into_future().await?;
-    let properties_hashmap = get_fs_props_response.properties.hash_map();
-    let added_via_option = properties_hashmap.get("AddedVia");
-    assert!(
-        added_via_option.is_some(),
-        "did not find expected property: AddedVia"
-    );
-    assert_eq!(
-        added_via_option.unwrap().to_string(),
-        "Azure SDK for Rust",
-        "did not find expected property value for: AddedVia"
-    );
-
-    fs_properties.insert("ModifiedBy", "Iota");
-    file_system_client
-        .set_properties(fs_properties)
-        .into_future()
-        .await?;
-
-    let get_fs_props_response = file_system_client.get_properties().into_future().await?;
-    let properties_hashmap = get_fs_props_response.properties.hash_map();
-    let modified_by_option = properties_hashmap.get("ModifiedBy");
-    assert!(
-        modified_by_option.is_some(),
-        "did not find expected property: ModifiedBy"
-    );
-    assert_eq!(
-        modified_by_option.unwrap().to_string(),
-        "Iota",
-        "did not find expected property value for: ModifiedBy"
-    );
-
-    file_system_client.delete().into_future().await?;
-
-    Ok(())
-}
-
-#[tokio::test]
-async fn test_data_lake_file_create_delete_functions() -> Result<(), Box<dyn Error + Send + Sync>> {
+async fn file_create_delete() -> Result<(), Box<dyn Error + Send + Sync>> {
     let data_lake_client = setup::create_data_lake_client("datalake_file_create_delete")
         .await
         .unwrap();
@@ -121,7 +40,7 @@ async fn test_data_lake_file_create_delete_functions() -> Result<(), Box<dyn Err
 }
 
 #[tokio::test]
-async fn test_data_lake_file_upload_functions() -> Result<(), Box<dyn Error + Send + Sync>> {
+async fn file_upload() -> Result<(), Box<dyn Error + Send + Sync>> {
     let data_lake_client = setup::create_data_lake_client("datalake_file_upload")
         .await
         .unwrap();
@@ -158,7 +77,7 @@ async fn test_data_lake_file_upload_functions() -> Result<(), Box<dyn Error + Se
 }
 
 #[tokio::test]
-async fn test_data_lake_file_rename_functions() -> Result<(), Box<dyn Error + Send + Sync>> {
+async fn file_rename() -> Result<(), Box<dyn Error + Send + Sync>> {
     let data_lake_client = setup::create_data_lake_client("datalake_file_rename")
         .await
         .unwrap();
