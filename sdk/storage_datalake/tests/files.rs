b823f07543f1a240f16e3ff1861f60a72ca55637
Robert Pack <robstar.pack@gmail.com>
2022-01-15 09:55:36 +0100

    add test for reading file

--- sdk/storage_datalake/tests/files.rs
+++ sdk/storage_datalake/tests/files.rs
@@ -41,11 +41,11 @@ async fn file_create_delete() -> Result<(), Box<dyn Error + Send + Sync>> {
 
 #[tokio::test]
 async fn file_upload() -> Result<(), Box<dyn Error + Send + Sync>> {
-    let data_lake_client = setup::create_data_lake_client("datalake_file_upload")
+    let data_lake_client = setup::create_data_lake_client("datalake_file_read")
         .await
         .unwrap();
 
-    let file_system_name = "azurerustsdk-datalake-file-upload";
+    let file_system_name = "azurerustsdk-datalake-file-read";
     let file_system_client = data_lake_client
         .clone()
         .into_file_system_client(file_system_name.to_string());
@@ -70,6 +70,46 @@ async fn file_upload() -> Result<(), Box<dyn Error + Send + Sync>> {
         .close(true)
         .into_future()
         .await?;
+
+    file_system_client.delete().into_future().await?;
+
+    Ok(())
+}
+
+#[tokio::test]
+async fn file_read() -> Result<(), Box<dyn Error + Send + Sync>> {
+    let data_lake_client = setup::create_data_lake_client("datalake_file_upload")
+        .await
+        .unwrap();
+
+    let file_system_name = "azurerustsdk-datalake-file-upload";
+    let file_system_client = data_lake_client
+        .clone()
+        .into_file_system_client(file_system_name.to_string());
+
+    let create_fs_response = file_system_client.create().into_future().await?;
+    assert!(
+        create_fs_response.namespace_enabled,
+        "namespace should be enabled"
+    );
+
+    let file_path = "some/path/e2etest-file.txt";
+    let file_client = file_system_client.get_file_client(file_path);
+
+    file_client.create().into_future().await?;
+
+    let bytes = bytes::Bytes::from("some data");
+    let file_length = bytes.len() as i64;
+    file_client.append(0, bytes.clone()).into_future().await?;
+
+    file_client
+        .flush(file_length)
+        .close(true)
+        .into_future()
+        .await?;
+
+    let read_file_response = file_client.read().into_future().await?;
+    assert_eq!(bytes, read_file_response.data);
 
     file_system_client.delete().into_future().await?;
 
