afd95a5eead9eaf130c471161aa464ef12f1840e
Cameron Taggart <cameron.taggart@gmail.com>
2022-01-25 07:55:19 -0500

    cargo publish

--- services/autorust/codegen/examples/list_crates.rs
+++ services/autorust/codegen/examples/list_crates.rs
@@ -5,20 +5,33 @@ use camino::Utf8Path;
 pub type Result<T> = std::result::Result<T, Box<dyn std::error::Error>>;
 
 fn main() -> Result<()> {
-    let paths = fs::read_dir("../svc")?;
+    let mut names = Vec::new();
+    let paths = fs::read_dir("../mgmt")?;
     for path in paths {
         match path {
             Ok(path) => {
-                // path.metadata().
                 match Utf8Path::from_path(&path.path()){
                     Some(path) => {
-                        println!("path {}", path);
+                        if path.is_dir() {
+                            match path.file_name() {
+                                Some(file_name) => {
+                                    // println!("path {}", file_name);
+                                    names.push(file_name.to_string())
+                                },
+                                None => (),
+                            }
+                        }
                     },
                     None => (),
                 }
             },
             Err(_) => todo!(),
         }
+    }
+
+    names.sort();
+    for name in names {
+        println!("cargo publish -p azure_mgmt_{}", name);
     }
 
     Ok(())
