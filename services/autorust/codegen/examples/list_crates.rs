0f66e67ea8cb672b8e54a045c3c07b0fb8356625
Cameron Taggart <cameron.taggart@gmail.com>
2022-01-29 03:51:33 -0500

    list missing crates

--- services/autorust/codegen/examples/list_crates.rs
+++ services/autorust/codegen/examples/list_crates.rs
@@ -1,13 +1,20 @@
 // cargo run --example list_crates
-
 // This list all crates that are generated.
+
+// cargo run --example list_crates -- 0.1.0
 // If a version is passed in as an option, it will filter out any that
 // are already published to crates.io.
 // An updated clone of crates.io-index is required.
 // git clone https://github.com/rust-lang/crates.io-index
 
 use camino::{Utf8Path, Utf8PathBuf};
-use std::fs;
+use serde::Deserialize;
+use std::io::BufRead;
+use std::{
+    fs::{self, File},
+    io::BufReader,
+    str::FromStr,
+};
 pub type Result<T> = std::result::Result<T, Box<dyn std::error::Error>>;
 
 /// Get all directories below the given directory.
@@ -30,9 +37,7 @@ fn get_dirs(dir: impl AsRef<Utf8Path>) -> Result<Vec<Utf8PathBuf>> {
     Ok(dirs)
 }
 
-fn main() -> Result<()> {
-    let version = std::env::args().nth(1);
-
+fn list_crate_names() -> Result<Vec<String>> {
     let mut names: Vec<_> = get_dirs("../mgmt")?
         .into_iter()
         .filter_map(|d| d.file_name().map(|d| format!("azure_mgmt_{}", d)))
@@ -42,11 +47,56 @@ fn main() -> Result<()> {
             .into_iter()
             .filter_map(|d| d.file_name().map(|d| format!("azure_svc_{}", d))),
     );
-
     names.sort();
-    for name in names {
-        println!("{}", name);
+    Ok(names)
+}
+
+fn main() -> Result<()> {
+    let version = std::env::args().nth(1);
+
+    let names = list_crate_names()?;
+    match &version {
+        Some(version) => {
+            for name in names.iter() {
+                if !has_version(name, version)? {
+                    println!("{}", name);
+                    // println!("cargo publish -p {}", name);
+                    // println!("Start-Sleep -Seconds 420");
+                    // println!("cargo owner --add github:Azure:azure-sdk-publish-rust -- {}", name);
+                }
+            }
+        }
+        None => {
+            for (i, name) in names.iter().enumerate() {
+                println!("{} {}", i + 1, name);
+            }
+        }
     }
 
     Ok(())
+}
+
+fn has_version(name: &str, version: &str) -> Result<bool> {
+    Ok(get_versions(name)?.iter().any(|v| v.vers.as_str() == version))
+}
+
+fn get_versions(name: &str) -> Result<Vec<CrateVersion>> {
+    let path = format!("../../../crates.io-index/az/ur/{}", name);
+    let path = Utf8PathBuf::from_str(&path)?;
+    let mut versions = Vec::new();
+    if path.exists() {
+        let file = File::open(path)?;
+        let reader = BufReader::new(file);
+        for line in reader.lines() {
+            let version: CrateVersion = serde_json::from_str(&line?)?;
+            versions.push(version);
+        }
+    }
+    Ok(versions)
+}
+
+#[derive(Debug, Deserialize)]
+pub struct CrateVersion {
+    pub name: String,
+    pub vers: String,
 }
