935abe3596a2415d0d6a2c156b9422a47ef83790
Antonio Jimenez <anjimene@microsoft.com>
2022-05-06 14:59:01 -0700

    style

--- sdk/identity/src/token_credentials/default_credentials.rs
+++ sdk/identity/src/token_credentials/default_credentials.rs
@@ -142,7 +142,9 @@ impl Default for DefaultAzureCredential {
         DefaultAzureCredential {
             sources: vec![
                 DefaultAzureCredentialEnum::Environment(EnvironmentCredential::default()),
-                DefaultAzureCredentialEnum::ManagedIdentity(ImdsManagedIdentityCredential::default()),
+                DefaultAzureCredentialEnum::ManagedIdentity(
+                    ImdsManagedIdentityCredential::default(),
+                ),
                 DefaultAzureCredentialEnum::AzureCli(AzureCliCredential {}),
             ],
         }
