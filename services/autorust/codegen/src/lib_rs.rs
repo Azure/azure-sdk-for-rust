a031c1e74b503922928a2cf235e06c2b575e514c
Cameron Taggart <cameron.taggart@gmail.com>
2022-05-18 18:12:05 -0400

    other misc

--- services/autorust/codegen/src/lib_rs.rs
+++ services/autorust/codegen/src/lib_rs.rs
@@ -1,7 +1,7 @@
-use crate::{codegen::create_generated_by_header, config_parser::Tag, identifier::ident, write_file};
+use crate::{codegen::create_generated_by_header, config_parser::Tag, identifier::parse_ident, write_file};
 use camino::Utf8Path;
-use proc_macro2::TokenStream;
-use quote::quote;
+use proc_macro2::{Ident, TokenStream};
+use quote::{quote, ToTokens};
 
 pub type Result<T, E = Error> = std::result::Result<T, E>;
 
@@ -14,31 +14,54 @@ pub enum Error {
 }
 
 pub fn create(tags: &[&Tag], path: &Utf8Path, print_writing_file: bool) -> Result<()> {
-    Ok(write_file(path, &create_body(tags)?, print_writing_file)?)
+    Ok(write_file(path, &create_body(tags)?.into_token_stream(), print_writing_file)?)
 }
 
-fn create_body(tags: &[&Tag]) -> Result<TokenStream> {
-    let mut cfgs = TokenStream::new();
-    for tag in tags {
-        let feature_name = tag.rust_feature_name();
-        let mod_name = ident(&tag.rust_mod_name()).map_err(|source| Error::ModName {
-            source,
-            feature: feature_name.to_owned(),
-        })?;
-        cfgs.extend(quote! {
-            #[cfg(feature = #feature_name)]
-            pub mod #mod_name;
-            #[cfg(all(feature = #feature_name, not(feature = "no-default-tag")))]
-            pub use #mod_name::{models, operations, operations::Client, operations::ClientBuilder, operations::Error};
-        });
+struct Feature {
+    pub feature_name: String,
+    pub mod_name: Ident,
+}
+
+struct BodyCode {
+    pub features: Vec<Feature>,
+}
+
+fn create_body(tags: &[&Tag]) -> Result<BodyCode> {
+    let features: Vec<Feature> = tags
+        .iter()
+        .map(|tag| {
+            let feature_name = tag.rust_feature_name();
+            let mod_name = parse_ident(&tag.rust_mod_name()).map_err(|source| Error::ModName {
+                source,
+                feature: feature_name.to_owned(),
+            })?;
+            Ok(Feature { feature_name, mod_name })
+        })
+        .collect::<Result<_>>()?;
+    Ok(BodyCode { features })
+}
+
+impl ToTokens for BodyCode {
+    fn to_tokens(&self, tokens: &mut TokenStream) {
+        let mut cfgs = TokenStream::new();
+        for feature in &self.features {
+            let feature_name = &feature.feature_name;
+            let mod_name = &feature.mod_name;
+            cfgs.extend(quote! {
+                #[cfg(feature = #feature_name)]
+                pub mod #mod_name;
+                #[cfg(all(feature = #feature_name, not(feature = "no-default-tag")))]
+                pub use #mod_name::{models, operations, operations::Client, operations::ClientBuilder, operations::Error};
+            });
+        }
+        let generated_by = create_generated_by_header();
+        tokens.extend(quote! {
+            #![allow(clippy::module_inception)]
+            #![allow(clippy::too_many_arguments)]
+            #![allow(clippy::ptr_arg)]
+            #![allow(clippy::large_enum_variant)]
+            #generated_by
+            #cfgs
+        })
     }
-    let generated_by = create_generated_by_header();
-    Ok(quote! {
-        #![allow(clippy::module_inception)]
-        #![allow(clippy::too_many_arguments)]
-        #![allow(clippy::ptr_arg)]
-        #![allow(clippy::large_enum_variant)]
-        #generated_by
-        #cfgs
-    })
 }
