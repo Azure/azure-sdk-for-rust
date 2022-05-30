98557c9082d6529b8ae2cdfe0fb6bb76860110b8
Cameron Taggart <cameron.taggart@gmail.com>
2022-05-30 15:29:03 -0400

    switch HttpError.body & http_response_from_body to Bytes

--- sdk/core/src/error/http_error.rs
+++ sdk/core/src/error/http_error.rs
@@ -1,4 +1,5 @@
 use crate::Response;
+use bytes::Bytes;
 use std::collections::HashMap;
 
 /// An unsuccessful HTTP response
@@ -7,13 +8,13 @@ pub struct HttpError {
     status: u16,
     error_code: Option<String>,
     headers: std::collections::HashMap<String, String>,
-    body: String,
+    body: Bytes,
 }
 
 impl HttpError {
     /// Create an error from an http response.
     ///
-    /// This does not check whether the response was a success and should only be used with unsuccessul responses.
+    /// This does not check whether the response was a success and should only be used with unsuccessful responses.
     pub async fn new(response: Response) -> Self {
         let status = response.status();
         let mut error_code = get_error_code_from_header(&response);
@@ -24,7 +25,7 @@ impl HttpError {
             headers.insert(name.to_string(), value);
         }
 
-        let body = response.into_body_string().await;
+        let body = response.into_body().await;
         error_code = error_code.or_else(|| get_error_code_from_body(&body));
         HttpError {
             status: status.as_u16(),
@@ -55,7 +56,7 @@ impl std::fmt::Display for HttpError {
             self.error_code.as_deref().unwrap_or("unknown")
         )?;
         // TODO: sanitize body
-        writeln!(f, "\tBody: \"{}\"", self.body)?;
+        writeln!(f, "\tBody: \"{:?}\"", self.body)?;
         writeln!(f, "\tHeaders:")?;
         // TODO: sanitize headers
         for (k, v) in &self.headers {
@@ -85,9 +86,9 @@ fn get_error_code_from_header(response: &Response) -> Option<String> {
 /// Gets the error code if it's present in the body
 ///
 /// For more info, see [here](https://github.com/microsoft/api-guidelines/blob/vNext/azure/Guidelines.md#handling-errors)
-pub(crate) fn get_error_code_from_body(body: &str) -> Option<String> {
+pub(crate) fn get_error_code_from_body(body: &Bytes) -> Option<String> {
     Some(
-        serde_json::from_str::<serde_json::Value>(body)
+        serde_json::from_slice::<serde_json::Value>(body)
             .ok()?
             .get("error")?
             .get("code")?
