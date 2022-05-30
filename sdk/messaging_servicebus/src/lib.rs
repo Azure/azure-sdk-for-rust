eadfd805a06188e3e4530d0108a52b3d95175703
Dan Chiarlone <dchiarlone@microsoft.com>
2022-05-30 09:27:12 -0700

    removed extern crate declaration
    Signed-off-by: Dan Chiarlone <dchiarlone@microsoft.com>

--- sdk/messaging_servicebus/src/lib.rs
+++ sdk/messaging_servicebus/src/lib.rs
@@ -1,7 +1,5 @@
 //! Azure service bus crate for the unofficial Microsoft Azure SDK for Rust. This crate is part of a collection of crates: for more information please refer to [https://github.com/azure/azure-sdk-for-rust](https://github.com/azure/azure-sdk-for-rust).
 #![recursion_limit = "128"]
 
-extern crate log;
-
 pub mod prelude;
 pub mod service_bus;
