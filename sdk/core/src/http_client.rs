34997a310c6b1f5ce6591ffeab73004fabf8551d
Cameron Taggart <cameron.taggart@gmail.com>
2022-05-31 19:03:34 -0400

    able to upgrade execute_request2

--- sdk/core/src/http_client.rs
+++ sdk/core/src/http_client.rs
@@ -34,15 +34,7 @@ pub trait HttpClient: Send + Sync + std::fmt::Debug {
     /// responsibility of another policy (not the transport one). It does not consume the request.
     /// Implementors are expected to clone the necessary parts of the request and pass them to the
     /// underlying transport.
-    async fn execute_request2(
-        &self,
-        request: &crate::Request,
-    ) -> Result<crate::Response, HttpError>;
-
-    /// Send out a request using `azure_core`'s types.
-    ///
-    /// Same as `execute_request2`, but using the new error types.
-    async fn execute_request3(&self, request: &crate::Request) -> Result<crate::Response, Error>;
+    async fn execute_request2(&self, request: &crate::Request) -> Result<crate::Response, Error>;
 
     /// Send out a request and validate it was in the `2xx` range, using
     /// `hyperium/http`'s types.
@@ -105,57 +97,7 @@ impl HttpClient for reqwest::Client {
         Ok(response)
     }
 
-    async fn execute_request2(
-        &self,
-        request: &crate::Request,
-    ) -> Result<crate::Response, HttpError> {
-        let url = url::Url::parse(&request.uri().to_string())?;
-        let mut reqwest_request = self.request(request.method(), url);
-        for (name, value) in request.headers().iter() {
-            reqwest_request = reqwest_request.header(name, value);
-        }
-
-        // We clone the body since we need to give ownership of it to Reqwest.
-        let body = request.body().clone();
-
-        let reqwest_request = match body {
-            Body::Bytes(bytes) => reqwest_request
-                .body(bytes)
-                .build()
-                .map_err(|error| HttpError::BuildClientRequest(error.into()))?,
-            Body::SeekableStream(mut seekable_stream) => {
-                seekable_stream.reset().await.unwrap(); // TODO: remove unwrap when `HttpError` has been removed
-
-                reqwest_request
-                    .body(reqwest::Body::wrap_stream(seekable_stream))
-                    .build()
-                    .map_err(|error| HttpError::BuildClientRequest(error.into()))?
-            }
-        };
-
-        let reqwest_response = self
-            .execute(reqwest_request)
-            .await
-            .map_err(|error| HttpError::ExecuteRequest(error.into()))?;
-        let mut response = crate::ResponseBuilder::new(reqwest_response.status());
-
-        for (key, value) in reqwest_response.headers() {
-            response.with_header(key, value.clone());
-        }
-
-        let response =
-            response.with_pinned_stream(Box::pin(reqwest_response.bytes_stream().map_err(|e| {
-                Error::full(
-                    ErrorKind::Io,
-                    e,
-                    "error converting `reqwest` request into a byte stream",
-                )
-            })));
-
-        Ok(response)
-    }
-
-    async fn execute_request3(&self, request: &crate::Request) -> Result<crate::Response, Error> {
+    async fn execute_request2(&self, request: &crate::Request) -> Result<crate::Response, Error> {
         let url = url::Url::parse(&request.uri().to_string())?;
         let mut reqwest_request = self.request(request.method(), url);
         for (name, value) in request.headers().iter() {
