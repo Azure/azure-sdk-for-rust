9aa054b6f6d38faca4e34d0ec0acde653479030a
Robert Pack <robstar.pack@gmail.com>
2022-06-10 21:43:50 +0200

    clippy

--- sdk/storage_datalake/src/util.rs
+++ sdk/storage_datalake/src/util.rs
@@ -2,11 +2,11 @@ use azure_core::error::{Error, ErrorKind, Result, ResultExt};
 use http::HeaderMap;
 
 pub(crate) fn namespace_enabled_from_headers(headers: &HeaderMap) -> Result<bool> {
-    Ok(headers
+    headers
         .get("x-ms-namespace-enabled")
         .ok_or_else(|| Error::message(ErrorKind::Other, "Header x-ms-namespace-enabled not found"))?
         .to_str()
         .map_kind(ErrorKind::DataConversion)?
         .parse()
-        .map_kind(ErrorKind::DataConversion)?)
+        .map_kind(ErrorKind::DataConversion)
 }
