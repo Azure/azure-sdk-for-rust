8159e7f4c138633e9e6174a7e04eb218beaa9e81
Cameron Taggart <cameron.taggart@gmail.com>
2022-06-17 21:29:20 -0400

    remove execute_request_check_status from HttpClient

--- sdk/core/src/http_client.rs
+++ sdk/core/src/http_client.rs
@@ -6,7 +6,6 @@ use async_trait::async_trait;
 use bytes::Bytes;
 #[allow(unused_imports)]
 use futures::TryStreamExt;
-use http::{Request, Response, StatusCode};
 use serde::Serialize;
 
 /// Construct a new `HttpClient` with the `reqwest` backend.
@@ -20,11 +19,6 @@ pub fn new_http_client() -> std::sync::Arc<dyn HttpClient> {
 #[cfg_attr(target_arch = "wasm32", async_trait(?Send))]
 #[cfg_attr(not(target_arch = "wasm32"), async_trait)]
 pub trait HttpClient: Send + Sync + std::fmt::Debug {
-    /// Send out a request using `hyperium/http`'s types.
-    ///
-    /// This method is considered deprecated and should not be used in new code.
-    async fn execute_request(&self, request: Request<Bytes>) -> crate::Result<Response<Bytes>>;
-
     /// Send out a request using `azure_core`'s types.
     ///
     /// This function will be the only one remaining in the trait as soon as the trait stabilizes.
@@ -34,68 +28,12 @@ pub trait HttpClient: Send + Sync + std::fmt::Debug {
     /// Implementors are expected to clone the necessary parts of the request and pass them to the
     /// underlying transport.
     async fn execute_request2(&self, request: &crate::Request) -> crate::Result<crate::Response>;
-
-    /// Send out a request and validate it was in the `2xx` range, using
-    /// `hyperium/http`'s types.
-    ///
-    /// Note: the `expected_status` parameter is never used, and instead we
-    /// always validate the status was in the `2xx` range. This method should
-    /// be considered deprecated, and should not be used in new code.
-    async fn execute_request_check_status(
-        &self,
-        request: Request<Bytes>,
-        _expected_status: StatusCode,
-    ) -> crate::Result<Response<Bytes>> {
-        let response = self.execute_request(request).await?;
-        let status = response.status();
-        if (200..400).contains(&status.as_u16()) {
-            Ok(response)
-        } else {
-            let body = response.into_body();
-            Err(ErrorKind::http_response_from_body(status.into(), &body).into())
-        }
-    }
 }
 
 #[cfg(any(feature = "enable_reqwest", feature = "enable_reqwest_rustls"))]
 #[cfg(not(target_arch = "wasm32"))]
 #[async_trait]
 impl HttpClient for reqwest::Client {
-    async fn execute_request(&self, request: Request<Bytes>) -> crate::Result<Response<Bytes>> {
-        let url = url::Url::parse(&request.uri().to_string())?;
-        let mut reqwest_request = self.request(request.method().clone(), url);
-        for (header, value) in request.headers() {
-            reqwest_request = reqwest_request.header(header, value);
-        }
-
-        let reqwest_request = reqwest_request
-            .body(request.into_body())
-            .build()
-            .context(ErrorKind::DataConversion, "failed to build request")?;
-
-        let reqwest_response = self
-            .execute(reqwest_request)
-            .await
-            .context(ErrorKind::Io, "failed to execute request")?;
-
-        let mut response = Response::builder().status(reqwest_response.status());
-
-        for (key, value) in reqwest_response.headers() {
-            response = response.header(key, value);
-        }
-
-        let response = response
-            .body(
-                reqwest_response
-                    .bytes()
-                    .await
-                    .context(ErrorKind::Io, "failed to read response bytes")?,
-            )
-            .context(ErrorKind::DataConversion, "failed to build response")?;
-
-        Ok(response)
-    }
-
     async fn execute_request2(&self, request: &crate::Request) -> crate::Result<crate::Response> {
         let url = request.url().clone();
         let mut reqwest_request = self.request(request.method(), url);
