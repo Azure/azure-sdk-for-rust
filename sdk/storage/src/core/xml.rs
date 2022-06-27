3dee4c2bb9e43891495d423ecfbfe81e5e264559
Brian Caswell <bcaswell@microsoft.com>
2022-06-27 10:09:22 -0400

    fix clippy

--- sdk/storage/src/core/xml.rs
+++ sdk/storage/src/core/xml.rs
@@ -4,7 +4,7 @@ const UTF8_BOM: [u8; 3] = [0xEF, 0xBB, 0xBF];
 
 /// Reads the XML from the Bytes.
 pub fn read_xml<'de, T: serde::de::Deserialize<'de>>(body: &[u8]) -> Result<T, Error> {
-    serde_xml_rs::from_reader(slice_bom(body).as_ref())
+    serde_xml_rs::from_reader(slice_bom(body))
         .context(ErrorKind::DataConversion, "failed to deserialize xml")
 }
 
