9167ab5f64c145aa9ddc6ea83e358f60b5879e6e
Aaron Hardin <teslasprotege@gmail.com>
2022-06-28 15:38:58 -0500

    [Feature] Add support for setting blob properties (#869)

--- sdk/storage_blobs/src/blob_content_md5.rs
+++ sdk/storage_blobs/src/blob_content_md5.rs
@@ -1,4 +1,5 @@
 use azure_core::headers::{self, Header};
+use azure_storage::ConsistencyMD5;
 
 #[derive(Debug, Clone, PartialEq, PartialOrd, Eq, Ord)]
 pub struct BlobContentMD5([u8; 16]);
@@ -6,6 +7,12 @@ pub struct BlobContentMD5([u8; 16]);
 impl From<md5::Digest> for BlobContentMD5 {
     fn from(md5: md5::Digest) -> Self {
         BlobContentMD5(md5.0)
+    }
+}
+
+impl From<ConsistencyMD5> for BlobContentMD5 {
+    fn from(md5: ConsistencyMD5) -> Self {
+        BlobContentMD5(*md5.as_slice())
     }
 }
 
