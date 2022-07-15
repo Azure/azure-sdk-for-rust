d19c1d47fa4e8065bd54b7c949ec01dc95cc0cff
Ryan Levick <me@ryanlevick.com>
2022-07-15 14:14:58 +0200

    Improve the error on failed xml deserialization

--- sdk/storage/src/core/xml.rs
+++ sdk/storage/src/core/xml.rs
@@ -5,8 +5,11 @@ const UTF8_BOM: [u8; 3] = [0xEF, 0xBB, 0xBF];
 
 /// Reads the XML from bytes.
 pub fn read_xml<'de, T: serde::de::Deserialize<'de>>(body: &[u8]) -> azure_core::Result<T> {
-    serde_xml_rs::from_reader(slice_bom(body))
-        .context(ErrorKind::DataConversion, "failed to deserialize xml")
+    serde_xml_rs::from_reader(slice_bom(body)).with_context(ErrorKind::DataConversion, || {
+        let t = core::any::type_name::<T>();
+        let xml = std::str::from_utf8(body).unwrap_or("<XML IS NOT UTF-8>");
+        format!("failed to deserialize the following xml into a {t}\n{xml}")
+    })
 }
 
 /// Returns bytes without the UTF-8 BOM.
@@ -29,5 +32,22 @@ mod test {
 
         let bytes = &[8];
         assert_eq!(&[8], slice_bom(bytes));
+    }
+
+    #[test]
+    fn reading_xml() {
+        #[derive(Deserialize, PartialEq, Debug)]
+        #[serde(rename = "Foo")]
+        struct Test {
+            x: String,
+        }
+        let test = Test {
+            x: "Hello, world!".into(),
+        };
+        let xml = br#"<?xml version="1.0" encoding="utf-8"?><Foo><x>Hello, world!</x></Foo>"#;
+        assert_eq!(test, read_xml(xml).unwrap());
+
+        let error = read_xml::<Test>(&xml[..xml.len() - 2]).unwrap_err();
+        assert!(format!("{error}").contains("reading_xml::Test"));
     }
 }
