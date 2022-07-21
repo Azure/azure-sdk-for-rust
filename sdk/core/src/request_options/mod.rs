85fbeedfb6c91135553a27e9430f7c6847c650a3
Brian Caswell <bcaswell@microsoft.com>
2022-07-21 12:17:07 -0400

    add documentation from ACCEPT

--- sdk/core/src/request_options/mod.rs
+++ sdk/core/src/request_options/mod.rs
@@ -67,7 +67,21 @@ create_request_header_cow!(
     "The Content Type indicates the media type of the request body",
     (APPLICATION_JSON, "application/json")
 );
-create_request_header_cow!(Accept, ACCEPT,);
+create_request_header_cow!(
+    Accept,
+    ACCEPT,
+    "Advertises which content types the client is able to understand.
+
+The Accept request HTTP header advertises which content types, expressed
+as MIME types, the client is able to understand. Using content
+negotiation, the server then selects one of the proposals, uses it and
+informs the client of its choice with the Content-Type response header.
+Browsers set adequate values for this header depending of the context
+where the request is done: when fetching a CSS stylesheet a different
+value is set for the request than when fetching an image, video or a
+script.
+",
+);
 create_request_header_cow!(ActivityId, ACTIVITY_ID,);
 create_request_header_cow!(App, APP,);
 create_request_header_cow!(ClientRequestId, CLIENT_REQUEST_ID,);
