a4d175980dcd44a5ee38e6ae75e3e132e1c077cf
Brian Caswell <bcaswell@microsoft.com>
2022-07-21 13:32:38 -0400

    address feedback

--- sdk/core/src/request_options/if_sequence_number.rs
+++ sdk/core/src/request_options/if_sequence_number.rs
@@ -1,9 +1,22 @@
 use crate::{headers, Header};
 
+/// Conditional request header based on the value of the object's sequence number
+///
+/// Ref: <https://docs.microsoft.com/en-us/rest/api/storageservices/put-page-from-url>
 #[derive(Debug, Clone, Copy, PartialEq)]
 pub enum IfSequenceNumber {
-    Less(u64),
+    /// If the object's sequence number is less than the specified value, the
+    /// request proceeds; otherwise it fails with SequenceNumberConditionNotMet
+    /// error (HTTP status code 412 – Precondition Failed).
+    LessThan(u64),
+    /// If the object's sequence number is less than or equal to the specified
+    /// value, the request proceeds; otherwise it fails with the
+    /// SequenceNumberConditionNotMet error (HTTP status code 412 – Precondition
+    /// Failed).
     LessOrEqual(u64),
+    /// If the object’s sequence number is equal to the specified value, the
+    /// request proceeds; otherwise it fails with SequenceNumberConditionNotMet
+    /// error (HTTP status code 412 – Precondition Failed).
     Equal(u64),
 }
 
@@ -12,7 +25,7 @@ impl Header for IfSequenceNumber {
         match self {
             IfSequenceNumber::Equal(_) => headers::IF_SEQUENCE_NUMBER_EQ,
             IfSequenceNumber::LessOrEqual(_) => headers::IF_SEQUENCE_NUMBER_LE,
-            IfSequenceNumber::Less(_) => headers::IF_SEQUENCE_NUMBER_LT,
+            IfSequenceNumber::LessThan(_) => headers::IF_SEQUENCE_NUMBER_LT,
         }
     }
 
@@ -20,7 +33,7 @@ impl Header for IfSequenceNumber {
         match self {
             IfSequenceNumber::Equal(val)
             | IfSequenceNumber::LessOrEqual(val)
-            | IfSequenceNumber::Less(val) => val.to_string().into(),
+            | IfSequenceNumber::LessThan(val) => val.to_string().into(),
         }
     }
 }
