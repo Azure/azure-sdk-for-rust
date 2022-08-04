a7d30822484a9139bccf2da1edd0a9bd2282a943
Cameron Taggart <cameron.taggart@gmail.com>
2022-08-04 10:14:32 -0400

    update autorust to support operation docs port of https://github.com/microsoft/azure-devops-rust-api/pull/10 by @johnbatty

--- services/autorust/codegen/src/spec.rs
+++ services/autorust/codegen/src/spec.rs
@@ -277,6 +277,7 @@ impl Spec {
                         responses: op.responses,
                         examples: op.examples,
                         summary: op.summary,
+                        description: op.description,
                         api_version: self.doc(&op.doc_file)?.version()?.to_owned(),
                         pageable: op.pageable,
                         long_running_operation: op.long_running_operation,
@@ -409,6 +410,7 @@ struct WebOperationUnresolved {
     pub responses: IndexMap<StatusCode, Response>,
     pub examples: MsExamples,
     pub summary: Option<String>,
+    pub description: Option<String>,
     pub pageable: Option<MsPageable>,
     pub long_running_operation: bool,
     pub consumes: Vec<String>,
@@ -424,6 +426,7 @@ pub struct WebOperation {
     pub responses: IndexMap<StatusCode, Response>,
     pub examples: MsExamples,
     pub summary: Option<String>,
+    pub description: Option<String>,
     pub api_version: String,
     pub pageable: Option<MsPageable>,
     pub long_running_operation: bool,
@@ -441,6 +444,7 @@ impl Default for WebOperation {
             responses: Default::default(),
             examples: Default::default(),
             summary: Default::default(),
+            description: Default::default(),
             api_version: Default::default(),
             pageable: Default::default(),
             long_running_operation: Default::default(),
@@ -471,6 +475,10 @@ impl WebParameter {
 
     pub fn type_(&self) -> &ParameterType {
         &self.0.in_
+    }
+
+    pub fn description(&self) -> &Option<String> {
+        &self.0.common.description
     }
 
     pub fn data_type(&self) -> &Option<DataType> {
@@ -601,6 +609,7 @@ fn path_operations_unresolved(doc_file: impl AsRef<Utf8Path>, path: &str, item: 
                 responses: op.responses.clone(),
                 examples: op.x_ms_examples.clone(),
                 summary: op.summary.clone(),
+                description: op.description.clone(),
                 pageable: op.x_ms_pageable.clone(),
                 long_running_operation: op.x_ms_long_running_operation.unwrap_or(false),
                 consumes: op.consumes.clone(),
