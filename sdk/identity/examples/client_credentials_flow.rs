df648a8ccd12e576b984363174964cfbffa68cdf
Cameron Taggart <cameron.taggart@gmail.com>
2022-08-10 12:55:10 -0400

    make oauth2 an internal dependency of identity

--- sdk/identity/examples/client_credentials_flow.rs
+++ sdk/identity/examples/client_credentials_flow.rs
@@ -1,5 +1,4 @@
 use azure_identity::client_credentials_flow;
-use oauth2::{ClientId, ClientSecret};
 use url::Url;
 
 use std::env;
@@ -7,11 +6,9 @@ use std::error::Error;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn Error>> {
-    let client_id =
-        ClientId::new(env::var("CLIENT_ID").expect("Missing CLIENT_ID environment variable."));
-    let client_secret = ClientSecret::new(
-        env::var("CLIENT_SECRET").expect("Missing CLIENT_SECRET environment variable."),
-    );
+    let client_id = env::var("CLIENT_ID").expect("Missing CLIENT_ID environment variable.");
+    let client_secret =
+        env::var("CLIENT_SECRET").expect("Missing CLIENT_SECRET environment variable.");
     let tenant_id = env::var("TENANT_ID").expect("Missing TENANT_ID environment variable.");
     let subscription_id =
         env::var("SUBSCRIPTION_ID").expect("Missing SUBSCRIPTION_ID environment variable.");
