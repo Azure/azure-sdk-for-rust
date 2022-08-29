71c932ad080ea82272cad253f4b35a0a25675299
Cameron Taggart <cameron.taggart@gmail.com>
2022-08-29 11:34:50 -0400

    back to the info_future

--- services/autorust/codegen/src/codegen_operations.rs
+++ services/autorust/codegen/src/codegen_operations.rs
@@ -701,7 +701,7 @@ impl ToTokens for ResponseCode {
     }
 }
 
-/// The `send` function of the request builder.
+/// The `into_future` function of the request builder.
 struct RequestBuilderIntoFutureCode {
     new_request_code: NewRequestCode,
     request_builder: SetRequestCode,
@@ -768,7 +768,7 @@ impl ToTokens for RequestBuilderIntoFutureCode {
 
         let send_future = quote! {
             #[doc = "Send the request and returns the response."]
-            pub fn send(self) -> futures::future::BoxFuture<'static, azure_core::Result<Response>> {
+            pub fn into_future(self) -> futures::future::BoxFuture<'static, azure_core::Result<Response>> {
                 Box::pin({
                     let this = self.clone();
                     async move {
