f8002519ae7e41336ca524178f1a568603ee2d30
minghuaw <michael.wu1107@gmail.com>
2022-09-02 20:00:58 -0700

    naive impl of ServiceBusRetryPolicy

--- sdk/messaging_servicebus/src/core/transport_connection_scope.rs
+++ sdk/messaging_servicebus/src/core/transport_connection_scope.rs
@@ -1,1 +1,23 @@
-pub trait TransportConnectionScope {}
+use std::time::Duration;
+
+pub trait TransportConnectionScope {
+    /// <summary>
+    ///   Indicates whether this <see cref="TransportConnectionScope"/> has been disposed.
+    /// </summary>
+    ///
+    /// <value><c>true</c> if disposed; otherwise, <c>false</c>.</value>
+    ///
+    fn is_disposed(&self) -> bool;
+
+    /// <summary>
+    ///   The recommended timeout to associate with the session.
+    /// </summary>
+    fn session_timeout(&self) -> Duration {
+        Duration::from_secs(30)
+    }
+
+    /// <summary>
+    /// Disposes of the connection scope.
+    /// </summary>
+    fn dispose(&mut self);
+}
--- sdk/messaging_servicebus/src/primitives/service_bus_retry_policy.rs
+++ sdk/messaging_servicebus/src/primitives/service_bus_retry_policy.rs
@@ -5,14 +5,19 @@ use crate::diagnostics::ServiceBusEventSource;
 
 pub static SERVER_BUSY_BASE_SLEEP_TIME: Duration = Duration::from_secs(10);
 
+pub enum RetryError<E> {
+    ServiceBusy,
+    Operation(E),
+}
+
 /// An abstract representation of a policy to govern retrying of messaging operations.
 ///
 /// It is recommended that developers without advanced needs not implement custom retry
 /// policies but instead configure the default policy by specifying the desired set of
 /// retry options when creating one of the Service Bus clients.
 pub trait ServiceBusRetryPolicy: Eq + Hash + ToString {
-    type Ok;
-    type Error;
+    type Ok: Send + Sync;
+    type Error: std::error::Error + Send + Sync;
     type State: ServiceBusRetryPolicyState;
 
     fn state(&self) -> &Self::State;
@@ -45,7 +50,7 @@ pub trait ServiceBusRetryPolicy: Eq + Hash + ToString {
     /// the operation is no longer eligible to be retried.
     fn calculate_retry_delay(
         &self,
-        last_error: Self::Error,
+        last_error: &Self::Error,
         attempt_count: i32,
     ) -> Option<Duration>;
 }
@@ -72,59 +77,88 @@ pub(crate) mod private {
     use std::time::Duration;
 
     use async_trait::async_trait;
+    use tokio::time::timeout;
     use tokio_util::sync::CancellationToken;
 
     use crate::core::TransportConnectionScope;
 
-    use super::ServiceBusRetryPolicy;
+    use super::{
+        RetryError, ServiceBusRetryPolicy, ServiceBusRetryPolicyState, SERVER_BUSY_BASE_SLEEP_TIME,
+    };
 
     /// TODO:
     #[async_trait]
     pub trait ServiceBusRetryPolicyExt: ServiceBusRetryPolicy {
-        async fn run_operation_not_verbose<F, T1, Fut, S>(
-            &mut self,
-            operation: F,
-            t1: T1,
-            scope: S,
-            cancellation_token: CancellationToken,
-        ) -> Result<(), Self::Error>
-        where
-            F: Fn(T1, Duration, CancellationToken) -> Fut + Send,
-            T1: Send,
-            Fut: Future<Output = Result<Self::Ok, Self::Error>>,
-            S: TransportConnectionScope + Send,
-        {
-            self.run_operation(operation, t1, scope, cancellation_token, false)
-                .await
-        }
-
         async fn run_operation<F, T1, Fut, S>(
             &mut self,
             operation: F,
             t1: T1,
             scope: S,
             cancellation_token: CancellationToken,
-            log_retries_as_verbose: bool,
-        ) -> Result<(), Self::Error>
+        ) -> Result<Self::Ok, RetryError<Self::Error>>
         where
-            F: Fn(T1, Duration, CancellationToken) -> Fut + Send,
-            T1: Send,
-            Fut: Future<Output = Result<Self::Ok, Self::Error>>,
-            S: TransportConnectionScope + Send,
+            F: Fn(T1, Duration, &CancellationToken) -> Fut + Send + Sync,
+            T1: Clone + Send + Sync,
+            Fut: Future<Output = Result<Self::Ok, Self::Error>> + Send,
+            S: TransportConnectionScope + Send + Sync,
         {
-            todo!()
+            let mut failed_attempt_count = 0;
+            let mut try_timeout = self.calculate_try_timeout(0);
+            if self.state().is_server_busy() && try_timeout < SERVER_BUSY_BASE_SLEEP_TIME {
+                // We are in a server busy state before we start processing. Since
+                // ServerBusyBaseSleepTime > remaining time for the operation, we don't wait for the
+                // entire Sleep time.
+                timeout(try_timeout, cancellation_token.cancelled())
+                    .await
+                    .map_err(|_| RetryError::ServiceBusy)?
+            }
+
+            let outcome = loop {
+                if self.state().is_server_busy() {
+                    let cancelled_fut = cancellation_token.cancelled();
+                    let _ = timeout(SERVER_BUSY_BASE_SLEEP_TIME, cancelled_fut).await;
+                }
+
+                match (operation)(t1.clone(), try_timeout, &cancellation_token).await {
+                    Ok(outcome) => break outcome,
+                    Err(error) => {
+                        failed_attempt_count += 1;
+                        let retry_delay = self.calculate_retry_delay(&error, failed_attempt_count);
+
+                        match (
+                            retry_delay,
+                            scope.is_disposed(),
+                            cancellation_token.is_cancelled(),
+                        ) {
+                            (Some(retry_delay), false, false) => {
+                                log::error!("{}", &error);
+
+                                let _ = timeout(retry_delay, cancellation_token.cancelled()).await;
+                                try_timeout = self.calculate_try_timeout(failed_attempt_count);
+                            }
+                            _ => return Err(RetryError::Operation(error)),
+                        }
+                    }
+                }
+            };
+
+            Ok(outcome)
         }
 
         fn set_server_busy(&mut self, error_message: String) {
-            todo!()
+            let state = self.state_mut();
+
+            *state.is_server_busy_mut() = true;
+            *state.server_busy_error_message_mut() = error_message;
         }
 
         fn reset_server_busy(&mut self) {
-            todo!()
+            *self.state_mut().is_server_busy_mut() = false;
         }
 
         async fn schedule_reset_server_busy(&mut self) {
-            todo!()
+            tokio::time::sleep(SERVER_BUSY_BASE_SLEEP_TIME).await;
+            self.reset_server_busy()
         }
     }
 }
