ef1b445d129c1bec91dbcfc0dd7d4fa4e5bf241a
minghuaw <michael.wu1107@gmail.com>
2022-09-02 20:07:16 -0700

    added basic_retry_policy.rs

---
+++ sdk/messaging_servicebus/src/core/basic_retry_policy.rs
@@ -0,0 +1,7 @@
+/// <summary>
+///   The default retry policy for the Service Bus client library, respecting the
+///   configuration specified as a set of <see cref="ServiceBusRetryOptions" />.
+/// </summary>
+///
+/// <seealso cref="ServiceBusRetryOptions"/>
+pub(crate) struct BasicRetryPolicy {}
--- sdk/messaging_servicebus/src/core/mod.rs
+++ sdk/messaging_servicebus/src/core/mod.rs
@@ -1,9 +1,11 @@
+mod basic_retry_policy;
 mod transport_client;
 mod transport_connection_scope;
 mod transport_receiver;
 mod transport_rule_manager;
 mod transport_sender;
 
+pub(crate) use basic_retry_policy::*;
 pub(crate) use transport_client::*;
 pub(crate) use transport_connection_scope::*;
 pub(crate) use transport_receiver::*;
