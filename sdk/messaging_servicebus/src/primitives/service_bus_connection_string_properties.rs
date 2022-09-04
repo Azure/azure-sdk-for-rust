06a7b54e291a02d94260efbdfffc7bf6fa20158f
minghuaw <michael.wu1107@gmail.com>
2022-09-04 14:10:33 -0700

    using Url for Uri

--- sdk/messaging_servicebus/src/primitives/service_bus_connection_string_properties.rs
+++ sdk/messaging_servicebus/src/primitives/service_bus_connection_string_properties.rs
@@ -1,4 +1,6 @@
-use std::borrow::Cow;
+use std::{borrow::Cow, io::Cursor};
+
+use azure_core::Url;
 
 #[derive(Debug, thiserror::Error)]
 pub enum ParseError {
@@ -12,11 +14,11 @@ pub enum ParseError {
 /// The set of properties that comprise a Service Bus connection string.
 #[derive(Debug, PartialEq, Eq, Hash)]
 pub struct ServiceBusConnectionStringProperties<'a> {
-    endpoint: Cow<'a, str>,
-    entity_path: Cow<'a, str>,
-    shared_access_key_name: Cow<'a, str>,
-    shared_access_key: Cow<'a, str>,
-    shared_access_signature: Cow<'a, str>,
+    endpoint: url::Url,
+    entity_path: &'a str,
+    shared_access_key_name: &'a str,
+    shared_access_key: &'a str,
+    shared_access_signature: &'a str,
 }
 
 impl<'a> ServiceBusConnectionStringProperties<'a> {
@@ -71,7 +73,59 @@ impl<'a> ServiceBusConnectionStringProperties<'a> {
     ///
     /// <exception cref="FormatException">The specified connection string was malformed and could not be parsed.</exception>
     ///
-    pub fn parse(connection_string: impl Into<Cow<'a, str>>) -> Result<Self, ParseError> {
+    pub fn parse(connection_string: &'a str) -> Result<Self, ParseError> {
+        if connection_string.is_empty() {
+            return Err(ParseError::ConnectionStringIsEmpty);
+        }
+
+        let mut endpoint: Option<Url> = None;
+        let mut entity_path: Option<&'a str> = None;
+        let mut shared_access_key_name: Option<&'a str> = None;
+        let mut shared_access_key: Option<&'a str> = None;
+        let mut shared_access_signature: Option<&'a str> = None;
+
+        let token_value_pairs = connection_string.split(Self::TOKEN_VALUE_PAIR_DELIMITER);
+
+        for token_value_pair in token_value_pairs {
+            let mut split = token_value_pair.split(Self::TOKEN_VALUE_SEPARATOR);
+            let token = split
+                .next()
+                .ok_or(ParseError::InvalidConnectionString)?
+                .trim();
+            let value = split
+                .next()
+                .ok_or(ParseError::InvalidConnectionString)?
+                .trim();
+
+            // If there was no value for a key, then consider the connection string to
+            // be malformed.
+            if token.is_empty() || value.is_empty() {
+                return Err(ParseError::InvalidConnectionString);
+            }
+
+            // Compare the token against the known connection string properties and capture the
+            // pair if they are a known attribute.
+            match token {
+                Self::ENDPOINT_TOKEN => todo!(),
+                Self::ENTITY_PATH_TOKEN => entity_path = Some(value),
+                Self::SHARED_ACCESS_KEY_NAME_TOKEN => shared_access_key_name = Some(value),
+                Self::SHARED_ACCESS_KEY_VALUE_TOKEN => shared_access_key = Some(value),
+                Self::SHARED_ACCESS_SIGNATURE_TOKEN => shared_access_signature = Some(value),
+                _ => {}
+            }
+        }
         todo!()
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use azure_core::Url;
+
+    #[test]
+    fn test_parsing_endpoint_as_url() {
+        let connection_endpoint = "sb://fe2o3-amqp-example.servicebus.windows.net/";
+        let url = Url::parse(&connection_endpoint).unwrap();
+        println!("{:?}", url);
     }
 }
