5cbd5f8defb4bd83787bc66b4830b1686043f152
minghuaw <michael.wu1107@gmail.com>
2022-09-05 15:39:54 -0700

    outline sas signature and sas token cred

--- sdk/messaging_servicebus/src/authorization/shared_access_credential.rs
+++ sdk/messaging_servicebus/src/authorization/shared_access_credential.rs
@@ -1,13 +1,15 @@
+use std::time::Duration;
+
 use azure_core::auth::{TokenCredential, TokenResponse};
 
 pub(crate) struct SharedAccessCredential {}
 
 impl SharedAccessCredential {
     /// <summary>The buffer to apply when considering refreshing; signatures that expire less than this duration will be refreshed.</summary>
-    private static readonly TimeSpan SignatureRefreshBuffer = TimeSpan.FromMinutes(10);
+    const SIGNATURE_REFRESH_BUFFER: Duration = Duration::from_secs(10 * 60); // 10 mins
 
     /// <summary>The length of time extend signature validity, if a token was requested.</summary>
-    private static readonly TimeSpan SignatureExtensionDuration = TimeSpan.FromMinutes(30);
+    const SIGNATURE_EXTENSION_DURATION: Duration = Duration::from_secs(30 * 60); // 30 mins
 }
 
 #[cfg_attr(target_arch = "wasm32", async_trait::async_trait(?Send))]
--- sdk/messaging_servicebus/src/authorization/shared_access_signature.rs
+++ sdk/messaging_servicebus/src/authorization/shared_access_signature.rs
@@ -1,42 +1,46 @@
-pub(crate) struct SharedAccessSignature {
+use std::time::Duration;
 
-}
+use const_format::formatcp;
+use time::OffsetDateTime;
 
+pub(crate) struct SharedAccessSignature {}
 
 impl SharedAccessSignature {
     /// <summary>The maximum allowed length of the SAS key name.</summary>
-    internal const int MaximumKeyNameLength = 256;
+    pub(crate) const MAXIMUM_KEY_NAME_LENGTH: usize = 256;
 
     /// <summary>The maximum allowed length of the SAS key.</summary>
-    private const int MaximumKeyLength = 256;
+    const MAXIMUM_KEY_LENGTH: usize = 256;
 
     /// <summary>The token that represents the type of authentication used.</summary>
-    private const string AuthenticationTypeToken = "SharedAccessSignature";
+    const AUTHENTICATION_TYPE_TOKEN: &'static str = "SharedAccessSignature";
 
     /// <summary>The token that identifies the signed component of the shared access signature.</summary>
-    private const string SignedResourceToken = "sr";
+    const SIGNED_RESOURCE_TOKEN: &'static str = "sr";
 
     /// <summary>The token that identifies the signature component of the shared access signature.</summary>
-    private const string SignatureToken = "sig";
+    const SIGNATURE_TOKEN: &'static str = "sig";
 
     /// <summary>The token that identifies the signed SAS key component of the shared access signature.</summary>
-    private const string SignedKeyNameToken = "skn";
+    const SIGNED_KEY_NAME_TOKEN: &'static str = "skn";
 
     /// <summary>The token that identifies the signed expiration time of the shared access signature.</summary>
-    private const string SignedExpiryToken = "se";
+    const SIGNED_EXPIRY_TOKEN: &'static str = "se";
 
     /// <summary>The token that fully identifies the signed resource within the signature.</summary>
-    private const string SignedResourceFullIdentifierToken = AuthenticationTypeToken + " " + SignedResourceToken;
+    // AuthenticationTypeToken + " " + SignedResourceToken;
+    const SIGNED_RESOURCE_FULL_IDENTIFIER_TOKEN: &'static str = "SharedAccessSignature sr";
 
     /// <summary>The character used to separate a token and its value in the connection string.</summary>
-    private const char TokenValueSeparator = '=';
+    const TOKEN_VALUE_SEPARATOR: char = '=';
 
     /// <summary>The character used to mark the beginning of a new token/value pair in the signature.</summary>
-    private const char TokenValuePairDelimiter = '&';
+    const TOKEN_VALUE_PAIR_DELIMITER: char = '&';
 
     /// <summary>The default length of time to consider a signature valid, if not otherwise specified.</summary>
-    private static readonly TimeSpan DefaultSignatureValidityDuration = TimeSpan.FromMinutes(30);
+    const DEFAULT_SIGNATURE_VALIDITY_DURATION: Duration = Duration::from_secs(30 * 60); // 30 mins
 
     /// <summary>Represents the Unix epoch time value, January 1, 1970 12:00:00, UTC.</summary>
-    private static readonly DateTimeOffset Epoch = new DateTimeOffset(1970, 1, 1, 0, 0, 0, TimeSpan.Zero);
+    // static readonly DateTimeOffset Epoch = new DateTimeOffset(1970, 1, 1, 0, 0, 0, TimeSpan.Zero);
+    const EPOCH: OffsetDateTime = OffsetDateTime::UNIX_EPOCH;
 }
