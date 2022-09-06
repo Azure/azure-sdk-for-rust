3397f980446083419b41be1ddd6787a72b23893c
minghuaw <michael.wu1107@gmail.com>
2022-09-06 14:11:36 -0700

    impl SharedAccessCredential

--- sdk/messaging_servicebus/src/authorization/service_bus_token_credential.rs
+++ sdk/messaging_servicebus/src/authorization/service_bus_token_credential.rs
@@ -1,10 +1,41 @@
 use azure_core::auth::{TokenCredential, TokenResponse};
 
-pub(crate) struct ServiceBusTokenCredential {}
+/// <summary>
+///   Provides a generic token-based credential for a given Service Bus entity instance.
+/// </summary>
+///
+/// <seealso cref="Azure.Core.TokenCredential" />
+///
+pub(crate) struct ServiceBusTokenCredential<T>
+where
+    T: TokenCredential,
+{
+    credential: T,
+    is_shared_access_credential: bool,
+}
+
+impl<T> ServiceBusTokenCredential<T>
+where
+    T: TokenCredential,
+{
+    /// <summary>
+    ///   Indicates whether the credential is based on an Service Bus
+    ///   shared access policy.
+    /// </summary>
+    ///
+    /// <value><c>true</c> if the credential should be considered a SAS credential; otherwise, <c>false</c>.</value>
+    ///
+    pub fn is_shared_access_credential(&self) -> bool {
+        self.is_shared_access_credential
+    }
+}
 
 #[cfg_attr(target_arch = "wasm32", async_trait::async_trait(?Send))]
 #[cfg_attr(not(target_arch = "wasm32"), async_trait::async_trait)]
-impl TokenCredential for ServiceBusTokenCredential {
+impl<T> TokenCredential for ServiceBusTokenCredential<T>
+where
+    T: TokenCredential,
+{
     /// Gets a `TokenResponse` for the specified resource
     async fn get_token(&self, resource: &str) -> azure_core::Result<TokenResponse> {
         todo!()
