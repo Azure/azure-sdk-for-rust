43703b682d83f445a52d10c824889ab4791e3524
Cameron Taggart <cameron.taggart@gmail.com>
2022-09-08 11:54:20 -0400

    use stable tag if available

--- services/autorust/codegen/src/cargo_toml.rs
+++ services/autorust/codegen/src/cargo_toml.rs
@@ -21,15 +21,14 @@ pub fn create(package_name: &str, tags: &[&Tag], default_tag: &Tag, has_xml: boo
 }
 
 pub fn get_default_tag<'a>(tags: &[&'a Tag], default_tag: Option<&str>) -> &'a Tag {
-    if let Some(default_tag) = default_tag {
-        if let Some(tag) = tags.iter().find(|tag| tag.name() == default_tag) {
-            return tag;
-        }
-    }
-    let tag = tags.iter().find(|tag| !tag.name().contains("preview"));
-    match tag {
-        Some(tag) => tag,
-        None => tags[0],
+    let default_tag = tags.iter().find(|tag| Some(tag.name()) == default_tag);
+    let is_preview = default_tag.map(|tag| tag.name().contains("preview")).unwrap_or_default();
+    let stable_tag = tags.iter().find(|tag| !tag.name().contains("preview"));
+    match (default_tag, is_preview, stable_tag) {
+        (Some(tag), false, _) => tag,
+        (Some(tag), true, None) => tag,
+        (_, _, Some(tag)) => tag,
+        _ => tags[0],
     }
 }
 
@@ -53,3 +52,82 @@ pub fn get_default_tag<'a>(tags: &[&'a Tag], default_tag: Option<&str>) -> &'a T
 //     features.insert(0, NO_DEFAULT_TAG.to_owned());
 //     features
 // }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    // Some readme.md are specifying the default tag as a preview.
+    // If there is a stable version, we should use it instead.
+    #[test]
+    fn default_tag_is_stable_if_available() -> Result<()> {
+        let tags = vec![
+            "package-preview-2022-05",
+            "package-2021-06",
+            "package-2020-09",
+            "package-2020-04",
+            "package-2019-12",
+            "package-2019-06-preview",
+            "package-2019-06",
+            "package-2019-04",
+            "package-2017-10",
+            "package-2017-04",
+        ];
+        let tags: Vec<_> = tags.into_iter().map(Tag::new).collect();
+        let tags: Vec<_> = tags.iter().collect();
+        assert_eq!("package-2021-06", get_default_tag(&tags, Some("package-preview-2022-05")).name());
+        Ok(())
+    }
+
+    #[test]
+    fn default_tag_is_stable() -> Result<()> {
+        let tags = vec![
+            "package-preview-2022-05",
+            "package-2021-06",
+            "package-2020-09",
+            "package-2020-04",
+            "package-2019-12",
+            "package-2019-06-preview",
+            "package-2019-06",
+            "package-2019-04",
+            "package-2017-10",
+            "package-2017-04",
+        ];
+        let tags: Vec<_> = tags.into_iter().map(Tag::new).collect();
+        let tags: Vec<_> = tags.iter().collect();
+        assert_eq!("package-2021-06", get_default_tag(&tags, None).name());
+        Ok(())
+    }
+
+    #[test]
+    fn specified_tag() -> Result<()> {
+        let tags = vec![
+            "package-preview-2022-05",
+            "package-2021-06",
+            "package-2020-09",
+            "package-2020-04",
+            "package-2019-12",
+            "package-2019-06-preview",
+            "package-2019-06",
+            "package-2019-04",
+            "package-2017-10",
+            "package-2017-04",
+        ];
+        let tags: Vec<_> = tags.into_iter().map(Tag::new).collect();
+        let tags: Vec<_> = tags.iter().collect();
+        assert_eq!("package-2020-04", get_default_tag(&tags, Some("package-2020-04")).name());
+        Ok(())
+    }
+
+    #[test]
+    fn specified_preview() -> Result<()> {
+        let tags = vec!["package-preview-2022-05", "package-2019-06-preview", "package-2019-04-preview"];
+        let tags: Vec<_> = tags.into_iter().map(Tag::new).collect();
+        let tags: Vec<_> = tags.iter().collect();
+        assert_eq!(
+            "package-2019-06-preview",
+            get_default_tag(&tags, Some("package-2019-06-preview")).name()
+        );
+        Ok(())
+    }
+}
