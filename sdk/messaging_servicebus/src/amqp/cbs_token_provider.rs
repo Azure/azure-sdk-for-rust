2278a7c285cd430afef9429a6de6c9df4be84318
minghuaw <michael.wu1107@gmail.com>
2022-10-05 13:44:14 -0700

    wrap ServiceBusTokenCredential in TokenType

--- sdk/messaging_servicebus/src/amqp/cbs_token_provider.rs
+++ sdk/messaging_servicebus/src/amqp/cbs_token_provider.rs
@@ -1,4 +1,4 @@
-use azure_core::auth::TokenCredential;
+use azure_core::auth::{TokenCredential, TokenResponse};
 use time::Duration as TimeSpan;
 use tokio::sync::Semaphore;
 use tokio_util::sync::CancellationToken;
@@ -10,11 +10,10 @@ use super::token_type::TokenType;
 #[derive(Debug)]
 pub(crate) struct CbsTokenProvider<TC: TokenCredential> {
     /// The type to consider a token generated from the associated <see cref="Credential" />
-    token_type: TokenType,
+    token_type: TokenType<TC>,
 
-    /// The credential used to generate access tokens.
-    credential: ServiceBusTokenCredential<TC>,
-
+    // /// The credential used to generate access tokens.
+    // credential: ServiceBusTokenCredential<TC>,
     /// The amount of buffer to when evaluating token expiration; the token's expiration date will
     /// be adjusted earlier by this amount.
     token_expiration_buffer: TimeSpan,
@@ -45,9 +44,10 @@ where
         cancellation_token: CancellationToken,
     ) -> Self {
         let token_type = if credential.is_shared_access_credential() {
-            TokenType::SharedAccessToken
+            TokenType::SharedAccessToken { credential }
         } else {
             TokenType::JsonWebToken {
+                credential,
                 // Tokens are only cached for JWT-based credentials; no need
                 // to instantiate the semaphore if no caching is taking place.
                 semaphore: Semaphore::new(1),
@@ -56,9 +56,31 @@ where
 
         Self {
             token_type,
-            credential,
+            // credential,
             token_expiration_buffer,
             cancellation_token,
         }
+    }
+
+    /// <summary>
+    ///   Asynchronously requests a CBS token to be used for authorization within an AMQP
+    ///   scope.
+    /// </summary>
+    ///
+    /// <param name="namespaceAddress">The address of the namespace to be authorized.</param>
+    /// <param name="appliesTo">The resource to which the token should apply.</param>
+    /// <param name="requiredClaims">The set of claims that are required for authorization.</param>
+    ///
+    /// <returns>The token to use for authorization.</returns>
+    ///
+    pub async fn get_token(&mut self) -> TokenResponse {
+        match &self.token_type {
+            TokenType::SharedAccessToken { credential } => todo!(),
+            TokenType::JsonWebToken {
+                credential,
+                semaphore,
+            } => todo!(),
+        }
+        todo!()
     }
 }
--- sdk/messaging_servicebus/src/amqp/token_type.rs
+++ sdk/messaging_servicebus/src/amqp/token_type.rs
@@ -1,21 +1,28 @@
+use azure_core::auth::TokenCredential;
 use tokio::sync::Semaphore;
 
+use crate::authorization::service_bus_token_credential::ServiceBusTokenCredential;
+
 #[derive(Debug)]
-pub(crate) enum TokenType {
+pub(crate) enum TokenType<TC: TokenCredential> {
     /// The type to consider a token if it is based on an Service Bus shared access signature.
-    SharedAccessToken,
+    SharedAccessToken {
+        credential: ServiceBusTokenCredential<TC>,
+    },
     /// The type to consider a token if not based on a shared access signature.
     JsonWebToken {
+        credential: ServiceBusTokenCredential<TC>,
+
         /// Tokens are only cached for JWT-based credentials; no need
         /// to instantiate the semaphore if no caching is taking place.
         semaphore: Semaphore,
     },
 }
 
-impl ToString for TokenType {
+impl<TC: TokenCredential> ToString for TokenType<TC> {
     fn to_string(&self) -> String {
         match self {
-            TokenType::SharedAccessToken => "servicebus.windows.net:sastoken".to_string(),
+            TokenType::SharedAccessToken { .. } => "servicebus.windows.net:sastoken".to_string(),
             TokenType::JsonWebToken { .. } => "jwt".to_string(),
         }
     }
