f1aa1a071bcff30fd09f19b004e2d9f08808167c
Cameron Taggart <cameron.taggart@gmail.com>
2022-10-07 18:03:47 -0400

    add deserialize_null_default

--- sdk/core/src/util.rs
+++ sdk/core/src/util.rs
@@ -15,3 +15,14 @@ where
         .or_else(|_| T::deserialize(serde_json::Value::String(v.to_lowercase())))
         .map_err(de::Error::custom)
 }
+
+/// Deserialize JSON null as default
+/// https://github.com/serde-rs/serde/issues/1098
+pub fn deserialize_null_default<'de, D, T>(deserializer: D) -> Result<T, D::Error>
+where
+    T: Default + Deserialize<'de>,
+    D: Deserializer<'de>,
+{
+    let opt = Option::deserialize(deserializer)?;
+    Ok(opt.unwrap_or_default())
+}
