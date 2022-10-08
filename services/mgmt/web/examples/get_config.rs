e0b09217df36a3dc25d3dfecdf3c877c9e89c847
Cameron Taggart <cameron.taggart@gmail.com>
2022-10-07 20:12:01 -0400

    finish get_config example

--- services/mgmt/web/examples/get_config.rs
+++ services/mgmt/web/examples/get_config.rs
@@ -1,28 +1,18 @@
-// cargo run --package azure_mgmt_web --example get_config
+// cargo run --package azure_mgmt_web --example get_config -- resource_group webapp
 
 use azure_identity::AzureCliCredential;
-use azure_mgmt_web::ClientBuilder;
-use serde_json::{to_string_pretty, Value};
 use std::{error::Error, sync::Arc};
 
 #[tokio::main]
-async fn main()-> Result<(), Box<dyn Error>> {
-    let resource_group = "cataggar-avs-cw";
-    let webapp_name = "cataggar-avs-cw";
-    let sub_id = "7f1fae41-7708-4fa4-89b3-f6552cad2fc1";
+async fn main() -> Result<(), Box<dyn Error>> {
+    let resource_group_name = std::env::args().nth(1).expect("please specify resource group");
+    let webapp_name = std::env::args().nth(2).expect("please specify webapp name");
 
-    let creds = Arc::new(AzureCliCredential::default());
-    let client = ClientBuilder::new(creds).build();
+    let credential = Arc::new(AzureCliCredential::new());
+    let subscription_id = AzureCliCredential::get_subscription()?;
+    let client = azure_mgmt_web::Client::builder(credential).build();
 
-    // let get = client.web_apps_client().get_configuration(resource_group, webapp_name, sub_id).into_future().await?;
-    let get = client.web_apps_client().get_configuration(resource_group, webapp_name, sub_id).send().await?;
-    let data = get.into_raw_response().into_body().collect().await?;
-    // let data = to_string_pretty(&get.properties.unwrap())?;
-    println!("{:?}", data);
-
-    // let path = r#"C:\Users\cataggar\OneDrive - Microsoft\Documents\2022-10\2022-10-03 site config b.json"#;
-    // let bytes = std::fs::read(path)?;
-    // let body: azure_mgmt_web::models::SiteConfigResource = serde_json::from_slice(&bytes)?;
-
+    let config = client.web_apps_client().get_configuration(resource_group_name, webapp_name, subscription_id).into_future().await?;
+    println!("{:#?}", config);
     Ok(())
 }
