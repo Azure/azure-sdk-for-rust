70d0baa0a319f5d358d64d2a5191eb328bfa1906
Brian Caswell <bcaswell@microsoft.com>
2022-10-17 10:24:26 -0400

    address feedback

--- sdk/storage/src/cloud_location.rs
+++ sdk/storage/src/cloud_location.rs
@@ -65,18 +65,19 @@ impl CloudLocation {
 impl TryFrom<&Url> for CloudLocation {
     type Error = azure_core::Error;
 
-    // TODO: This only works for Public and China clouds
+    // TODO: This only works for Public and China clouds.
+    // ref: https://github.com/Azure/azure-sdk-for-rust/issues/502
     fn try_from(url: &Url) -> azure_core::Result<Self> {
         let token = url.query().ok_or_else(|| {
             azure_core::Error::with_message(azure_core::error::ErrorKind::DataConversion, || {
-                "missing token"
+                "unable to find SAS token in URL"
             })
         })?;
         let credentials = StorageCredentials::sas_token(token)?;
 
         let host = url.host_str().ok_or_else(|| {
             azure_core::Error::with_message(azure_core::error::ErrorKind::DataConversion, || {
-                "unable to find host from url"
+                "unable to find the target host in the URL"
             })
         })?;
 
@@ -84,7 +85,12 @@ impl TryFrom<&Url> for CloudLocation {
         if domain.len() < 2 {
             return Err(azure_core::Error::with_message(
                 azure_core::error::ErrorKind::DataConversion,
-                || "unable to find host from url",
+                || {
+                    format!(
+                        "URL refers to a domain that is not a Public or China domain: {}",
+                        host
+                    )
+                },
             ));
         }
 
@@ -103,7 +109,12 @@ impl TryFrom<&Url> for CloudLocation {
             }),
             _ => Err(azure_core::Error::with_message(
                 azure_core::error::ErrorKind::DataConversion,
-                || format!("CloudLocation conversion failed: {}", rest),
+                || {
+                    format!(
+                        "URL refers to a domain that is not a Public or China domain: {}",
+                        host
+                    )
+                },
             )),
         }
     }
@@ -121,3 +132,45 @@ pub const EMULATOR_ACCOUNT: &str = "devstoreaccount1";
 /// <https://docs.microsoft.com/azure/storage/common/storage-use-azurite#well-known-storage-account-and-key>
 pub const EMULATOR_ACCOUNT_KEY: &str =
     "Eby8vdM02xNOcqFlqUwJPLlmEtlCDXJ1OUzFT50uSRZ6IFsuFq2UVErCz4I6tq/K1SZFPTOtr/KBHBeksoGMGw==";
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_from_url() -> azure_core::Result<()> {
+        let public_without_token = Url::parse(&format!("https://test.blob.core.windows.net"))?;
+        let public_with_token =
+            Url::parse(&format!("https://test.blob.core.windows.net/?token=1"))?;
+
+        let cloud_location: CloudLocation = (&public_with_token).try_into()?;
+        assert_eq!(public_without_token, cloud_location.url(ServiceType::Blob)?);
+
+        let creds = cloud_location.credentials();
+        assert!(matches!(creds, &StorageCredentials::SASToken(_)));
+
+        let file_url = Url::parse(&format!("file://tmp/test.txt"))?;
+        let result: azure_core::Result<CloudLocation> = (&file_url).try_into();
+        assert!(result.is_err());
+
+        let missing_account = Url::parse(&format!("https://blob.core.windows.net?token=1"))?;
+        let result: azure_core::Result<CloudLocation> = (&missing_account).try_into();
+        assert!(result.is_err());
+
+        let missing_service_type = Url::parse(&format!("https://core.windows.net?token=1"))?;
+        let result: azure_core::Result<CloudLocation> = (&missing_service_type).try_into();
+        assert!(result.is_err());
+
+        let china_cloud = Url::parse(&format!("https://test.blob.core.chinacloudapi.cn/?token=1"))?;
+        let china_cloud_without_token =
+            Url::parse(&format!("https://test.blob.core.chinacloudapi.cn"))?;
+
+        let cloud_location: CloudLocation = (&china_cloud).try_into()?;
+        assert_eq!(
+            china_cloud_without_token,
+            cloud_location.url(ServiceType::Blob)?
+        );
+
+        Ok(())
+    }
+}
