b8286f1557b22a332774d9183177cde8c382eedd
minghuaw <michael.wu1107@gmail.com>
2022-11-20 04:10:11 -0800

    init impl of remove_rule req/res

--- sdk/messaging_servicebus/src/amqp/amqp_request_message/remove_rule.rs
+++ sdk/messaging_servicebus/src/amqp/amqp_request_message/remove_rule.rs
@@ -1,1 +1,80 @@
+use fe2o3_amqp_management::request::Request;
+use fe2o3_amqp_types::primitives::OrderedMap;
 
+use crate::amqp::{
+    amqp_response_message::remove_rule::RemoveRuleResponse,
+    management_constants::{operations::REMOVE_RULE_OPERATION, properties::RULE_NAME},
+};
+
+type RemoveRuleRequestBody = OrderedMap<String, String>;
+
+pub(crate) struct RemoveRuleRequest {
+    server_timeout: Option<u32>,
+    body: RemoveRuleRequestBody,
+}
+
+impl RemoveRuleRequest {
+    pub fn new(rule_name: String) -> Self {
+        let mut body = OrderedMap::new();
+        body.insert(RULE_NAME.to_string(), rule_name);
+
+        Self {
+            server_timeout: None,
+            body,
+        }
+    }
+}
+
+impl Request for RemoveRuleRequest {
+    const OPERATION: &'static str = REMOVE_RULE_OPERATION;
+
+    type Response = RemoveRuleResponse;
+
+    type Body = RemoveRuleRequestBody;
+
+    fn encode_application_properties(
+        &mut self,
+    ) -> Option<fe2o3_amqp_types::messaging::ApplicationProperties> {
+        super::encode_server_timeout_as_application_properties(self.server_timeout)
+    }
+
+    fn encode_body(self) -> Self::Body {
+        self.body
+    }
+}
+
+impl<'a> Request for &'a RemoveRuleRequest {
+    const OPERATION: &'static str = REMOVE_RULE_OPERATION;
+
+    type Response = RemoveRuleResponse;
+
+    type Body = &'a RemoveRuleRequestBody;
+
+    fn encode_application_properties(
+        &mut self,
+    ) -> Option<fe2o3_amqp_types::messaging::ApplicationProperties> {
+        super::encode_server_timeout_as_application_properties(self.server_timeout)
+    }
+
+    fn encode_body(self) -> Self::Body {
+        &self.body
+    }
+}
+
+impl<'a> Request for &'a mut RemoveRuleRequest {
+    const OPERATION: &'static str = REMOVE_RULE_OPERATION;
+
+    type Response = RemoveRuleResponse;
+
+    type Body = &'a RemoveRuleRequestBody;
+
+    fn encode_application_properties(
+        &mut self,
+    ) -> Option<fe2o3_amqp_types::messaging::ApplicationProperties> {
+        super::encode_server_timeout_as_application_properties(self.server_timeout)
+    }
+
+    fn encode_body(self) -> Self::Body {
+        &self.body
+    }
+}
