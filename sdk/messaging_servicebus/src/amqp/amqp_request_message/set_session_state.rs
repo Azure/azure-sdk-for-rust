92fab7048471e00d354a41daa5526813441a852a
minghuaw <michael.wu1107@gmail.com>
2022-11-20 02:49:02 -0800

    set_session_state req/res

--- sdk/messaging_servicebus/src/amqp/amqp_request_message/set_session_state.rs
+++ sdk/messaging_servicebus/src/amqp/amqp_request_message/set_session_state.rs
@@ -1,1 +1,84 @@
+use fe2o3_amqp_management::request::Request;
+use fe2o3_amqp_types::primitives::{Array, OrderedMap};
+use serde_amqp::Value;
 
+use crate::amqp::{
+    amqp_response_message::set_session_state::SetSessionStateResponse,
+    management_constants::{
+        operations::SET_SESSION_STATE_OPERATION,
+        properties::{SESSION_ID, SESSION_STATE},
+    },
+};
+
+type SetSessionStateRequestBody = OrderedMap<String, Value>;
+
+pub struct SetSessionStateRequest {
+    server_timeout: Option<u32>,
+    body: SetSessionStateRequestBody,
+}
+
+impl SetSessionStateRequest {
+    pub fn new(session_id: String, session_state: Array<u8>) -> Self {
+        let mut body = OrderedMap::new();
+        body.insert(SESSION_ID.into(), session_id.into());
+        body.insert(SESSION_STATE.into(), session_state.into());
+        Self {
+            server_timeout: None,
+            body,
+        }
+    }
+}
+
+impl Request for SetSessionStateRequest {
+    const OPERATION: &'static str = SET_SESSION_STATE_OPERATION;
+
+    type Response = SetSessionStateResponse;
+
+    type Body = SetSessionStateRequestBody;
+
+    fn encode_application_properties(
+        &mut self,
+    ) -> Option<fe2o3_amqp_types::messaging::ApplicationProperties> {
+        super::encode_server_timeout_as_application_properties(self.server_timeout)
+    }
+
+    fn encode_body(self) -> Self::Body {
+        self.body
+    }
+}
+
+impl<'a> Request for &'a mut SetSessionStateRequest {
+    const OPERATION: &'static str = SET_SESSION_STATE_OPERATION;
+
+    type Response = SetSessionStateResponse;
+
+    type Body = &'a SetSessionStateRequestBody;
+
+    fn encode_application_properties(
+        &mut self,
+    ) -> Option<fe2o3_amqp_types::messaging::ApplicationProperties> {
+        super::encode_server_timeout_as_application_properties(self.server_timeout)
+    }
+
+    fn encode_body(self) -> Self::Body {
+        &self.body
+    }
+}
+
+impl<'a> Request for &'a SetSessionStateRequest {
+    const OPERATION: &'static str = SET_SESSION_STATE_OPERATION;
+
+    type Response = SetSessionStateResponse;
+
+    type Body = &'a SetSessionStateRequestBody;
+
+    fn encode_application_properties(
+        &mut self,
+    ) -> Option<fe2o3_amqp_types::messaging::ApplicationProperties> {
+        super::encode_server_timeout_as_application_properties(self.server_timeout)
+    }
+
+    fn encode_body(self) -> Self::Body {
+        &self.body
+    }
+}
