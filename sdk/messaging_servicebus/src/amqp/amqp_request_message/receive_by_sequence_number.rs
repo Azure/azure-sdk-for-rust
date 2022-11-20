4f05f46dc68a018fde31106e9dd4700a1bdf76df
minghuaw <michael.wu1107@gmail.com>
2022-11-20 09:50:53 -0800

    init impl of receive by seq num req/res

--- sdk/messaging_servicebus/src/amqp/amqp_request_message/receive_by_sequence_number.rs
+++ sdk/messaging_servicebus/src/amqp/amqp_request_message/receive_by_sequence_number.rs
@@ -1,1 +1,58 @@
+use fe2o3_amqp_management::request::Request;
+use fe2o3_amqp_types::{
+    definitions::ReceiverSettleMode,
+    primitives::{Array, OrderedMap},
+};
+use serde_amqp::Value;
 
+use crate::amqp::{
+    amqp_response_message::receive_by_sequence_number::ReceiveBySequenceNumberResponse,
+    management_constants::{
+        operations::RECEIVE_BY_SEQUENCE_NUMBER_OPERATION,
+        properties::{RECEIVER_SETTLE_MODE, SEQUENCE_NUMBERS},
+    },
+};
+
+type ReceiveBySequenceNumberRequestBody = OrderedMap<String, Value>;
+
+pub struct ReceiveBySequenceNumberRequest {
+    server_timeout: Option<u32>,
+    body: ReceiveBySequenceNumberRequestBody,
+}
+
+impl ReceiveBySequenceNumberRequest {
+    pub fn new(
+        sequence_numbers: impl Into<Array<i64>>,
+        receiver_settle_mode: ReceiverSettleMode,
+    ) -> Self {
+        let sequence_numbers = sequence_numbers.into();
+        let mut body = OrderedMap::new();
+        body.insert(SEQUENCE_NUMBERS.into(), sequence_numbers.into());
+        body.insert(
+            RECEIVER_SETTLE_MODE.into(),
+            Value::UByte(receiver_settle_mode.into()),
+        );
+        Self {
+            server_timeout: None,
+            body,
+        }
+    }
+}
+
+impl Request for ReceiveBySequenceNumberRequest {
+    const OPERATION: &'static str = RECEIVE_BY_SEQUENCE_NUMBER_OPERATION;
+
+    type Response = ReceiveBySequenceNumberResponse;
+
+    type Body = ReceiveBySequenceNumberRequestBody;
+
+    fn encode_application_properties(
+        &mut self,
+    ) -> Option<fe2o3_amqp_types::messaging::ApplicationProperties> {
+        super::encode_server_timeout_as_application_properties(self.server_timeout)
+    }
+
+    fn encode_body(self) -> Self::Body {
+        self.body
+    }
+}
--- sdk/messaging_servicebus/src/amqp/amqp_response_message/receive_by_sequence_number.rs
+++ sdk/messaging_servicebus/src/amqp/amqp_response_message/receive_by_sequence_number.rs
@@ -1,1 +1,45 @@
+use fe2o3_amqp_management::{error::InvalidType, response::Response};
+use fe2o3_amqp_types::primitives::{OrderedMap, Uuid};
+use serde_amqp::Value;
 
+use crate::amqp::management_constants::properties::{LOCK_TOKEN, MESSAGE, MESSAGES};
+
+type DeferredMessage = OrderedMap<String, Value>;
+type ListOfDeferredMessages = Vec<DeferredMessage>;
+type ReceiveBySequenceNumberResponseBody = OrderedMap<String, ListOfDeferredMessages>;
+
+pub struct ReceiveBySequenceNumberResponse {
+    pub deferred_messages: Vec<(Uuid, Vec<u8>)>,
+}
+
+impl Response for ReceiveBySequenceNumberResponse {
+    const STATUS_CODE: u16 = 200;
+
+    type Body = ReceiveBySequenceNumberResponseBody;
+
+    type Error = super::ManagementError;
+
+    fn decode_message(
+        mut message: fe2o3_amqp_types::messaging::Message<Self::Body>,
+    ) -> Result<Self, Self::Error> {
+        let messages = message.body.remove(MESSAGES).ok_or_else(|| InvalidType {
+            expected: MESSAGES.into(),
+            actual: "None".into(),
+        })?;
+
+        let deferred_messages = messages
+            .into_iter()
+            .map(|mut map| {
+                let uuid = map.remove(LOCK_TOKEN).map(Uuid::try_from);
+                let message = map.remove(MESSAGE).map(Vec::<u8>::try_from);
+                match (uuid, message) {
+                    (Some(Ok(uuid)), Some(Ok(message))) => Some((uuid, message)),
+                    _ => None,
+                }
+            })
+            .collect::<Option<Vec<_>>>()
+            .ok_or(super::ManagementError::DecodeError(None))?;
+
+        Ok(Self { deferred_messages })
+    }
+}
