06354898734422f71e2dde83e27b5e89837f57a5
Brian Caswell <bcaswell@microsoft.com>
2022-11-29 17:53:13 -0500

    make change to crate instead of README

--- sdk/core/src/lib.rs
+++ sdk/core/src/lib.rs
@@ -1,7 +1,7 @@
 //! Core types and traits for the Rust Azure SDK.
 //!
 //! This crate is part of the unofficial Azure SDK effort in Rust. For more
-//! information on the project, and an overview of other crates, please refer to
+//! information on the project and an overview of other crates, please refer to
 //! [our GitHub repository](https://github.com/azure/azure-sdk-for-rust).
 //!
 //! It is a library that provides cross-cutting services to other client
