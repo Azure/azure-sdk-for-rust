0f6cbdedd69c3186270f63da4c4dc6e2955a6711
bmc-msft <41130664+bmc-msft@users.noreply.github.com>
2022-11-29 18:19:12 -0500

    fix readme generation (#1159)

--- sdk/storage_datalake/src/lib.rs
+++ sdk/storage_datalake/src/lib.rs
@@ -4,10 +4,6 @@
 `azure-storage-datalake` offers functionality needed to interact with an ADLS Gen2 storage account from Rust.
 As an abstraction over the [Azure Data Lake Store REST API](https://docs.microsoft.com/en-us/rest/api/storageservices/data-lake-storage-gen2), anything that is possible through that Rest API
 should also be possible with this crate.
-
-## Examples
-
-TODO
 */
 #[macro_use]
 extern crate serde_derive;
