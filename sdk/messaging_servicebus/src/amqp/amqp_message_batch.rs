b839354aad23ccefc191aa1ef5f6ad746e830aaf
minghuaw <michael.wu1107@gmail.com>
2022-12-26 22:28:54 -0800

    added todo

--- sdk/messaging_servicebus/src/amqp/amqp_message_batch.rs
+++ sdk/messaging_servicebus/src/amqp/amqp_message_batch.rs
@@ -31,6 +31,7 @@ impl AmqpMessageBatch {
 
 impl Sealed for AmqpMessageBatch {}
 
+// TODO: Should account for metadata/additional bytes added to the batch itself?
 impl TransportMessageBatch for AmqpMessageBatch {
     type TryAddError = TryAddMessageError;
 
