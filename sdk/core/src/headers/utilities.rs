9565f32ffd9048c4d9e5f298c025225ce9e90f6a
Brian Caswell <bcaswell@microsoft.com>
2023-03-31 13:02:16 -0400

    cargo fmt

--- sdk/core/src/headers/utilities.rs
+++ sdk/core/src/headers/utilities.rs
@@ -80,8 +80,8 @@ pub fn delete_type_permanent_from_headers(headers: &Headers) -> crate::Result<bo
     if result.is_ok() {
         result
     } else {
-       log::warn!("Error receiving delete type permanent.  returning false");
-       Ok(false)
+        log::warn!("Error receiving delete type permanent.  returning false");
+        Ok(false)
     }
 }
 
