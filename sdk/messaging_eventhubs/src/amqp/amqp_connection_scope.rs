26514b09d06f925999ab24486ec1b17e4f3c0aa3
minghuaw <michael.wu1107@gmail.com>
2023-04-02 01:38:43 -0700

    commented out unused fields in amqp conn scope

--- sdk/messaging_eventhubs/src/amqp/amqp_connection_scope.rs
+++ sdk/messaging_eventhubs/src/amqp/amqp_connection_scope.rs
@@ -1,93 +1,76 @@
 use std::{time::Duration as StdDuration, sync::Arc};
 
-use fe2o3_amqp::{Connection, sasl_profile::SaslProfile, connection::ConnectionHandle, session::SessionHandle, Sender};
+use fe2o3_amqp::{Connection, sasl_profile::SaslProfile, connection::ConnectionHandle, session::SessionHandle, Sender, Session};
+use fe2o3_amqp_cbs::client::CbsClient;
+use fe2o3_amqp_types::definitions::Milliseconds;
 use fe2o3_amqp_ws::WebSocketStream;
 use url::Url;
+use time::Duration as TimeSpan;
 
-use crate::{event_hubs_transport_type::EventHubsTransportType, amqp::amqp_constants, authorization::event_hub_token_credential::EventHubTokenCredential, core::transport_producer_features::TransportProducerFeatures, producer::PartitionPublishingOptions};
+use crate::{event_hubs_transport_type::EventHubsTransportType, amqp::{amqp_constants, amqp_cbs_link::AmqpCbsLink}, authorization::event_hub_token_credential::EventHubTokenCredential, core::transport_producer_features::TransportProducerFeatures, producer::PartitionPublishingOptions};
 
-use super::{amqp_connection::AmqpConnection, error::{AmqpConnectionScopeError, OpenProducerError}};
+use super::{amqp_connection::AmqpConnection, error::{AmqpConnectionScopeError, OpenProducerError}, amqp_cbs_link::AmqpCbsLinkHandle, cbs_token_provider::CbsTokenProvider};
+
+const AUTHORIZATION_REFRESH_BUFFER_SECONDS: u64 = 7 * 60;
 
 pub(crate) struct AmqpConnectionScope {
-        /// The recommended timeout to associate with an AMQP session.  It is recommended that this
-        /// interval be used when creating or opening AMQP links and related constructs.
-        pub(crate) session_timeout: StdDuration,
+    // /// The recommended timeout to associate with an AMQP session.  It is recommended that this
+    // /// interval be used when creating or opening AMQP links and related constructs.
+    // pub(crate) session_timeout: StdDuration,
 
-        /// The amount of time to allow a connection to have no observed traffic before considering it idle.
-        pub(crate) connection_idle_timeout: StdDuration,
+    // /// The amount of time to allow a connection to have no observed traffic before considering it idle.
+    // pub(crate) connection_idle_timeout_millis: Milliseconds,
 
-        /// Indicates whether this <see cref="AmqpConnectionScope"/> has been disposed.
-        pub(crate) is_disposed: bool,
+    /// Indicates whether this <see cref="AmqpConnectionScope"/> has been disposed.
+    pub(crate) is_disposed: bool,
 
-        // /// <summary>
-        // ///   The cancellation token to use with operations initiated by the scope.
-        // /// </summary>
-        // ///
-        // private CancellationTokenSource OperationCancellationSource { get; } = new CancellationTokenSource();
+    /// The unique identifier of the scope.
+    pub(crate) id: String,
 
-        // /// <summary>
-        // ///   The set of active AMQP links associated with the connection scope.  These are considered children
-        // ///   of the active connection and should be managed as such.
-        // /// </summary>
-        // ///
-        // private ConcurrentDictionary<AmqpObject, Timer> ActiveLinks { get; } = new ConcurrentDictionary<AmqpObject, Timer>();
+    /// The endpoint for the Event Hubs service to which the scope is associated.
+    pub(crate) service_endpoint: Url,
 
-        /// The unique identifier of the scope.
-        pub(crate) id: String,
+    /// The endpoint to used establishing a connection to the Event Hubs service to which the scope is associated.
+    pub(crate) connection_endpoint: Url,
 
-        /// The endpoint for the Event Hubs service to which the scope is associated.
-        pub(crate) service_endpoint: Url,
+    /// The name of the Event Hub to which the scope is associated.
+    pub(crate) event_hub_name: String,
 
-        /// The endpoint to used establishing a connection to the Event Hubs service to which the scope is associated.
-        pub(crate) connection_endpoint: Url,
+    // ///   The provider to use for obtaining a token for authorization with the Event Hubs service.
+    // private CbsTokenProvider TokenProvider { get; }
 
-        /// The name of the Event Hub to which the scope is associated.
-        pub(crate) event_hub_name: String,
+    /// The type of transport to use for communication.
+    pub(crate) transport: EventHubsTransportType,
 
-        // ///   The provider to use for obtaining a token for authorization with the Event Hubs service.
-        // private CbsTokenProvider TokenProvider { get; }
+    // /// The size of the buffer used for sending information via the active transport.
+    // pub(crate) send_buffer_size_in_bytes: usize,
 
-        /// The type of transport to use for communication.
-        pub(crate) transport: EventHubsTransportType,
+    // /// The size of the buffer used for receiving information via the active transport.
+    // pub(crate) receive_buffer_size_in_bytes: usize,
 
-        // /// <summary>
-        // ///   The proxy, if any, which should be used for communication.
-        // /// </summary>
-        // ///
-        // private IWebProxy Proxy { get; }
+    /// The session dedicated for cbs auth
+    pub(crate) cbs_session_handle: SessionHandle<()>,
 
-        /// The size of the buffer used for sending information via the active transport.
-        pub(crate) send_buffer_size_in_bytes: usize,
-
-        /// The size of the buffer used for receiving information via the active transport.
-        pub(crate) receive_buffer_size_in_bytes: usize,
-
-        // /// <summary>
-        // ///   A <see cref="RemoteCertificateValidationCallback" /> delegate allowing custom logic to be considered for
-        // ///   validation of the remote certificate responsible for encrypting communication.
-        // /// </summary>
-        // ///
-        // private RemoteCertificateValidationCallback CertificateValidationCallback { get; }
-
-        /// <summary>
-        ///   The AMQP connection that is active for the current scope.
-        /// </summary>
-        ///
-        // private FaultTolerantAmqpObject<AmqpConnection> ActiveConnection { get; }\
-        pub(crate) active_connection: AmqpConnection,
+    /// CBS link for auth
+    pub(crate) cbs_link_handle: AmqpCbsLinkHandle,
 }
 
 impl AmqpConnectionScope {
     const CONNECTION_IDLE_TIMEOUT: StdDuration = StdDuration::from_secs(60);
     const AUTHORIZATION_REFRESH_TIMEOUT: StdDuration = StdDuration::from_secs(60 * 7);
+    /// The amount of buffer to apply when considering an authorization token
+    /// to be expired.  The token's actual expiration will be decreased by this
+    /// amount, ensuring that it is renewed before it has expired.
+    const AUTHORIZATION_TOKEN_EXPIRATION_BUFFER: TimeSpan =
+        TimeSpan::seconds(AUTHORIZATION_REFRESH_BUFFER_SECONDS as i64 + 2 * 60);
 
     pub(crate) async fn new(
-        service_endpoint: &Url,
+        service_endpoint: Url,
         connection_endpoint: Url,
         event_hub_name: String,
         credential: EventHubTokenCredential,
         transport_type: EventHubsTransportType,
-        operation_timeout: StdDuration,
+        idle_timeout: StdDuration,
         identifier: Option<String>
     ) -> Result<Self, AmqpConnectionScopeError> {
         // sendBufferSizeInBytes and receiveBufferSizeInBytes are not used for now. They probably
@@ -95,17 +78,37 @@ impl AmqpConnectionScope {
         // `tokio::net::TcpSocket::set_recv_buffer_size` for the TCP transport, and to
         // `tungstenite::WebSocketConfig::max_message_size` or `max_frame_size` for the WebSocket
         // transport.
+
+        // Id = identifier ?? $"{ eventHubName }-{ Guid.NewGuid().ToString("D", CultureInfo.InvariantCulture).Substring(0, 8) }";
         let id = identifier.unwrap_or_else(|| {
             let uuid = uuid::Uuid::new_v4();
-            format!("{}-{}", service_endpoint, &uuid.to_string()[0..8])
+            format!("{}-{}", event_hub_name, &uuid.to_string()[0..8])
         });
         let credential = Arc::new(credential);
 
-        let fut = Self::open_connection(service_endpoint, &connection_endpoint, transport_type, &id);
-        let connection_handle = crate::util::time::timeout(operation_timeout, fut).await??;
+        let fut = Self::open_connection(&service_endpoint, &connection_endpoint, transport_type, &id, idle_timeout.as_millis() as u32);
+        let connection_handle = crate::util::time::timeout(idle_timeout, fut).await??;
         let mut connection = AmqpConnection::new(connection_handle);
 
-        todo!()
+        let mut cbs_session_handle = Session::begin(&mut connection.handle).await?;
+
+        let cbs_client = attach_cbs_client(&mut cbs_session_handle).await?;
+        let cbs_token_provider = CbsTokenProvider::new(
+            credential.clone(),
+            Self::AUTHORIZATION_TOKEN_EXPIRATION_BUFFER,
+        );
+        let cbs_link_handle = AmqpCbsLink::spawn(cbs_token_provider, cbs_client);
+
+        Ok(Self {
+            is_disposed: false,
+            id,
+            service_endpoint,
+            connection_endpoint,
+            event_hub_name,
+            transport: transport_type,
+            cbs_session_handle,
+            cbs_link_handle,
+        })
     }
 
     async fn open_connection(
@@ -113,8 +116,8 @@ impl AmqpConnectionScope {
         connection_endpoint: &Url,
         transport_type: EventHubsTransportType,
         id: &str,
+        idle_timeout_millis: Milliseconds,
     ) -> Result<ConnectionHandle<()>, AmqpConnectionScopeError> {
-        let idle_time_out = Self::CONNECTION_IDLE_TIMEOUT.as_millis() as u32;
         let max_frame_size = amqp_constants::DEFAULT_MAX_FRAME_SIZE;
         let container_id = id;
 
@@ -124,7 +127,7 @@ impl AmqpConnectionScope {
             .alt_tls_establishment(true)
             .sasl_profile(SaslProfile::Anonymous)
             .max_frame_size(max_frame_size)
-            .idle_time_out(idle_time_out);
+            .idle_time_out(idle_timeout_millis);
 
         match transport_type {
             #[cfg(not(target_arch = "wasm32"))]
@@ -160,4 +163,17 @@ impl AmqpConnectionScope {
     ) -> Result<(SessionHandle<()>, Sender), OpenProducerError> {
         todo!()
     }
+}
+
+async fn attach_cbs_client(
+    session: &mut SessionHandle<()>,
+) -> Result<CbsClient, AmqpConnectionScopeError> {
+    CbsClient::attach(session).await.map_err(|err| match err {
+        fe2o3_amqp_management::error::AttachError::Sender(err) => {
+            AmqpConnectionScopeError::SenderAttach(err)
+        }
+        fe2o3_amqp_management::error::AttachError::Receiver(err) => {
+            AmqpConnectionScopeError::ReceiverAttach(err)
+        }
+    })
 }
--- sdk/messaging_eventhubs/src/event_hubs_transport_type.rs
+++ sdk/messaging_eventhubs/src/event_hubs_transport_type.rs
@@ -1,5 +1,8 @@
 /// Specifies the type of protocol and transport that will be used for communicating with
 /// Azure Event Hubs.
+///
+/// This is a simple enum, so copying it is cheap.
+#[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
 pub enum EventHubsTransportType
 {
     /// The connection uses the AMQP protocol over TCP
