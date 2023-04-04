266acfa22ff63ace0ab42d2a35473faf50e36627
minghuaw <michael.wu1107@gmail.com>
2023-04-04 02:09:29 -0700

    initial impl of event hub properties req/res

--- sdk/messaging_eventhubs/src/amqp/amqp_management/event_hub_properties.rs
+++ sdk/messaging_eventhubs/src/amqp/amqp_management/event_hub_properties.rs
@@ -1,38 +1,133 @@
-use fe2o3_amqp_management::{Request, Response, error::Error as ManagementError};
-use fe2o3_amqp_types::messaging::Body;
+use std::borrow::Cow;
+
+use fe2o3_amqp_management::{error::Error as ManagementError, Request, Response};
+use fe2o3_amqp_types::{
+    messaging::{ApplicationProperties},
+    primitives::{Array, OrderedMap},
+};
 use serde_amqp::Value;
+use time::OffsetDateTime;
 
-use super::READ_OPERATION_VALUE;
+use crate::{amqp::amqp_management::response_map, event_hubs_properties::EventHubProperties};
 
-pub(crate) struct EventHubPropertiesRequest {
+use super::{
+    EVENT_HUB_RESOURCE_TYPE_VALUE, READ_OPERATION_VALUE, RESOURCE_NAME_KEY, RESOURCE_TYPE_KEY,
+    SECURITY_TOKEN_KEY,
+};
 
+pub(crate) struct EventHubPropertiesRequest<'a> {
+    event_hub_name: Cow<'a, str>,
+    management_authorization_token: Cow<'a, str>,
 }
 
-impl Request for EventHubPropertiesRequest {
+fn encode_application_properties<'a>(
+    event_hub_name: String,
+    management_authorization_token: String,
+) -> Option<ApplicationProperties> {
+    // Encoding the operation entry will be taken care of by the Request trait
+    let application_properties = ApplicationProperties::builder()
+        .insert(RESOURCE_NAME_KEY, event_hub_name.as_ref())
+        .insert(RESOURCE_TYPE_KEY, EVENT_HUB_RESOURCE_TYPE_VALUE)
+        .insert(SECURITY_TOKEN_KEY, management_authorization_token.as_ref())
+        .build();
+    Some(application_properties)
+}
+
+impl<'a> Request for EventHubPropertiesRequest<'a> {
     const OPERATION: &'static str = READ_OPERATION_VALUE;
 
-    type Response = EventHubPropertiesResponse;
+    type Response = EventHubProperties;
 
     type Body = ();
 
+    fn encode_application_properties(&mut self) -> Option<ApplicationProperties> {
+        encode_application_properties(
+            self.event_hub_name.to_string(),
+            self.management_authorization_token.to_string(),
+        )
+    }
+
     fn encode_body(self) -> Self::Body {
-        todo!()
+        ()
     }
 }
 
-pub(crate) struct EventHubPropertiesResponse {
+impl<'a> Request for &'a EventHubPropertiesRequest<'a> {
+    const OPERATION: &'static str = READ_OPERATION_VALUE;
 
+    type Response = EventHubProperties;
+
+    type Body = ();
+
+    fn encode_application_properties(&mut self) -> Option<ApplicationProperties> {
+        encode_application_properties(
+            self.event_hub_name.to_string(),
+            self.management_authorization_token.to_string(),
+        )
+    }
+
+    fn encode_body(self) -> Self::Body {
+        ()
+    }
 }
 
-impl Response for EventHubPropertiesResponse {
-    type Body = Body<Value>;
+impl<'a> Request for &'a mut EventHubPropertiesRequest<'a> {
+    const OPERATION: &'static str = READ_OPERATION_VALUE;
+
+    type Response = EventHubProperties;
+
+    type Body = ();
+
+    fn encode_application_properties(&mut self) -> Option<ApplicationProperties> {
+        encode_application_properties(
+            self.event_hub_name.to_string(),
+            self.management_authorization_token.to_string(),
+        )
+    }
+
+    fn encode_body(self) -> Self::Body {
+        ()
+    }
+}
+
+type EventHubPropertiesResponseBody = OrderedMap<String, Value>;
+
+impl Response for EventHubProperties {
+    type Body = EventHubPropertiesResponseBody;
 
     const STATUS_CODE: u16 = 200;
 
     type Error = ManagementError;
 
-    fn decode_message(message: fe2o3_amqp_types::messaging::Message<Self::Body>) -> Result<Self, Self::Error> {
-        todo!()
-    }
+    fn decode_message(
+        message: fe2o3_amqp_types::messaging::Message<Self::Body>,
+    ) -> Result<Self, Self::Error> {
+        let mut body = message.body;
+        let name = match body.remove(response_map::NAME) {
+            Some(Value::String(name)) => name,
+            _ => return Err(ManagementError::DecodeError(None)),
+        };
+        let created_on = match body.remove(response_map::CREATED_AT) {
+            Some(Value::Timestamp(created_on)) => OffsetDateTime::from(created_on),
+            _ => return Err(ManagementError::DecodeError(None)),
+        };
+        let partition_ids = match body.remove(response_map::PARTITION_IDENTIFIERS) {
+            Some(Value::Array(Array(partition_ids))) | Some(Value::List(partition_ids)) => {
+                partition_ids
+                    .into_iter()
+                    .map(|id| match id {
+                        Value::String(id) => Ok(id),
+                        _ => Err(ManagementError::DecodeError(None)),
+                    })
+                    .collect::<Result<Vec<String>, ManagementError>>()?
+            }
+            _ => return Err(ManagementError::DecodeError(None)),
+        };
 
+        Ok(EventHubProperties {
+            name,
+            created_on,
+            partition_ids,
+        })
+    }
 }
--- sdk/messaging_eventhubs/src/amqp/amqp_management/mod.rs
+++ sdk/messaging_eventhubs/src/amqp/amqp_management/mod.rs
@@ -1,5 +1,9 @@
 //! Management operations for the AMQP protocol.
 
+use const_format::concatcp;
+
+use super::amqp_constants::VENDOR;
+
 pub(crate) mod event_hub_properties;
 
 // /// <summary>The location to specify for management operations.</summary>
@@ -10,18 +14,22 @@ pub(crate) mod event_hub_properties;
 
 // /// <summary>The key to use for specifying an Event Hubs resource name.</summary>
 // public const string ResourceNameKey = "name";
+const RESOURCE_NAME_KEY: &str = "name";
 
 // /// <summary>The key to use for specifying a partition. </summary>
 // public const string PartitionNameKey = "partition";
+const PARTITION_NAME_KEY: &str = "partition";
 
 // /// <summary>The key to use for specifying an operation.</summary>
 // public const string OperationKey = "operation";
 
 // /// <summary>The key to use for specifying the type of Event Hubs resource.</summary>
 // public const string ResourceTypeKey = "type";
+const RESOURCE_TYPE_KEY: &str = "type";
 
 // /// <summary>The key to use for specifying a security token.</summary>
 // public const string SecurityTokenKey = "security_token";
+const SECURITY_TOKEN_KEY: &str = "security_token";
 
 // /// <summary>The value to specify when requesting a read-based operation.</summary>
 // public const string ReadOperationValue = "READ";
@@ -29,6 +37,7 @@ const READ_OPERATION_VALUE: &str = "READ";
 
 // /// <summary>The value to specify when identifying an Event Hub resource.</summary>
 // public const string EventHubResourceTypeValue = AmqpConstants.Vendor + ":eventhub";
+const EVENT_HUB_RESOURCE_TYPE_VALUE: &str = concatcp!(VENDOR, ":eventhub");
 
 // /// <summary>The value to specify when identifying a partition resource.</summary>
 // public const string PartitionResourceTypeValue = AmqpConstants.Vendor + ":partition";
@@ -39,64 +48,71 @@ const READ_OPERATION_VALUE: &str = "READ";
 // /// </summary>
 // ///
 // public static class ResponseMap
-// {
-//     /// <summary>
-//     ///   The message property that identifies the name of a resource.
-//     /// </summary>
-//     ///
-//     public static MapKey Name { get; } = new MapKey("name");
+pub(self) mod response_map {
+    // {
+    //     /// <summary>
+    //     ///   The message property that identifies the name of a resource.
+    //     /// </summary>
+    //     ///
+    //     public static MapKey Name { get; } = new MapKey("name");
+    pub(super) const NAME: &str = "name";
 
-//     /// <summary>
-//     ///   The message property that identifies the date/time that a resource was created.
-//     /// </summary>
-//     ///
-//     public static MapKey CreatedAt { get; } = new MapKey("created_at");
+    //     /// <summary>
+    //     ///   The message property that identifies the date/time that a resource was created.
+    //     /// </summary>
+    //     ///
+    //     public static MapKey CreatedAt { get; } = new MapKey("created_at");
+    pub(super) const CREATED_AT: &str = "created_at";
 
-//     /// <summary>
-//     ///   The message property that identifies the unique identifier associated with a partition.
-//     /// </summary>
-//     ///
-//     public static MapKey PartitionIdentifier { get; } = new MapKey("partition");
+    //     /// <summary>
+    //     ///   The message property that identifies the unique identifier associated with a partition.
+    //     /// </summary>
+    //     ///
+    //     public static MapKey PartitionIdentifier { get; } = new MapKey("partition");
+    pub(super) const PARTITION_IDENTIFIER: &str = "partition";
 
-//     /// <summary>
-//     ///   The message property that identifies the set of unique identifiers for each partition of an Event Hub.
-//     /// </summary>
-//     ///
-//     public static MapKey PartitionIdentifiers { get; } = new MapKey("partition_ids");
+    //     /// <summary>
+    //     ///   The message property that identifies the set of unique identifiers for each partition of an Event Hub.
+    //     /// </summary>
+    //     ///
+    //     public static MapKey PartitionIdentifiers { get; } = new MapKey("partition_ids");
+    pub(super) const PARTITION_IDENTIFIERS: &str = "partition_ids";
 
-//     /// <summary>
-//     ///   The message property that identifies the beginning sequence number in a partition.
-//     /// </summary>
-//     ///
-//     public static MapKey PartitionBeginSequenceNumber { get; } = new MapKey("begin_sequence_number");
+    //     /// <summary>
+    //     ///   The message property that identifies the beginning sequence number in a partition.
+    //     /// </summary>
+    //     ///
+    //     public static MapKey PartitionBeginSequenceNumber { get; } = new MapKey("begin_sequence_number");
+    pub(super) const PARTITION_BEGIN_SEQUENCE_NUMBER: &str = "begin_sequence_number";
 
-//     /// <summary>
-//     ///   The message property that identifies the last sequence number enqueued for a partition.
-//     /// </summary>
-//     ///
-//     public static MapKey PartitionLastEnqueuedSequenceNumber { get; } = new MapKey("last_enqueued_sequence_number");
+    //     /// <summary>
+    //     ///   The message property that identifies the last sequence number enqueued for a partition.
+    //     /// </summary>
+    //     ///
+    //     public static MapKey PartitionLastEnqueuedSequenceNumber { get; } = new MapKey("last_enqueued_sequence_number");
+    pub(super) const PARTITION_LAST_ENQUEUED_SEQUENCE_NUMBER: &str = "last_enqueued_sequence_number";
 
-//     /// <summary>
-//     ///   The message property that identifies the last offset enqueued for a partition.
-//     /// </summary>
-//     ///
-//     public static MapKey PartitionLastEnqueuedOffset { get; } = new MapKey("last_enqueued_offset");
+    //     /// <summary>
+    //     ///   The message property that identifies the last offset enqueued for a partition.
+    //     /// </summary>
+    //     ///
+    //     public static MapKey PartitionLastEnqueuedOffset { get; } = new MapKey("last_enqueued_offset");
+    pub(super) const PARTITION_LAST_ENQUEUED_OFFSET: &str = "last_enqueued_offset";
 
-//     /// <summary>
-//     ///   The message property that identifies the last time enqueued for a partition.
-//     /// </summary>
-//     ///
-//     public static MapKey PartitionLastEnqueuedTimeUtc { get; } = new MapKey("last_enqueued_time_utc");
+    //     /// <summary>
+    //     ///   The message property that identifies the last time enqueued for a partition.
+    //     /// </summary>
+    //     ///
+    //     public static MapKey PartitionLastEnqueuedTimeUtc { get; } = new MapKey("last_enqueued_time_utc");
+    pub(super) const PARTITION_LAST_ENQUEUED_TIME_UTC: &str = "last_enqueued_time_utc";
 
-//     /// <summary>
-//     ///   The message property that identifies the date and time, in UTC, that partition information was sent from the Event Hubs service.
-//     /// </summary>
-//     ///
-//     public static MapKey PartitionRuntimeInfoRetrievalTimeUtc { get; } = new MapKey("runtime_info_retrieval_time_utc");
+    //     /// <summary>
+    //     ///   The message property that identifies the date and time, in UTC, that partition information was sent from the Event Hubs service.
+    //     /// </summary>
+    //     ///
+    //     public static MapKey PartitionRuntimeInfoRetrievalTimeUtc { get; } = new MapKey("runtime_info_retrieval_time_utc");
+    pub(super) const PARTITION_RUNTIME_INFO_RETRIEVAL_TIME_UTC: &str = "runtime_info_retrieval_time_utc";
 
-//     /// <summary>
-//     ///   The message property that identifies whether or not a partition is considered empty.
-//     /// </summary>
-//     ///
-//     public static MapKey PartitionRuntimeInfoPartitionIsEmpty { get; } = new MapKey("is_partition_empty");
-// }
+    /// The message property that identifies whether or not a partition is considered empty.
+    pub(super) const PARTITION_RUNTIME_INFO_PARTITION_IS_EMPTY: &str = "is_partition_empty";
+}
