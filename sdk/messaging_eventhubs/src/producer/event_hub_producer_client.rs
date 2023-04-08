51a1bce12ed42b7bcf5ad8e5f9f86db784addc5e
minghuaw <michael.wu1107@gmail.com>
2023-04-07 23:12:29 -0700

    get gateway producer if no partition id

--- sdk/messaging_eventhubs/src/producer/event_hub_producer_client.rs
+++ sdk/messaging_eventhubs/src/producer/event_hub_producer_client.rs
@@ -14,6 +14,7 @@ pub const MINIMUM_BATCH_SIZE_LIMIT: usize = 24;
 
 pub struct EventHubProducerClient<RP> {
     connection: EventHubConnection<AmqpClient>,
+    gateway_producer: Option<AmqpProducer<RP>>,
     producer_pool: HashMap<String, AmqpProducer<RP>>,
     options: EventHubProducerClientOptions,
     retry_policy_marker: PhantomData<RP>,
@@ -68,6 +69,7 @@ impl<RP> WithCustomRetryPolicy<RP> {
         ).await?;
         Ok(EventHubProducerClient {
             connection,
+            gateway_producer: None,
             producer_pool: HashMap::new(),
             options: client_options,
             retry_policy_marker: PhantomData,
@@ -83,6 +85,7 @@ impl<RP> WithCustomRetryPolicy<RP> {
 
         EventHubProducerClient {
             connection,
+            gateway_producer: None,
             producer_pool: HashMap::new(),
             options: client_options,
             retry_policy_marker: PhantomData,
@@ -96,35 +99,68 @@ where
 {
     async fn get_pooled_producer_mut(
         &mut self,
-        partition_id: &str,
+        partition_id: Option<&str>,
     ) -> Result<&mut AmqpProducer<RP>, azure_core::Error> {
-        if !self.producer_pool.contains_key(partition_id) {
-            let producer_identifier = Some(
-                self.options
-                    .identifier
-                    .clone()
-                    .unwrap_or(uuid::Uuid::new_v4().to_string()),
-            );
-            let requested_features = self.options.create_features();
-            let retry_policy = RP::from(self.options.retry_options.clone());
-            let partition_options = self
-                .options
-                .get_publishing_options_or_default_for_partition(Some(partition_id));
+        let producer_identifier = Some(
+            self.options
+                .identifier
+                .clone()
+                .unwrap_or(uuid::Uuid::new_v4().to_string()),
+        );
+        let requested_features = self.options.create_features();
+        let retry_policy = RP::from(self.options.retry_options.clone());
 
-            let producer = self.connection
-                .create_transport_producer::<RP>(
-                    Some(partition_id.to_string()),
-                    producer_identifier,
-                    requested_features,
-                    partition_options,
-                    retry_policy,
-                )
-                .await?;
-            self.producer_pool.insert(partition_id.to_string(), producer);
+        let partition_id = partition_id.and_then(|id| {
+            if id.is_empty() {
+                None
+            } else {
+                Some(id)
+            }
+        });
+
+        match partition_id {
+            Some(partition_id) => {
+                if !self.producer_pool.contains_key(partition_id) {
+                    let partition_options = self
+                        .options
+                        .get_publishing_options_or_default_for_partition(Some(partition_id));
+
+                    let producer = self.connection
+                        .create_transport_producer::<RP>(
+                            Some(partition_id.to_string()),
+                            producer_identifier,
+                            requested_features,
+                            partition_options,
+                            retry_policy,
+                        )
+                        .await?;
+                    self.producer_pool.insert(partition_id.to_string(), producer);
+                }
+
+                // This is safe because we just checked that the key exists.
+                Ok(self.producer_pool.get_mut(partition_id).unwrap())
+            },
+            None => {
+                if self.gateway_producer.is_none() {
+                    let partition_options = self
+                        .options
+                        .get_publishing_options_or_default_for_partition(None);
+
+                    let producer = self.connection
+                        .create_transport_producer::<RP>(
+                            None,
+                            producer_identifier,
+                            requested_features,
+                            partition_options,
+                            retry_policy,
+                        )
+                        .await?;
+                    self.gateway_producer = Some(producer);
+                }
+
+                Ok(self.gateway_producer.as_mut().unwrap())
+            }
         }
-
-        // This is safe because we just checked that the key exists.
-        Ok(self.producer_pool.get_mut(partition_id).unwrap())
     }
 
     async fn send_idempotent(&mut self, events: impl Iterator<Item = Event> + ExactSizeIterator, options: SendEventOptions) -> Result<(), azure_core::Error> {
@@ -132,13 +168,9 @@ where
     }
 
     async fn send_inner(&mut self, events: impl Iterator<Item = Event> + ExactSizeIterator + Send, options: SendEventOptions) -> Result<(), azure_core::Error> {
-        match &options.partition_id {
-            Some(partition_id) => {
-                let producer = self.get_pooled_producer_mut(&partition_id).await?;
-                producer.send(events, options).await.map_err(IntoAzureCoreError::into_azure_core_error)
-            }
-            None => todo!(),
-        }
+        let partition_id = options.partition_id.as_deref();
+        let producer = self.get_pooled_producer_mut(partition_id).await?;
+        producer.send(events, options).await.map_err(IntoAzureCoreError::into_azure_core_error)
     }
 
     pub async fn send_event(&mut self, event: impl Into<Event>, options: SendEventOptions) -> Result<(), azure_core::Error> {
