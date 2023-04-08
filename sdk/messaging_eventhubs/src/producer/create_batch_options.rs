9eaca66aab812a181eaa2fc879cab2f16754fa69
minghuaw <michael.wu1107@gmail.com>
2023-04-07 23:17:09 -0700

    added convenience methods to CreateBatchOptions

--- sdk/messaging_eventhubs/src/producer/create_batch_options.rs
+++ sdk/messaging_eventhubs/src/producer/create_batch_options.rs
@@ -4,3 +4,24 @@ pub struct CreateBatchOptions {
     pub partition_key: Option<String>,
     pub partition_id: Option<String>,
 }
+
+impl CreateBatchOptions {
+    pub fn new() -> Self {
+        Self::default()
+    }
+
+    pub fn with_max_size_in_bytes(mut self, max_size_in_bytes: u64) -> Self {
+        self.max_size_in_bytes = Some(max_size_in_bytes);
+        self
+    }
+
+    pub fn with_partition_key(mut self, partition_key: impl Into<String>) -> Self {
+        self.partition_key = Some(partition_key.into());
+        self
+    }
+
+    pub fn with_partition_id(mut self, partition_id: impl Into<String>) -> Self {
+        self.partition_id = Some(partition_id.into());
+        self
+    }
+}
