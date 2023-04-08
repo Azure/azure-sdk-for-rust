306717beeb57aae3bf557167b8279c7b0a429ede
minghuaw <michael.wu1107@gmail.com>
2023-04-07 17:44:11 -0700

    changed name to close_if_owned

--- sdk/messaging_eventhubs/src/producer/event_hub_producer_client.rs
+++ sdk/messaging_eventhubs/src/producer/event_hub_producer_client.rs
@@ -1,5 +1,9 @@
-use crate::{EventHubConnection, amqp::amqp_client::AmqpClient};
+use std::collections::HashMap;
+
+use crate::{EventHubConnection, amqp::{amqp_client::AmqpClient, amqp_producer::AmqpProducer}};
 
 pub struct EventHubProducerClient {
     connection: EventHubConnection<AmqpClient>,
 }
+
+
