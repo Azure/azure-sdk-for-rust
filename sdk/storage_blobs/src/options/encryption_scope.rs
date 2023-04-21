9412c4e79d54de7769f9d337ab0d7a979d6b9374
Leavrth <jianjun.liao@outlook.com>
2023-04-21 16:04:28 +0800

    add x-ms-encryption-scope for put_block_blob
    Signed-off-by: Leavrth <jianjun.liao@outlook.com>

---
+++ sdk/storage_blobs/src/options/encryption_scope.rs
@@ -0,0 +1,14 @@
+use azure_core::headers::{self, Header};
+
+#[derive(Debug, Clone)]
+pub struct EncryptionScope(String);
+
+impl Header for EncryptionScope {
+    fn name(&self) -> headers::HeaderName {
+        "x-ms-encryption-scope".into()
+    }
+
+    fn value(&self) -> headers::HeaderValue {
+        self.0.clone().into()
+    }
+}
