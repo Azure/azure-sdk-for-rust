06ef5200dca10cf253e0e3fff03dbc4b063e8e6f
minghuaw <michael.wu1107@gmail.com>
2023-04-23 16:24:07 -0700

    exclude crate from workspace to pass wasm test

--- sdk/messaging_eventhubs/src/lib.rs
+++ sdk/messaging_eventhubs/src/lib.rs
@@ -126,36 +126,40 @@
 #![deny(missing_docs, missing_debug_implementations)]
 
 #[macro_use]
+#[allow(unused_macros)]
 mod cfg;
 
-pub(crate) mod amqp;
-pub(crate) mod authorization;
-pub(crate) mod constants;
-pub(crate) mod core;
-pub(crate) mod diagnostics;
-pub(crate) mod event_data;
-pub(crate) mod event_hubs_connection;
-pub(crate) mod event_hubs_connection_option;
-pub(crate) mod event_hubs_connection_string_properties;
-pub(crate) mod event_hubs_properties;
-pub(crate) mod event_hubs_retry_mode;
-pub(crate) mod event_hubs_retry_options;
-pub(crate) mod event_hubs_retry_policy;
-pub(crate) mod event_hubs_transport_type;
-pub(crate) mod partition_properties;
-pub(crate) mod util;
+// TODO: support wasm32 target
+cfg_not_wasm32! {
+    pub(crate) mod amqp;
+    pub(crate) mod authorization;
+    pub(crate) mod constants;
+    pub(crate) mod core;
+    pub(crate) mod diagnostics;
+    pub(crate) mod event_data;
+    pub(crate) mod event_hubs_connection;
+    pub(crate) mod event_hubs_connection_option;
+    pub(crate) mod event_hubs_connection_string_properties;
+    pub(crate) mod event_hubs_properties;
+    pub(crate) mod event_hubs_retry_mode;
+    pub(crate) mod event_hubs_retry_options;
+    pub(crate) mod event_hubs_retry_policy;
+    pub(crate) mod event_hubs_transport_type;
+    pub(crate) mod partition_properties;
+    pub(crate) mod util;
 
-pub mod consumer;
-pub mod primitives;
-pub mod producer;
+    pub mod consumer;
+    pub mod primitives;
+    pub mod producer;
 
-pub use crate::event_data::*;
-pub use crate::event_hubs_connection::*;
-pub use crate::event_hubs_connection_option::*;
-pub use crate::event_hubs_connection_string_properties::*;
-pub use crate::event_hubs_properties::*;
-pub use crate::event_hubs_retry_mode::*;
-pub use crate::event_hubs_retry_options::*;
-pub use crate::event_hubs_retry_policy::*;
-pub use crate::event_hubs_transport_type::*;
-pub use crate::partition_properties::*;
+    pub use crate::event_data::*;
+    pub use crate::event_hubs_connection::*;
+    pub use crate::event_hubs_connection_option::*;
+    pub use crate::event_hubs_connection_string_properties::*;
+    pub use crate::event_hubs_properties::*;
+    pub use crate::event_hubs_retry_mode::*;
+    pub use crate::event_hubs_retry_options::*;
+    pub use crate::event_hubs_retry_policy::*;
+    pub use crate::event_hubs_transport_type::*;
+    pub use crate::partition_properties::*;
+}
