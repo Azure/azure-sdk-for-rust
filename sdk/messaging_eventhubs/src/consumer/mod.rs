52980284d0f1c7d7253d06bd83da12612b10dc80
minghuaw <michael.wu1107@gmail.com>
2023-04-22 17:31:48 -0700

    re-export  to match with dotnet sdk

--- sdk/messaging_eventhubs/src/consumer/mod.rs
+++ sdk/messaging_eventhubs/src/consumer/mod.rs
@@ -1,24 +1,19 @@
 //! Azure Event Hubs Consumer
 
+mod event_hub_consume_client_options;
+mod event_hub_consumer_client;
 mod event_position;
-pub use event_position::EventPosition;
-
-mod partition_context;
-pub use partition_context::PartitionContext;
-
-mod partition_event;
-pub use partition_event::PartitionEvent;
-
-mod read_event_options;
-pub use read_event_options::*;
-
 mod last_enqueued_event_properties;
-pub use last_enqueued_event_properties::LastEnqueuedEventProperties;
+mod partition_context;
+mod partition_event;
+mod read_event_options;
 
 pub mod error;
 
-mod event_hub_consumer_client;
-pub use event_hub_consumer_client::EventHubConsumerClient;
-
-mod event_hub_consume_client_options;
 pub use event_hub_consume_client_options::EventHubConsumeClientOptions;
+pub use event_hub_consumer_client::EventHubConsumerClient;
+pub use event_position::EventPosition;
+pub use last_enqueued_event_properties::LastEnqueuedEventProperties;
+pub use partition_context::PartitionContext;
+pub use partition_event::PartitionEvent;
+pub use read_event_options::ReadEventOptions;
