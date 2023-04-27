a7bf99c461399f376a80920c94aecffce1d86697
Lucasliang <nkcs_lykx@hotmail.com>
2023-04-27 14:38:49 +0800

    Modify the return type for `GetRandomBytes`.
    Signed-off-by: Lucasliang <nkcs_lykx@hotmail.com>

--- sdk/security_keyvault/src/keys/models.rs
+++ sdk/security_keyvault/src/keys/models.rs
@@ -406,10 +406,6 @@ pub struct EncryptResult {
 
 #[derive(Debug, Deserialize)]
 pub struct GetRandomBytesResult {
-    #[serde(
-        rename = "value",
-        serialize_with = "ser_base64",
-        deserialize_with = "deser_base64"
-    )]
-    pub result: Vec<u8>,
+    #[serde(rename = "value")]
+    pub result: String,
 }
