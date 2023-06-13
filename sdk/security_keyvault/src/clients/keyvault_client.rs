c61a825166191cffedf9a8ab00ac62e2c4d1feb7
Michael Marzullo <mmarzullo@bastiansolutions.com>
2023-06-13 02:04:43 -0400

    security_keyvault: Fix continuation links not working due to query string being overriden

--- sdk/security_keyvault/src/clients/keyvault_client.rs
+++ sdk/security_keyvault/src/clients/keyvault_client.rs
@@ -75,7 +75,9 @@ impl KeyvaultClient {
         let dt = OffsetDateTime::now_utc();
         let time = date::to_rfc1123(&dt);
 
-        url.set_query(Some(API_VERSION_PARAM));
+        if url.query().is_some() && !url.query().unwrap().contains(API_VERSION_PARAM) {
+            url.set_query(Some(API_VERSION_PARAM));
+        }
 
         let mut request = Request::new(url, method);
         for (k, v) in headers {
