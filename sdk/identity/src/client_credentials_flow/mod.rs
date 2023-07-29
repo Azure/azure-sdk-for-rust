6c5284e3e50117205ec5c7b820415bf69b574f04
Jason <126402326+bgdev-guy@users.noreply.github.com>
2023-07-29 11:08:25 -0400

    Update mod.rs
    identical update as the example where scope is clarified.

--- sdk/identity/src/client_credentials_flow/mod.rs
+++ sdk/identity/src/client_credentials_flow/mod.rs
@@ -16,8 +16,8 @@
 //!     let client_secret =
 //!         env::var("CLIENT_SECRET").expect("Missing CLIENT_SECRET environment variable.");
 //!     let tenant_id = env::var("TENANT_ID").expect("Missing TENANT_ID environment variable.");
-//!     let subscription_id =
-//!         env::var("SUBSCRIPTION_ID").expect("Missing SUBSCRIPTION_ID environment variable.");
+//!     let scope =
+//!         env::var("SCOPE").expect("Missing SCOPE environment variable.");
 //!
 //!     let http_client = azure_core::new_http_client();
 //!     // This will give you the final token to use in authorization.
@@ -25,7 +25,7 @@
 //!         http_client.clone(),
 //!         &client_id,
 //!         &client_secret,
-//!         &["https://management.azure.com/"],
+//!         &[&scope],
 //!         &tenant_id,
 //!     )
 //!     .await?;
