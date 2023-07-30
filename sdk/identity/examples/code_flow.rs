f879b85e365367b7e22cbff8e8b576c047faed4d
Felix Marezki <fmarezki@gmail.com>
2023-07-30 14:23:10 +0200

    wip

--- sdk/identity/examples/code_flow.rs
+++ sdk/identity/examples/code_flow.rs
@@ -8,15 +8,17 @@ use url::Url;
 async fn main() -> Result<(), Box<dyn Error>> {
     let client_id =
         ClientId::new(env::var("CLIENT_ID").expect("Missing CLIENT_ID environment variable."));
-    // let client_secret = ClientSecret::new(
-    //     env::var("CLIENT_SECRET").expect("Missing CLIENT_SECRET environment variable."),
-    // );
+    let client_secret = ClientSecret::new(
+        env::var("CLIENT_SECRET").expect("Missing CLIENT_SECRET environment variable."),
+    );
     let tenant_id = env::var("TENANT_ID").expect("Missing TENANT_ID environment variable.");
+    let subscription_id =
+        env::var("SUBSCRIPTION_ID").expect("Missing SUBSCRIPTION_ID environment variable.");
 
     // Create URL to browse for initial authorization
     let c = authorization_code_flow::start(
         client_id,
-        None,
+        Some(client_secret),
         &tenant_id,
         Url::parse("http://localhost:3003/redirect").unwrap(),
         "https://management.azure.com/user_impersonation",
@@ -43,7 +45,9 @@ async fn main() -> Result<(), Box<dyn Error>> {
     // in the subscription. Note: this way of calling the REST API
     // will be different (and easier) using other Azure Rust SDK
     // crates, this is just an example.
-    let url = Url::parse(&format!("https://management.azure.com/user_impersonation"))?;
+    let url = Url::parse(&format!(
+            "https://management.azure.com/subscriptions/{subscription_id}/providers/Microsoft.Sql/servers?api-version=2015-05-01-preview"
+        ))?;
 
     let resp = reqwest::Client::new()
         .get(url)
--- sdk/security_keyvault/src/clients/mod.rs
+++ sdk/security_keyvault/src/clients/mod.rs
@@ -1,7 +1,6 @@
 mod certificate_client;
 mod key_client;
 mod keyvault_client;
-mod keyvault_client2;
 mod pipeline;
 mod policy;
 mod secret_client;
