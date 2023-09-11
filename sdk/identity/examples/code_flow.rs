29dcf7591705bdbf8c6f7b54b6e99be158d4f1ab
Brian Caswell <bmc@shmoo.com>
2023-09-11 17:23:24 -0400

    cleanup azure_identity examples (#1369)

--- sdk/identity/examples/code_flow.rs
+++ sdk/identity/examples/code_flow.rs
@@ -1,22 +1,22 @@
-use azure_identity::*;
+use azure_core::new_http_client;
+use azure_identity::{authorization_code_flow, development::naive_redirect_server};
 use oauth2::{ClientId, ClientSecret, TokenResponse};
-use std::env;
-use std::error::Error;
+use std::{env::var, error::Error};
 use url::Url;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn Error>> {
     let client_id =
-        ClientId::new(env::var("CLIENT_ID").expect("Missing CLIENT_ID environment variable."));
+        ClientId::new(var("CLIENT_ID").expect("Missing CLIENT_ID environment variable."));
     let client_secret = ClientSecret::new(
-        env::var("CLIENT_SECRET").expect("Missing CLIENT_SECRET environment variable."),
+        var("CLIENT_SECRET").expect("Missing CLIENT_SECRET environment variable."),
     );
-    let tenant_id = env::var("TENANT_ID").expect("Missing TENANT_ID environment variable.");
+    let tenant_id = var("TENANT_ID").expect("Missing TENANT_ID environment variable.");
     let subscription_id =
-        env::var("SUBSCRIPTION_ID").expect("Missing SUBSCRIPTION_ID environment variable.");
+        var("SUBSCRIPTION_ID").expect("Missing SUBSCRIPTION_ID environment variable.");
 
     // Create URL to browse for initial authorization
-    let c = authorization_code_flow::start(
+    let code_flow = authorization_code_flow::start(
         client_id,
         Some(client_secret),
         &tenant_id,
@@ -24,20 +24,17 @@ async fn main() -> Result<(), Box<dyn Error>> {
         "https://management.azure.com/user_impersonation",
     );
 
-    println!("c == {c:?}");
-    println!("\nbrowse this url:\n{}", c.authorize_url);
+    println!("c == {code_flow:?}");
+    println!("\nbrowse this url:\n{}", code_flow.authorize_url);
 
     // Start a naive server to receive the redirect with the token. This naive server is blocking
     // so you should use something better.
-    let code = development::naive_redirect_server(&c, 3003).unwrap();
+    let code = naive_redirect_server(&code_flow, 3003).unwrap();
 
     println!("code received: {code:?}");
 
     // Exchange the token with one that can be used for authorization
-    let token = c
-        .exchange(azure_core::new_http_client(), code)
-        .await
-        .unwrap();
+    let token = code_flow.exchange(new_http_client(), code).await.unwrap();
 
     println!("token received: {token:?}");
 
