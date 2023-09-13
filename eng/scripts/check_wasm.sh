819fe9300e562f2a7ce4d0422a3a4bbdb7731380
Brian Caswell <bmc@shmoo.com>
2023-09-13 18:53:12 -0400

    split wasm checks into distinct job (#1375)
    * split wasm checks into distinct job
    
    Builds are inconsistently failing due to space issues on the build VMs.  Splitting the WASM target checks will reduce the amount of data in `target`, which help both build and cache sizes.
    
    * fix path
    
    ---------
    
    Co-authored-by: Brian Caswell <bcaswell@microsoft.com>

--- eng/scripts/check_wasm.sh
+++ eng/scripts/check_wasm.sh
@@ -6,8 +6,7 @@ cd $(dirname ${BASH_SOURCE[0]})/../../
 BUILD=${1:-stable}
 
 rustup update --no-self-update ${BUILD}
+rustup target add --toolchain ${BUILD} wasm32-unknown-unknown
 
 export RUSTFLAGS="-Dwarnings"
-cargo +${BUILD} check -p azure_core --no-default-features
-cargo +${BUILD} check --all --features azurite_workaround
-cargo +${BUILD} test --all
+cargo +${BUILD} check --target=wasm32-unknown-unknown --no-default-features
