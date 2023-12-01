dd396bf41e2581524b8e968d960d197af3c5ebb5
Brian Caswell <bcaswell@microsoft.com>
2023-11-30 20:25:17 -0500

    required changes when regenerating the test transactions

--- eng/scripts/sdk_tests.sh
+++ eng/scripts/sdk_tests.sh
@@ -12,4 +12,4 @@ rustup update --no-self-update ${BUILD}
 export RUSTFLAGS="-Dwarnings"
 cargo +${BUILD} check -p azure_core --no-default-features
 cargo +${BUILD} check --all --features azurite_workaround
-cargo +${BUILD} test --all
+cargo +${BUILD} test --all --features hmac_rust
