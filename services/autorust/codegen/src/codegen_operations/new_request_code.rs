1f9181db9135aa7a2ae3bb9be8250f0708d389f9
Brian Caswell <bcaswell@microsoft.com>
2023-12-05 16:33:04 -0500

    update the generator to use scopes rather than resource for identity

--- services/autorust/codegen/src/codegen_operations/new_request_code.rs
+++ services/autorust/codegen/src/codegen_operations/new_request_code.rs
@@ -29,11 +29,8 @@ pub(crate) struct AuthCode {}
 impl ToTokens for AuthCode {
     fn to_tokens(&self, tokens: &mut TokenStream) {
         tokens.extend(quote! {
-            let credential = this.client.token_credential();
-            let token_response = credential
-                .get_token(&this.client.scopes().join(" "))
-                .await?;
-            req.insert_header(azure_core::headers::AUTHORIZATION, format!("Bearer {}", token_response.token.secret()));
+            let bearer_token = this.client.bearer_token().await?;
+            req.insert_header(azure_core::headers::AUTHORIZATION, format!("Bearer {}", bearer_token.secret()));
         })
     }
 }
