588f4158139e496cc50e1013957294dde1d04525
Brian Caswell <bmc@shmoo.com>
2023-12-06 17:13:49 -0500

    update lints for generated crates (#1501)

--- services/mgmt/managementgroups/src/lib.rs
+++ services/mgmt/managementgroups/src/lib.rs
@@ -3,6 +3,10 @@
 #![allow(clippy::ptr_arg)]
 #![allow(clippy::large_enum_variant)]
 #![allow(clippy::derive_partial_eq_without_eq)]
+#![allow(clippy::new_without_default)]
+#![allow(rustdoc::bare_urls)]
+#![allow(rustdoc::invalid_html_tags)]
+#![allow(rustdoc::broken_intra_doc_links)]
 #[cfg(feature = "package-2023-04")]
 pub mod package_2023_04;
 #[cfg(all(feature = "package-2023-04", not(feature = "without_tag_import")))]
--- services/mgmt/powerplatform/src/lib.rs
+++ services/mgmt/powerplatform/src/lib.rs
@@ -3,6 +3,10 @@
 #![allow(clippy::ptr_arg)]
 #![allow(clippy::large_enum_variant)]
 #![allow(clippy::derive_partial_eq_without_eq)]
+#![allow(clippy::new_without_default)]
+#![allow(rustdoc::bare_urls)]
+#![allow(rustdoc::invalid_html_tags)]
+#![allow(rustdoc::broken_intra_doc_links)]
 #[cfg(feature = "package-2020-10-30-preview")]
 pub mod package_2020_10_30_preview;
 #[cfg(all(feature = "package-2020-10-30-preview", not(feature = "without_tag_import")))]
