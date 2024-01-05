e59862dbc7b7bf51f4946a86ba1a3be534adf8f1
Brian Caswell <bcaswell@microsoft.com>
2024-01-04 19:39:19 -0500

    updates to azure_mgmt_datamigration to exclude tags that fail when compiled with "--all-features"

--- services/mgmt/datamigration/src/lib.rs
+++ services/mgmt/datamigration/src/lib.rs
@@ -7,6 +7,8 @@
 #![allow(rustdoc::bare_urls)]
 #![allow(rustdoc::invalid_html_tags)]
 #![allow(rustdoc::broken_intra_doc_links)]
+#[cfg(feature = "package-2018-04-19")]
+pub mod package_2018_04_19;
 #[cfg(feature = "package-2021-06")]
 pub mod package_2021_06;
 #[cfg(feature = "package-preview-2021-10")]
@@ -15,7 +17,5 @@ pub mod package_preview_2021_10;
 pub mod package_preview_2022_01;
 #[cfg(feature = "package-preview-2022-03")]
 pub mod package_preview_2022_03;
-#[cfg(feature = "package-preview-2023-07")]
-pub mod package_preview_2023_07;
 #[cfg(all(feature = "default_tag", feature = "package-2021-06"))]
 pub use package_2021_06::*;
