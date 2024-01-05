8faef658fd9aa794e7f423fdd96134ff4ff6bff8
Brian Caswell <bcaswell@microsoft.com>
2024-01-05 16:51:25 -0500

    fix switch handling

--- eng/scripts/verify-docs.sh
+++ eng/scripts/verify-docs.sh
@@ -15,33 +15,35 @@ SERVICES=$(cd services; cargo metadata --format-version=1 --no-deps | jq -r -c '
 
 for i in ${SDK}; do
     case ${BUILD} in
-       all | sdk)
+       all | sdk )
            cargo +nightly docs-rs -p ${i}
-	   ;;
-       svc | mgmt)
+           ;;
+       svc | mgmt )
            ;;
        *)
-	   echo "unsupported build.  use all, sdk, svc, or mgmt"
-	   exit 1
-	   ;;
+           echo "unsupported build. (${BUILD}) use all, sdk, svc, or mgmt"
+           exit 1
+           ;;
     esac
 done
 
 for i in ${SERVICES}; do
     case ${BUILD} in
        all | svc )
-	   if [[ ${i} =~ "azure_svc_" ]]; then
+           if [[ ${i} =~ "azure_svc_" ]]; then
                cargo +nightly docs-rs -p ${i} --manifest-path services/Cargo.toml
-	   fi
-	   ;;
+           fi
+           ;;
        all | mgmt )
-	   if [[ ${i} == "azure_mgmt_" ]]; then
+           if [[ ${i} == "azure_mgmt_" ]]; then
                cargo +nightly docs-rs -p ${i} --manifest-path services/Cargo.toml
-	   fi
+           fi
+           ;;
+       sdk )
            ;;
        *)
-	   echo "unsupported build.  use all, sdk, svc, or mgmt"
-	   exit 1
-	   ;;
+           echo "unsupported build (${BUILD}) use all, sdk, svc, or mgmt"
+           exit 1
+           ;;
     esac
 done
