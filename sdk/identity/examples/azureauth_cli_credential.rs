fa8dd3edf6d9ba979af686c2bd24ea50396bc0a2
Kyle Rader <kyrader@microsoft.com>
2024-01-12 13:49:36 -0800

    Add example CLI for using AzureauthCliCredential

---
+++ sdk/identity/examples/azureauth_cli_credential.rs
@@ -0,0 +1,31 @@
+use azure_core::auth::TokenCredential;
+use azure_identity::AzureauthCliCredential;
+use clap::Parser;
+use std::error::Error;
+use url::Url;
+
+#[derive(Debug, Parser)]
+struct Args {
+    tenant_id: String,
+    client_id: String,
+    scopes: Vec<String>,
+}
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn Error>> {
+    let Args {
+        tenant_id,
+        client_id,
+        scopes,
+    } = Args::parse();
+
+    let creds = AzureauthCliCredential::new(tenant_id, client_id);
+    let res = creds
+        // Get an access token for Azure Devops
+        .get_token(&["499b84ac-1321-427f-aa17-267ca6975798/.default"])
+        .await?;
+
+    println!("azureauth cli response == {res:?}");
+
+    Ok(())
+}
