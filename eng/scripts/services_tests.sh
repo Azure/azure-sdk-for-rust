b92bb3b9330d13ef0eb5a598ee8c8c8b45fbc34d
Heath Stewart <heaths@microsoft.com>
2024-05-28 14:28:25 -0700

    No nightly clippy except weekly
    Also removes dependabot.yml since defaults should be sufficient. This was causing problems with the Azure/azure-sdk-for-rust-pr repo.

--- eng/scripts/services_tests.sh
+++ eng/scripts/services_tests.sh
@@ -12,5 +12,7 @@ rustup component add rustfmt clippy --toolchain ${BUILD}
 export RUSTFLAGS="-Dwarnings -Aunreachable-code -Aunused-assignments -Adead-code -Aclippy::new-without-default -Aclippy::unnecessary_to_owned"
 cargo +${BUILD} check --manifest-path services/Cargo.toml --all
 cargo +${BUILD} check --manifest-path services/Cargo.toml --examples
-cargo +${BUILD} clippy --manifest-path services/Cargo.toml --all
+if [ -z $NO_CLIPPY ]; then
+    cargo +${BUILD} clippy --manifest-path services/Cargo.toml --all
+fi
 cargo +${BUILD} fmt --manifest-path services/Cargo.toml --all -- --check
