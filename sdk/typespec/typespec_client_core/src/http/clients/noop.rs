adc2a227a171d808bd9cc9f228dbcf4475424225
Heath Stewart <heaths@microsoft.com>
2024-08-14 14:40:24 -0700

    Fix resolution errors on Noop client

--- sdk/typespec/typespec_client_core/src/http/clients/noop.rs
+++ sdk/typespec/typespec_client_core/src/http/clients/noop.rs
@@ -1,21 +1,25 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 
+use crate::{
+    error::Result,
+    http::{RawResponse, Request},
+};
 use async_trait::async_trait;
 
 #[derive(Debug)]
 struct NoopClient;
 
-pub(crate) fn new_noop_client() -> std::sync::Arc<dyn crate::HttpClient> {
+pub(crate) fn new_noop_client() -> std::sync::Arc<dyn super::HttpClient> {
     std::sync::Arc::new(NoopClient)
 }
 
 // TODO: We probably don't want to limit this to wasm32 since there will be wasm environments with threads. This should be a feature flag.
 #[cfg_attr(target_arch = "wasm32", async_trait(?Send))]
 #[cfg_attr(not(target_arch = "wasm32"), async_trait)]
-impl crate::HttpClient for NoopClient {
+impl super::HttpClient for NoopClient {
     #[allow(clippy::diverging_sub_expression)]
-    async fn execute_request(&self, request: &crate::Request) -> crate::Result<crate::RawResponse> {
+    async fn execute_request(&self, request: &Request) -> Result<RawResponse> {
         panic!(
             "A request was called on the default http client `NoopClient`.\
 	This client does nothing but panic. Make sure to enable an http\
