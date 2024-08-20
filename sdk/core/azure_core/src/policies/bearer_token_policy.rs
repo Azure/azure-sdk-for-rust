549a9c41dfa1a78dd702e134291bed4b5a74c8e6
vincenttran-msft <vincenttran@microsoft.com>
2024-08-19 17:12:23 -0700

    POC 1

--- sdk/core/azure_core/src/policies/bearer_token_policy.rs
+++ sdk/core/azure_core/src/policies/bearer_token_policy.rs
@@ -5,13 +5,19 @@ use crate::{
     Context, Request,
 };
 use async_trait::async_trait;
-use std::sync::Arc;
+use std::sync::{Arc, Mutex};
+use time::OffsetDateTime;
 
 #[derive(Debug, Clone)]
 pub struct BearerTokenCredentialPolicy {
     credential: Arc<dyn TokenCredential>,
     scopes: Vec<String>,
+    access_token: Arc<Mutex<String>>,
+    last_refresh_time: Arc<Mutex<i64>>,
 }
+
+/// Default timeout in seconds before refreshing a new token.
+const DEFAULT_REFRESH_TIME: i64 = 120;
 
 impl BearerTokenCredentialPolicy {
     pub fn new<A, B>(credential: Arc<dyn TokenCredential>, scopes: A) -> Self
@@ -22,7 +28,30 @@ impl BearerTokenCredentialPolicy {
         Self {
             credential,
             scopes: scopes.into_iter().map(|s| s.into()).collect(),
+            access_token: Arc::new(Mutex::new("UNSET".to_string())), // Need a better default here or use Option
+            last_refresh_time: Arc::new(Mutex::new(0)), // Need a better default here aswell
         }
+    }
+
+    fn update_data(&self, new_access_token: &str) {
+        let mut my_access_token = self.access_token.lock().unwrap();
+        *my_access_token = new_access_token.to_string();
+        let mut my_last_refresh_time = self.last_refresh_time.lock().unwrap();
+        *my_last_refresh_time = OffsetDateTime::now_utc().unix_timestamp();
+    }
+
+    fn should_refresh(&self) -> bool {
+        let current_time = OffsetDateTime::now_utc().unix_timestamp();
+        let my_last_refresh_time = self.last_refresh_time.lock().unwrap();
+        if current_time - *my_last_refresh_time > DEFAULT_REFRESH_TIME {
+            return true;
+        }
+        return false;
+    }
+
+    fn access_token(&self) -> String {
+        let my_access_token = self.access_token.lock().unwrap();
+        (*my_access_token.clone()).to_string()
     }
 }
 
@@ -40,9 +69,18 @@ impl Policy for BearerTokenCredentialPolicy {
             .iter()
             .map(|s| s.as_str())
             .collect::<Vec<&str>>();
-        let access_token = self.credential.get_token(&scopes).await?;
-        let token = access_token.token.secret();
+        // let access_token = self.credential.get_token(&scopes).await?;
+        // let token = access_token.token.secret();
+        // request.insert_header(AUTHORIZATION, format!("Bearer {token}"));
 
+        // Test Stuff
+        let should_update = self.should_refresh();
+        if should_update {
+            let access_token = self.credential.get_token(&scopes).await?;
+            let token = access_token.token.secret();
+            self.update_data(token)
+        }
+        let token = String::from(self.access_token());
         request.insert_header(AUTHORIZATION, format!("Bearer {token}"));
 
         next[0].send(ctx, request, &next[1..]).await
