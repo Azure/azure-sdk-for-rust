7b4b3ac44d4f80ff348d52b0c1b4b6384d4521c3
Patrick Hallisey <pahallis@microsoft.com>
2024-08-26 17:31:18 -0700

    Remove RustToolchain redundant naming

--- eng/scripts/Language-Settings.ps1
+++ eng/scripts/Language-Settings.ps1
@@ -10,29 +10,30 @@ function Get-AllPackageInfoFromRepo ([string] $ServiceDirectory) {
     $allPackageProps = @()
     Push-Location $RepoRoot
     try {
-        $searchPath = 'sdk'
+        $sdkPath = Join-Path $RepoRoot 'sdk' -Resolve
 
         if ($ServiceDirectory -and $ServiceDirectory -ne 'auto') {
             $searchPath = Join-Path 'sdk' $ServiceDirectory
         }
 
-        $cargoTomlFiles = Get-ChildItem -Path $searchPath -Recurse -Filter 'Cargo.toml' -ErrorAction SilentlyContinue
+        $packages = cargo metadata --format-version 1
+        | ConvertFrom-Json -AsHashtable
+        | Select-Object -ExpandProperty packages
+        | Where-Object { $_.manifest_path.StartsWith($sdkPath) }
 
         $packageManifests = @{}
-        foreach ($file in $cargoTomlFiles) {
-            $manifest = cargo read-manifest --manifest-path $file | ConvertFrom-Json -AsHashtable
-
-            if ($manifest.manifest_path -replace '\\', '/' -match '/sdk/([^/]+)/') {
-                $manifest.ServiceDirectoryName = $Matches[1]
+        foreach ($package in $packages) {
+            if ($package.manifest_path -replace '\\', '/' -match '/sdk/([^/]+)/') {
+                $package.ServiceDirectoryName = $Matches[1]
             }
             else {
                 # ignore manifests that are not in a service directory
                 continue
             }
 
-            $manifest.RelativePath = (Split-Path $manifest.manifest_path -Parent).Replace($RepoRoot, "").SubString(1)
-            $manifest.DependentPackages = @()
-            $packageManifests[$manifest.name] = $manifest
+            $package.RelativePath = (Split-Path $package.manifest_path -Parent).Replace($RepoRoot, "").SubString(1)
+            $package.DependentPackages = @()
+            $packageManifests[$package.name] = $package
         }
     }
     catch {
@@ -44,22 +45,22 @@ function Get-AllPackageInfoFromRepo ([string] $ServiceDirectory) {
     }
 
     # Invert the manifest dependency graph
-    foreach ($manifest in $packageManifests.Values) {
-        foreach ($dependency in $manifest.dependencies) {
+    foreach ($package in $packageManifests.Values) {
+        foreach ($dependency in $package.dependencies) {
             $dependencyManifest = $packageManifests[$dependency.name]
             if ($dependencyManifest) {
-                $dependencyManifest.DependentPackages += $manifest
+                $dependencyManifest.DependentPackages += $package
             }
         }
     }
 
     # Flatten the dependency graph recursively
-    function GetDependentPackages($manifest, $dependantPackages) {
+    function GetDependentPackages($package, $dependantPackages) {
         if (!$dependantPackages) {
             $dependantPackages = @()
         }
 
-        foreach ($dependency in $manifest.DependentPackages) {
+        foreach ($dependency in $package.DependentPackages) {
             if ($dependantPackages.Contains($dependency)) {
                 continue
             }
@@ -70,20 +71,20 @@ function Get-AllPackageInfoFromRepo ([string] $ServiceDirectory) {
         return $dependantPackages;
     }
 
-    foreach ($manifest in $packageManifests.Values) {
-        $absolutePath = Split-Path $manifest.manifest_path -Parent -Resolve
-        $pkgProp = [PackageProps]::new($manifest.name, $manifest.version, $absolutePath, $manifest.ServiceDirectoryName)
+    foreach ($package in $packageManifests.Values) {
+        $absolutePath = Split-Path $package.manifest_path -Parent -Resolve
+        $pkgProp = [PackageProps]::new($package.name, $package.version, $absolutePath, $package.ServiceDirectoryName)
         $pkgProp.IsNewSdk = $true
-        $pkgProp.ArtifactName = $manifest.name
+        $pkgProp.ArtifactName = $package.name
 
-        if ($manifest.name -match "mgmt") {
+        if ($package.name -match "mgmt") {
             $pkgProp.SdkType = "mgmt"
         }
         else {
             $pkgProp.SdkType = "client"
         }
 
-        $pkgProp.DependentPackages = GetDependentPackages $manifest | Select-Object -ExpandProperty RelativePath
+        $pkgProp.DependentPackages = GetDependentPackages $package | Select-Object -ExpandProperty RelativePath
 
         $allPackageProps += $pkgProp
     }
