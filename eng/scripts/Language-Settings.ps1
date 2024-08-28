4ff0cd28d1f93b43b871a523adf6ac678f6705ad
Patrick Hallisey <hallipr@gmail.com>
2024-08-28 14:58:33 -0700

    Add ADO pipeline scaffolding (#1762)

---
+++ eng/scripts/Language-Settings.ps1
@@ -0,0 +1,93 @@
+$Language = "rust"
+$LanguageDisplayName = "Rust"
+$PackageRepository = "crates.io"
+$packagePattern = "Cargo.toml"
+#$MetadataUri = "https://raw.githubusercontent.com/Azure/azure-sdk/main/_data/releases/latest/rust-packages.csv"
+$GithubUri = "https://github.com/Azure/azure-sdk-for-rust"
+$PackageRepositoryUri = "https://crates.io/crates"
+
+function Get-AllPackageInfoFromRepo ([string] $ServiceDirectory) {
+  $allPackageProps = @()
+  Push-Location $RepoRoot
+  try {
+    $searchPath = Join-Path $RepoRoot 'sdk' -Resolve
+
+    if ($ServiceDirectory -and $ServiceDirectory -ne 'auto') {
+      $searchPath = Join-Path 'sdk' $ServiceDirectory
+    }
+
+    $packages = cargo metadata --format-version 1
+    | ConvertFrom-Json -AsHashtable
+    | Select-Object -ExpandProperty packages
+    | Where-Object { $_.manifest_path.StartsWith($searchPath) }
+
+    $packageManifests = @{}
+    foreach ($package in $packages) {
+      if ($package.manifest_path -replace '\\', '/' -match '/sdk/([^/]+)/') {
+        $package.ServiceDirectoryName = $Matches[1]
+      }
+      else {
+        # ignore manifests that are not in a service directory
+        continue
+      }
+
+      $package.RelativePath = (Split-Path $package.manifest_path -Parent).Replace($RepoRoot, "").SubString(1)
+      $package.DependentPackages = @()
+      $packageManifests[$package.name] = $package
+    }
+  }
+  catch {
+    # This is soft error and failure is expected for python metapackages
+    LogError "Failed to get all package properties"
+  }
+  finally {
+    Pop-Location
+  }
+
+  # Invert the manifest dependency graph
+  foreach ($package in $packageManifests.Values) {
+    foreach ($dependency in $package.dependencies) {
+      $dependencyManifest = $packageManifests[$dependency.name]
+      if ($dependencyManifest) {
+        $dependencyManifest.DependentPackages += $package
+      }
+    }
+  }
+
+  # Flatten the dependency graph recursively
+  function GetDependentPackages($package, $dependantPackages) {
+    if (!$dependantPackages) {
+      $dependantPackages = @()
+    }
+
+    foreach ($dependency in $package.DependentPackages) {
+      if ($dependantPackages.Contains($dependency)) {
+        continue
+      }
+      $dependantPackages += $dependency
+      [array]$dependantPackages = GetDependentPackages $dependency $dependantPackages
+    }
+
+    return $dependantPackages;
+  }
+
+  foreach ($package in $packageManifests.Values) {
+    $absolutePath = Split-Path $package.manifest_path -Parent -Resolve
+    $pkgProp = [PackageProps]::new($package.name, $package.version, $absolutePath, $package.ServiceDirectoryName)
+    $pkgProp.IsNewSdk = $true
+    $pkgProp.ArtifactName = $package.name
+
+    if ($package.name -match "mgmt") {
+      $pkgProp.SdkType = "mgmt"
+    }
+    else {
+      $pkgProp.SdkType = "client"
+    }
+
+    $pkgProp.DependentPackages = GetDependentPackages $package | Select-Object -ExpandProperty RelativePath
+
+    $allPackageProps += $pkgProp
+  }
+
+  return $allPackageProps
+}
---
+++ eng/scripts/Test-Packages.ps1
@@ -0,0 +1,64 @@
+#Requires -Version 7.0
+
+param(
+  [string]$Toolchain = 'stable',
+  [bool]$UnitTests = $true,
+  [bool]$FunctionalTests = $true,
+  [string]$PackageInfoPath
+)
+
+$ErrorActionPreference = 'Stop'
+Set-StrictMode -Version 2.0
+
+. "$PSScriptRoot\..\common\scripts\common.ps1"
+. (Join-Path $EngCommonScriptsDir "Helpers" CommandInvocation-Helpers.ps1)
+
+Write-Host "Testing packages with
+    Toolchain: '$Toolchain'
+    UnitTests: '$UnitTests'
+    FunctionalTests: '$FunctionalTests'
+    PackageInfoPath: '$PackageInfoPath'"
+
+if ($PackageInfoPath) {
+  if (!(Test-Path $PackageInfoPath)) {
+    Write-Error "Package info path '$PackageInfoPath' does not exist."
+    exit 1
+  }
+
+  $pacakgesToTest = Get-ChildItem $PackageInfoPath -Filter "*.json" -Recurse
+  | ConvertFrom-Json
+}
+else {
+  $pacakgesToTest = Get-AllPackagesInRepo
+}
+
+Write-Host "Testing packages:"
+foreach ($package in $pacakgesToTest) {
+  Write-Host "  '$($package.Name)'"
+}
+
+Write-Host "Setting RUSTFLAGS to '-Dwarnings'"
+$env:RUSTFLAGS = "-Dwarnings"
+
+$verifyDependenciesScript = Join-Path $RepoRoot '/eng/scripts/verify-dependencies.rs' -Resolve
+
+foreach ($package in $pacakgesToTest) {
+  Push-Location $package.DirectoryPath
+  try {
+    Write-Host "`n`nTesting package: '$($package.Name)' in directory: '$($package.DirectoryPath)'`n"
+
+    Invoke-LoggedCommand "cargo +$Toolchain build --keep-going"
+    Write-Host "`n`n"
+    Invoke-LoggedCommand "cargo +$Toolchain test --lib --no-fail-fast"
+    Write-Host "`n`n"
+    Invoke-LoggedCommand "cargo +$Toolchain test --doc --no-fail-fast"
+
+    if ($Toolchain -eq 'nightly') {
+      Write-Host "`n`n"
+      Invoke-LoggedCommand "cargo +nightly -Zscript $verifyDependenciesScript"
+    }
+  }
+  finally {
+    Pop-Location
+  }
+}
