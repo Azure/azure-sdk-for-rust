1c8556160d60e91dea12359b77f7cb2fd6bd5500
Horatiu Damian <hdamian@gmail.com>
2024-09-17 14:11:07 +0300

    fix: relax date time type to string to allow parsing of SystemData for container apps

--- services/mgmt/app/src/package_preview_2023_08/models.rs
+++ services/mgmt/app/src/package_preview_2023_08/models.rs
@@ -6733,8 +6733,8 @@ pub struct SystemData {
     #[serde(rename = "createdByType", default, skip_serializing_if = "Option::is_none")]
     pub created_by_type: Option<system_data::CreatedByType>,
     #[doc = "The timestamp of resource creation (UTC)."]
-    #[serde(rename = "createdAt", default, with = "azure_core::date::rfc3339::option")]
-    pub created_at: Option<time::OffsetDateTime>,
+    #[serde(rename = "createdAt")]
+    pub created_at: Option<String>,
     #[doc = "The identity that last modified the resource."]
     #[serde(rename = "lastModifiedBy", default, skip_serializing_if = "Option::is_none")]
     pub last_modified_by: Option<String>,
@@ -6742,8 +6742,8 @@ pub struct SystemData {
     #[serde(rename = "lastModifiedByType", default, skip_serializing_if = "Option::is_none")]
     pub last_modified_by_type: Option<system_data::LastModifiedByType>,
     #[doc = "The timestamp of resource last modification (UTC)"]
-    #[serde(rename = "lastModifiedAt", default, with = "azure_core::date::rfc3339::option")]
-    pub last_modified_at: Option<time::OffsetDateTime>,
+    #[serde(rename = "lastModifiedAt")]
+    pub last_modified_at: Option<String>,
 }
 impl SystemData {
     pub fn new() -> Self {
