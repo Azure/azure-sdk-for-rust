6a7faef6a47fc5c1c11ee49b7191e2f9c8f95113
Ashley Stanton-Nurse <ashleyst@microsoft.com>
2024-10-25 10:16:18 -0700

    [Cosmos] Refactoring how the resource type and link are passed down to the auth policy (#1861)

--- sdk/cosmos/azure_data_cosmos/src/pipeline/mod.rs
+++ sdk/cosmos/azure_data_cosmos/src/pipeline/mod.rs
@@ -2,49 +2,67 @@
 // Licensed under the MIT License.
 
 mod authorization_policy;
+mod signature_target;
 
 use std::sync::Arc;
 
-pub(crate) use authorization_policy::{AuthorizationPolicy, ResourceType};
+pub use authorization_policy::AuthorizationPolicy;
 use azure_core::{Context, Pager, Request};
 use serde::de::DeserializeOwned;
 use typespec_client_core::http::PagerResult;
+use url::Url;
 
-use crate::{constants, Query};
+use crate::{constants, resource_context::ResourceLink, Query};
 
 /// Newtype that wraps an Azure Core pipeline to provide a Cosmos-specific pipeline which configures our authorization policy and enforces that a [`ResourceType`] is set on the context.
 #[derive(Debug, Clone)]
-pub struct CosmosPipeline(azure_core::Pipeline);
+pub struct CosmosPipeline {
+    pub endpoint: Url,
+    pipeline: azure_core::Pipeline,
+}
 
 impl CosmosPipeline {
     pub fn new(
+        endpoint: Url,
         auth_policy: AuthorizationPolicy,
         client_options: azure_core::ClientOptions,
     ) -> Self {
-        CosmosPipeline(azure_core::Pipeline::new(
-            option_env!("CARGO_PKG_NAME"),
-            option_env!("CARGO_PKG_VERSION"),
-            client_options,
-            Vec::new(),
-            vec![Arc::new(auth_policy)],
-        ))
+        CosmosPipeline {
+            endpoint,
+            pipeline: azure_core::Pipeline::new(
+                option_env!("CARGO_PKG_NAME"),
+                option_env!("CARGO_PKG_VERSION"),
+                client_options,
+                Vec::new(),
+                vec![Arc::new(auth_policy)],
+            ),
+        }
+    }
+
+    /// Creates a [`Url`] out of the provided [`ResourceLink`]
+    ///
+    /// This is a little backwards, ideally we'd accept [`ResourceLink`] in the [`CosmosPipeline::send`] method,
+    /// but we need callers to be able to build an [`azure_core::Request`] so they need to be able to get the full URL.
+    /// This allows the clients to hold a single thing representing the "connection" to a Cosmos DB account though.
+    pub fn url(&self, link: &ResourceLink) -> Url {
+        link.url(&self.endpoint)
     }
 
     pub async fn send<T>(
         &self,
         ctx: azure_core::Context<'_>,
         request: &mut azure_core::Request,
-        resource_type: ResourceType,
+        resource_link: ResourceLink,
     ) -> azure_core::Result<azure_core::Response<T>> {
-        let ctx = ctx.with_value(resource_type);
-        self.0.send(&ctx, request).await
+        let ctx = ctx.with_value(resource_link);
+        self.pipeline.send(&ctx, request).await
     }
 
     pub fn send_query_request<T: DeserializeOwned>(
         &self,
         query: Query,
         mut base_request: Request,
-        resource_type: ResourceType,
+        resource_link: ResourceLink,
     ) -> azure_core::Result<Pager<T>> {
         base_request.insert_header(constants::QUERY, "True");
         base_request.add_mandatory_header(&constants::QUERY_CONTENT_TYPE);
@@ -52,8 +70,8 @@ impl CosmosPipeline {
 
         // We have to double-clone here.
         // First we clone the pipeline to pass it in to the closure
-        let pipeline = self.0.clone();
-        let context = Context::new().with_value(resource_type);
+        let pipeline = self.pipeline.clone();
+        let context = Context::new().with_value(resource_link);
         Ok(Pager::from_callback(move |continuation| {
             // Then we have to clone it again to pass it in to the async block.
             // This is because Pageable can't borrow any data, it has to own it all.
--- sdk/cosmos/azure_data_cosmos/src/utils.rs
+++ sdk/cosmos/azure_data_cosmos/src/utils.rs
@@ -1,26 +1,6 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 
-use url::Url;
-
-/// Appends new path segments to the target [`Url`].
-pub trait AppendPathSegments: Clone {
-    fn append_path_segments<'a>(&mut self, segments: impl IntoIterator<Item = &'a str>);
-
-    fn with_path_segments<'a>(&self, segments: impl IntoIterator<Item = &'a str>) -> Self {
-        let mut new = self.clone();
-        new.append_path_segments(segments);
-        new
-    }
-}
-
-impl AppendPathSegments for Url {
-    fn append_path_segments<'a>(&mut self, segments: impl IntoIterator<Item = &'a str>) {
-        let mut path_segments = self
-            .path_segments_mut()
-            .expect("the URL must not be a 'cannot-be-a-base' URL");
-        for segment in segments {
-            path_segments.push(segment.as_ref());
-        }
-    }
+pub fn url_encode(s: impl AsRef<[u8]>) -> String {
+    url::form_urlencoded::byte_serialize(s.as_ref()).collect::<String>()
 }
