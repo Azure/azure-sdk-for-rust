fc05796de00b1dc291c75f31fc9862efa4d90daa
Harsha Nalluru <sanallur@microsoft.com>
2025-01-06 19:49:57 +0000

    start from rustdoc output

--- sdk/temp-project/docs/src/lib.rs
+++ sdk/temp-project/docs/src/lib.rs
@@ -1,7 +1,13 @@
-//! Here are some crate-level docs!
+/// This is a sample module
+pub mod sample_module {
+    /// This is a sample function
+    pub fn sample_function() {
+        // function implementation
+    }
 
-/// Here are some docs for `some_fn`!
-pub fn some_fn() {}
-
-/// Here are some docs for `SomeStruct`!
-pub struct SomeStruct;
+    /// This is a sample struct
+    pub struct SampleStruct {
+        /// This is a sample field
+        pub field: i32,
+    }
+}
