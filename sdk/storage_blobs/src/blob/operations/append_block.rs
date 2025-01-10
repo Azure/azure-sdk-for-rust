dce289dc72dbe4efbf375f2c6efb6dd1c435e3f5
Leon Hartley <lhartley97@gmail.com>
2025-01-10 21:16:20 +0000

    Create new response struct for AppendBlock, exposing ETag, blob_append_offset and blob_committed_block_count (#1965)
    * Create bespoke response struct for AppendBlock operation exposing ETag
    
    * Expose blob_append_offset and blob_commited_block_count
    
    * Cargo fmt

--- sdk/storage_blobs/src/blob/operations/append_block.rs
+++ sdk/storage_blobs/src/blob/operations/append_block.rs
@@ -1,5 +1,8 @@
-use crate::{blob::operations::put_block::PutBlockResponse, prelude::*};
-use azure_core::{headers::*, prelude::*, Body};
+use crate::prelude::*;
+use azure_core::{headers::*, prelude::*, Body, RequestId};
+use azure_storage::headers::consistency_from_headers;
+use azure_storage::{ConsistencyCRC64, ConsistencyMD5};
+use time::OffsetDateTime;
 
 operation! {
     AppendBlock,
@@ -39,9 +42,42 @@ impl AppendBlockBuilder {
 
             let response = self.client.send(&mut self.context, &mut request).await?;
 
-            PutBlockResponse::from_headers(response.headers())
+            AppendBlockResponse::from_headers(response.headers())
         })
     }
 }
 
-type AppendBlockResponse = PutBlockResponse;
+#[derive(Debug, Clone, PartialEq, Eq)]
+pub struct AppendBlockResponse {
+    pub etag: String,
+    pub content_md5: Option<ConsistencyMD5>,
+    pub content_crc64: Option<ConsistencyCRC64>,
+    pub request_id: RequestId,
+    pub date: OffsetDateTime,
+    pub request_server_encrypted: bool,
+    pub blob_append_offset: u64,
+    pub blob_committed_block_count: u64,
+}
+
+impl AppendBlockResponse {
+    pub(crate) fn from_headers(headers: &Headers) -> azure_core::Result<AppendBlockResponse> {
+        let etag = etag_from_headers(headers)?;
+        let (content_md5, content_crc64) = consistency_from_headers(headers)?;
+        let request_id = request_id_from_headers(headers)?;
+        let date = date_from_headers(headers)?;
+        let request_server_encrypted = request_server_encrypted_from_headers(headers)?;
+        let blob_append_offset = headers.get_as(&BLOB_APPEND_OFFSET)?;
+        let blob_committed_block_count = headers.get_as(&BLOB_COMMITTED_BLOCK_COUNT)?;
+
+        Ok(AppendBlockResponse {
+            etag,
+            content_md5,
+            content_crc64,
+            request_id,
+            date,
+            request_server_encrypted,
+            blob_append_offset,
+            blob_committed_block_count,
+        })
+    }
+}
