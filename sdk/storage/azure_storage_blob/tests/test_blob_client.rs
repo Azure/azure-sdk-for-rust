570f117ff23ae0116d6dd92123a932eb9ca9388e
Vincent Tran <vincenttran@microsoft.com>
2025-01-22 16:56:32 -0800

    Refactor off Storage feedback

--- sdk/storage/azure_storage_blob/tests/test_blob_client.rs
+++ sdk/storage/azure_storage_blob/tests/test_blob_client.rs
@@ -1,6 +1,7 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 
+use azure_core::error::ErrorKind::HttpResponse;
 use azure_core_test::recorded;
 use azure_identity::DefaultAzureCredentialBuilder;
 use azure_storage_blob::blob_blob_client::BlobBlobClientGetPropertiesOptions;
@@ -12,15 +13,19 @@ use std::error::Error;
 #[cfg(test)]
 mod tests {
 
+    use azure_core::StatusCode;
+
     use super::*;
 
     #[recorded::test(live)]
     async fn test_get_blob_properties() -> Result<(), Box<dyn Error>> {
-        let storage_account_name =
-            env::var("STORAGE_ACCOUNT_NAME").unwrap_or("storagename".to_string());
+        // Setup
+        let storage_account_name = env::var("AZURE_STORAGE_ACCOUNT_NAME")
+            .expect("Failed to get environment variable: AZURE_STORAGE_ACCOUNT_NAME");
         let endpoint = format!("https://{}.blob.core.windows.net/", storage_account_name);
         let credential = DefaultAzureCredentialBuilder::default().build()?;
 
+        // Act
         let blob_client = BlobClient::new(
             endpoint,
             String::from("testcontainer"),
@@ -32,17 +37,24 @@ mod tests {
             .get_blob_properties(Some(BlobBlobClientGetPropertiesOptions::default()))
             .await;
 
+        // Assert
         assert!(response.is_ok());
         Ok(())
     }
 
     #[recorded::test(live)]
     async fn test_get_blob_properties_invalid_container() -> Result<(), Box<dyn Error>> {
-        let storage_account_name =
-            env::var("STORAGE_ACCOUNT_NAME").unwrap_or("storagename".to_string());
+        // Setup
+        let storage_account_name = env::var("AZURE_STORAGE_ACCOUNT_NAME")
+            .expect("Failed to get environment variable: AZURE_STORAGE_ACCOUNT_NAME");
         let endpoint = format!("https://{}.blob.core.windows.net/", storage_account_name);
         let credential = DefaultAzureCredentialBuilder::default().build()?;
+        let expected_error = HttpResponse {
+            status: StatusCode::NotFound,
+            error_code: Some(String::from("ContainerNotFound")),
+        };
 
+        // Act
         let blob_client = BlobClient::new(
             endpoint,
             String::from("missingcontainer"),
@@ -54,7 +66,10 @@ mod tests {
             .get_blob_properties(Some(BlobBlobClientGetPropertiesOptions::default()))
             .await;
 
+        // Assert
         assert!(response.is_err());
+        assert_eq!(expected_error, response.unwrap_err().kind().clone());
+
         Ok(())
     }
 }
