70f8173267a6ff7ba951887b280f0587e6603917
Heath Stewart <heaths@microsoft.com>
2025-02-04 15:13:40 -0800

    Add delete + purge test to Key Vault Secrets

--- sdk/keyvault/azure_security_keyvault_secrets/tests/secret_client.rs
+++ sdk/keyvault/azure_security_keyvault_secrets/tests/secret_client.rs
@@ -3,12 +3,15 @@
 
 #![cfg_attr(target_arch = "wasm32", allow(unused_imports))]
 
-use azure_core::Result;
-use azure_core_test::{recorded, TestContext};
+mod common;
+
+use azure_core::{Result, StatusCode};
+use azure_core_test::{recorded, TestContext, TestMode};
 use azure_security_keyvault_secrets::{
     models::{SecretSetParameters, SecretUpdateParameters},
     ResourceExt as _, SecretClient, SecretClientOptions,
 };
+use common::Retry;
 use futures::TryStreamExt;
 use std::collections::HashMap;
 
@@ -150,6 +153,67 @@ async fn list_secrets(ctx: TestContext) -> Result<()> {
         }
     }
     assert!(names.is_empty());
+
+    Ok(())
+}
+
+#[recorded::test]
+async fn purge_secret(ctx: TestContext) -> Result<()> {
+    let recording = ctx.recording();
+    recording.remove_sanitizers(REMOVE_SANITIZERS).await?;
+
+    let mut options = SecretClientOptions::default();
+    recording.instrument(&mut options.client_options);
+
+    let client = SecretClient::new(
+        recording.var("AZURE_KEYVAULT_URL", None).as_str(),
+        recording.credential(),
+        Some(options),
+    )?;
+
+    // Create a secret.
+    let secret = client
+        .set_secret(
+            "purge-secret",
+            SecretSetParameters {
+                value: Some("secret-value".into()),
+                ..Default::default()
+            }
+            .try_into()?,
+            None,
+        )
+        .await?
+        .into_body()
+        .await?;
+
+    // Delete the secret.
+    let name = secret.resource_id()?.name;
+    client.delete_secret(name.as_ref(), None).await?;
+
+    // Because deletes may not happen right away, try purging in a loop.
+    let mut retry = match recording.test_mode() {
+        TestMode::Playback => Retry::immediate(),
+        _ => Retry::progressive(None),
+    };
+
+    loop {
+        match client.purge_deleted_secret(name.as_ref(), None).await {
+            Ok(_) => {
+                println!("{name} has been purged");
+                break;
+            }
+            Err(err) if matches!(err.http_status(), Some(StatusCode::Conflict)) => {
+                println!(
+                    "Retrying in {} seconds",
+                    retry.duration().unwrap_or_default().as_secs_f32()
+                );
+                if retry.next().await.is_none() {
+                    return Err(err);
+                }
+            }
+            Err(err) => return Err(err),
+        }
+    }
 
     Ok(())
 }
