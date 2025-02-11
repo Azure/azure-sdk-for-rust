13f073988787fbf06f0106a9fcf5cdcd1ce382f5
Heath Stewart <heaths@microsoft.com>
2025-02-11 11:03:44 -0800

    Add Key Vault Keys integration tests (#2092)
    Also refactors out some test utilities into a new
    azure_security_keyvault_test module with `publish = false`.

--- sdk/keyvault/azure_security_keyvault_keys/tests/key_client.rs
+++ sdk/keyvault/azure_security_keyvault_keys/tests/key_client.rs
@@ -5,9 +5,9 @@
 
 use azure_core::{Result, StatusCode};
 use azure_core_test::{recorded, TestContext, TestMode};
-use azure_security_keyvault_secrets::{
-    models::{SecretSetParameters, SecretUpdateParameters},
-    ResourceExt as _, SecretClient, SecretClientOptions,
+use azure_security_keyvault_keys::{
+    models::{JsonWebKeyCurveName, JsonWebKeyType, KeyCreateParameters, KeyUpdateParameters},
+    KeyClient, KeyClientOptions, ResourceExt as _,
 };
 use azure_security_keyvault_test::Retry;
 use futures::TryStreamExt;
@@ -19,132 +19,141 @@ const REMOVE_SANITIZERS: &[&str] = &[
 ];
 
 #[recorded::test]
-async fn secret_roundtrip(ctx: TestContext) -> Result<()> {
+async fn key_roundtrip(ctx: TestContext) -> Result<()> {
     let recording = ctx.recording();
     recording.remove_sanitizers(REMOVE_SANITIZERS).await?;
 
-    let mut options = SecretClientOptions::default();
+    let mut options = KeyClientOptions::default();
     recording.instrument(&mut options.client_options);
 
-    let client = SecretClient::new(
+    let client = KeyClient::new(
         recording.var("AZURE_KEYVAULT_URL", None).as_str(),
         recording.credential(),
         Some(options),
     )?;
 
-    // Set a secret.
-    let body = SecretSetParameters {
-        value: Some("secret-value".into()),
+    // Create an RSA key.
+    let body = KeyCreateParameters {
+        kty: Some(JsonWebKeyType::RSA),
+        key_size: Some(2048),
         ..Default::default()
     };
-    let secret = client
-        .set_secret("secret-roundtrip", body.try_into()?, None)
+    let key = client
+        .create_key("key-roundtrip", body.try_into()?, None)
         .await?
         .into_body()
         .await?;
-    assert_eq!(secret.value, Some("secret-value".into()));
+    assert!(matches!(key.key, Some(ref jwk) if jwk.e == Some(vec![1, 0, 1])));
 
-    // Get a specific version of a secret.
-    let version = secret.resource_id()?.version.unwrap_or_default();
-    let secret = client
-        .get_secret("secret-roundtrip", version.as_ref(), None)
+    // Get a specific version of a key.
+    let version = key.resource_id()?.version.unwrap_or_default();
+    let key = client
+        .get_key("key-roundtrip", version.as_ref(), None)
         .await?
         .into_body()
         .await?;
-    assert_eq!(secret.value, Some("secret-value".into()));
+    assert!(matches!(key.key, Some(ref jwk) if jwk.e == Some(vec![1, 0, 1])));
 
     Ok(())
 }
 
 #[recorded::test]
-async fn update_secret_properties(ctx: TestContext) -> Result<()> {
+async fn update_key_properties(ctx: TestContext) -> Result<()> {
     let recording = ctx.recording();
     recording.remove_sanitizers(REMOVE_SANITIZERS).await?;
 
-    let mut options = SecretClientOptions::default();
+    let mut options = KeyClientOptions::default();
     recording.instrument(&mut options.client_options);
 
-    let client = SecretClient::new(
+    let client = KeyClient::new(
         recording.var("AZURE_KEYVAULT_URL", None).as_str(),
         recording.credential(),
         Some(options),
     )?;
 
-    // Set a secret.
-    let body = SecretSetParameters {
-        value: Some("secret-value".into()),
+    // Create an EC key.
+    let body = KeyCreateParameters {
+        kty: Some(JsonWebKeyType::EC),
+        curve: Some(JsonWebKeyCurveName::P256),
         ..Default::default()
     };
-    let secret = client
-        .set_secret("update-secret", body.try_into()?, None)
+    let key = client
+        .create_key("update-key", body.try_into()?, None)
         .await?
         .into_body()
         .await?;
-    assert_eq!(secret.value, Some("secret-value".into()));
+    assert!(matches!(key.key, Some(ref jwk) if jwk.x.is_some()));
 
-    // Update secret properties.
-    let properties = SecretUpdateParameters {
-        content_type: Some("text/plain".into()),
-        secret_attributes: secret.attributes,
+    // Update key properties.
+    let properties = KeyUpdateParameters {
+        key_attributes: key.attributes,
         tags: Some(HashMap::from_iter(vec![(
             "test-name".into(),
-            "update_secret_properties".into(),
+            "update_key_properties".into(),
         )])),
+        ..Default::default()
     };
-    let secret = client
-        .update_secret("update-secret", "", properties.try_into()?, None)
+    let key = client
+        .update_key("update-key", "", properties.try_into()?, None)
         .await?
         .into_body()
         .await?;
-    assert_eq!(secret.content_type, Some("text/plain".into()));
     assert_eq!(
-        secret.tags.as_ref().and_then(|tags| tags.get("test-name")),
-        Some(&String::from("update_secret_properties"))
+        key.tags.as_ref().and_then(|tags| tags.get("test-name")),
+        Some(&String::from("update_key_properties"))
     );
 
     Ok(())
 }
 
 #[recorded::test]
-async fn list_secrets(ctx: TestContext) -> Result<()> {
+async fn list_keys(ctx: TestContext) -> Result<()> {
     let recording = ctx.recording();
     recording.remove_sanitizers(REMOVE_SANITIZERS).await?;
 
-    let mut options = SecretClientOptions::default();
+    let mut options = KeyClientOptions::default();
     recording.instrument(&mut options.client_options);
 
-    let client = SecretClient::new(
+    let client = KeyClient::new(
         recording.var("AZURE_KEYVAULT_URL", None).as_str(),
         recording.credential(),
         Some(options),
     )?;
 
-    // Create several secrets.
-    let mut names = vec!["list-secrets-1", "list-secrets-2"];
+    // Create several EC keys.
+    let mut names = vec!["list-keys-1", "list-keys-2"];
     let secret1 = client
-        .set_secret(names[0], r#"{"value":"secret-value-1"}"#.try_into()?, None)
+        .create_key(
+            names[0],
+            r#"{"kty":"EC","curve":"P-384"}"#.try_into()?,
+            None,
+        )
         .await?
         .into_body()
         .await?;
-    assert_eq!(secret1.value, Some("secret-value-1".into()));
+    assert!(matches!(secret1.key, Some(ref jwk) if jwk.x.is_some()));
 
     let secret2 = client
-        .set_secret(names[1], r#"{"value":"secret-value-2"}"#.try_into()?, None)
+        .create_key(
+            names[1],
+            r#"{"kty":"EC","curve":"P-384"}"#.try_into()?,
+            None,
+        )
         .await?
         .into_body()
         .await?;
-    assert_eq!(secret2.value, Some("secret-value-2".into()));
+    assert!(matches!(secret2.key, Some(ref jwk) if jwk.x.is_some()));
 
-    // List secrets.
-    let mut pager = client.get_secrets(None)?.into_stream();
-    while let Some(secrets) = pager.try_next().await? {
-        let Some(secrets) = secrets.into_body().await?.value else {
+    // List keys.
+    let mut pager = client.get_keys(None)?.into_stream();
+    while let Some(keys) = pager.try_next().await? {
+        let Some(keys) = keys.into_body().await?.value else {
             continue;
         };
 
-        for secret in secrets {
-            // Get the secret name from the ID.
-            let name = secret.resource_id()?.name;
+        for key in keys {
+            // Get the key name from the ID.
+            let name = key.resource_id()?.name;
             if let Some(idx) = names.iter().position(|n| name.eq(*n)) {
                 names.remove(idx);
             }
@@ -156,37 +165,35 @@ async fn list_secrets(ctx: TestContext) -> Result<()> {
 }
 
 #[recorded::test]
-async fn purge_secret(ctx: TestContext) -> Result<()> {
+async fn purge_key(ctx: TestContext) -> Result<()> {
     let recording = ctx.recording();
     recording.remove_sanitizers(REMOVE_SANITIZERS).await?;
 
-    let mut options = SecretClientOptions::default();
+    let mut options = KeyClientOptions::default();
     recording.instrument(&mut options.client_options);
 
-    let client = SecretClient::new(
+    let client = KeyClient::new(
         recording.var("AZURE_KEYVAULT_URL", None).as_str(),
         recording.credential(),
         Some(options),
     )?;
 
-    // Create a secret.
-    let secret = client
-        .set_secret(
-            "purge-secret",
-            SecretSetParameters {
-                value: Some("secret-value".into()),
-                ..Default::default()
-            }
-            .try_into()?,
-            None,
-        )
+    // Create an RSA key.
+    let body = KeyCreateParameters {
+        kty: Some(JsonWebKeyType::RSA),
+        key_size: Some(2048),
+        ..Default::default()
+    };
+    let key = client
+        .create_key("purge-key", body.try_into()?, None)
         .await?
         .into_body()
         .await?;
+    assert!(matches!(key.key, Some(ref jwk) if jwk.e == Some(vec![1, 0, 1])));
 
-    // Delete the secret.
-    let name = secret.resource_id()?.name;
-    client.delete_secret(name.as_ref(), None).await?;
+    // Delete the key.
+    let name = key.resource_id()?.name;
+    client.delete_key(name.as_ref(), None).await?;
 
     // Because deletes may not happen right away, try purging in a loop.
     let mut retry = match recording.test_mode() {
@@ -195,7 +202,7 @@ async fn purge_secret(ctx: TestContext) -> Result<()> {
     };
 
     loop {
-        match client.purge_deleted_secret(name.as_ref(), None).await {
+        match client.purge_deleted_key(name.as_ref(), None).await {
             Ok(_) => {
                 println!("{name} has been purged");
                 break;
