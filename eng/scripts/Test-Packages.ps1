c77c6199a60c4fbda2414fddaf7dde43674d4319
Patrick Hallisey <pahallis@microsoft.com>
2025-02-13 15:45:15 -0800

    Fan out to test legs faster

--- eng/scripts/Test-Packages.ps1
+++ eng/scripts/Test-Packages.ps1
@@ -3,8 +3,6 @@
 #Requires -Version 7.0
 param(
   [string]$Toolchain = 'stable',
-  [bool]$UnitTests = $true,
-  [bool]$FunctionalTests = $true,
   [string]$PackageInfoDirectory
 )
 
@@ -15,8 +13,6 @@ Set-StrictMode -Version 2.0
 
 Write-Host "Testing packages with
     Toolchain: '$Toolchain'
-    UnitTests: '$UnitTests'
-    FunctionalTests: '$FunctionalTests'
     PackageInfoDirectory: '$PackageInfoDirectory'"
 
 if ($PackageInfoDirectory) {
@@ -41,7 +37,6 @@ foreach ($package in $packagesToTest) {
 Write-Host "Setting RUSTFLAGS to '-Dwarnings'"
 $env:RUSTFLAGS = "-Dwarnings"
 
-
 foreach ($package in $packagesToTest) {
   Push-Location ([System.IO.Path]::Combine($RepoRoot, $package.DirectoryPath))
   try {
@@ -50,7 +45,7 @@ foreach ($package in $packagesToTest) {
     $setupScript = Join-Path $packageDirectory "Test-Setup.ps1"
     if (Test-Path $setupScript) {
       Write-Host "`n`nRunning test setup script for package: '$($package.Name)'`n"
-      Invoke-LoggedCommand $setupScript
+      Invoke-LoggedCommand $setupScript -GroupOutput
       if (!$? -ne 0) {
         Write-Error "Test setup script failed for package: '$($package.Name)'"
         exit 1
@@ -59,33 +54,20 @@ foreach ($package in $packagesToTest) {
 
     Write-Host "`n`nTesting package: '$($package.Name)'`n"
 
-    Invoke-LoggedCommand "cargo +$Toolchain build --keep-going"
+    Invoke-LoggedCommand "cargo +$Toolchain build --keep-going" -GroupOutput
     Write-Host "`n`n"
 
-    $targets = @()
-    if ($UnitTests) {
-      $targets += "--lib"
-    }
-
-    if ($FunctionalTests) {
-      $targets += "--bins"
-      $targets += "--examples"
-      $targets += "--tests"
-      $targets += "--benches"
-    }
-
-    Invoke-LoggedCommand "cargo +$Toolchain test $($targets -join ' ') --no-fail-fast"
+    Invoke-LoggedCommand "cargo +$Toolchain test --doc --no-fail-fast" -GroupOutput
     Write-Host "`n`n"
 
-    Invoke-LoggedCommand "cargo +$Toolchain test --doc --no-fail-fast"
+    Invoke-LoggedCommand "cargo +$Toolchain test --all-targets --no-fail-fast" -GroupOutput
     Write-Host "`n`n"
 
     $cleanupScript = Join-Path $packageDirectory "Test-Cleanup.ps1"
     if (Test-Path $cleanupScript) {
       Write-Host "`n`nRunning test cleanup script for package: '$($package.Name)'`n"
-      Invoke-LoggedCommand $cleanupScript
+      Invoke-LoggedCommand $cleanupScript -GroupOutput
       # We ignore the exit code of the cleanup script.
-
     }
   }
   finally {
