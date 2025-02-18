a3b39281f736391481af3f68a4219662f5e15158
Arpad Müller <arpad@neon.tech>
2025-02-18 15:09:23 +0100

    Make it trait based

--- sdk/core/azure_core/src/process/mod.rs
+++ sdk/core/azure_core/src/process/mod.rs
@@ -1,27 +1,51 @@
+use async_trait::async_trait;
+use std::io::Error;
+use std::{ffi::OsStr, process::Output};
+
 #[cfg(not(feature = "tokio_process"))]
 mod thread;
 
 #[cfg(not(feature = "tokio_process"))]
-pub use thread::run_command;
+pub use thread::ThreadRunner;
 
 #[cfg(feature = "tokio_process")]
-pub use self::tokio::run_command;
+pub use tokio::TokioRunner;
 
 #[cfg(feature = "tokio_process")]
 mod tokio {
-    use std::io::Error;
-    use std::{ffi::OsStr, process::Output};
+    use super::*;
 
-    /// Run a command with the given arguments until it terminates, returning the output
-    pub async fn run_command<S, I, A>(program: S, args: I) -> Result<Output, Error>
-    where
-        S: AsRef<OsStr>,
-        I: IntoIterator<Item = A>,
-        A: AsRef<OsStr>,
-    {
-        tokio::process::Command::new(program)
-            .args(args)
-            .output()
-            .await
+    /// Implement [`ProcessRunner`] via [`::tokio::process`]
+    #[derive(Debug)]
+    pub struct TokioRunner;
+
+    #[cfg_attr(target_arch = "wasm32", async_trait(?Send))]
+    #[cfg_attr(not(target_arch = "wasm32"), async_trait)]
+    impl ProcessRunner for TokioRunner {
+        async fn run_command(&self, program: &OsStr, args: &[&OsStr]) -> Result<Output, Error> {
+            let mut cmd = ::tokio::process::Command::new(program);
+            cmd.args(args);
+            cmd.output().await
+        }
     }
+}
+
+/// Obtains a new process runner
+pub fn new_process_runner() -> Box<dyn ProcessRunner> {
+    #[cfg(feature = "tokio_process")]
+    {
+        Box::new(TokioRunner)
+    }
+    #[cfg(not(feature = "tokio_process"))]
+    {
+        Box::new(ThreadRunner)
+    }
+}
+
+/// An abstraction to run processes
+#[cfg_attr(target_arch = "wasm32", async_trait(?Send))]
+#[cfg_attr(not(target_arch = "wasm32"), async_trait)]
+pub trait ProcessRunner: Send + Sync + std::fmt::Debug {
+    /// Run a command with the given arguments until it terminates, returning the output
+    async fn run_command(&self, program: &OsStr, args: &[&OsStr]) -> Result<Output, Error>;
 }
--- sdk/core/azure_core/src/process/thread.rs
+++ sdk/core/azure_core/src/process/thread.rs
@@ -1,25 +1,28 @@
+use super::ProcessRunner;
+use async_trait::async_trait;
+use futures::channel::oneshot;
 use std::io::Error;
+use std::io::ErrorKind;
 use std::{ffi::OsStr, process::Output};
 
-/// Run a command with the given arguments until it terminates, returning the output
-pub async fn run_command<S, I, A>(program: S, args: I) -> Result<Output, Error>
-where
-    S: AsRef<OsStr>,
-    I: IntoIterator<Item = A>,
-    A: AsRef<OsStr>,
-{
-    use futures::channel::oneshot;
-    use std::io::ErrorKind;
+/// Implement [`ProcessRunner`] via a thread and [`std::process`]
+#[derive(Debug)]
+pub struct ThreadRunner;
 
-    let (tx, rx) = oneshot::channel();
-    let mut cmd = std::process::Command::new(program);
-    cmd.args(args);
-    std::thread::spawn(move || {
-        let output = cmd.output();
-        tx.send(output)
-    });
-    let output = rx
-        .await
-        .map_err(|err| Error::new(ErrorKind::Other, err))??;
-    Ok(output)
+#[cfg_attr(target_arch = "wasm32", async_trait(?Send))]
+#[cfg_attr(not(target_arch = "wasm32"), async_trait)]
+impl ProcessRunner for ThreadRunner {
+    async fn run_command(&self, program: &OsStr, args: &[&OsStr]) -> Result<Output, Error> {
+        let (tx, rx) = oneshot::channel();
+        let mut cmd = std::process::Command::new(program);
+        cmd.args(args);
+        std::thread::spawn(move || {
+            let output = cmd.output();
+            tx.send(output)
+        });
+        let output = rx
+            .await
+            .map_err(|err| Error::new(ErrorKind::Other, err))??;
+        Ok(output)
+    }
 }
