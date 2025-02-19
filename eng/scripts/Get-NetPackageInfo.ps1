5fba6fbe2ff1ebc52162b26dba9160172f4dc150
Heath Stewart <heaths@microsoft.com>
2025-02-19 14:31:53 -0800

    Get package information based on Azure SDK for .NET names

---
+++ eng/scripts/Get-NetPackageInfo.ps1
@@ -0,0 +1,59 @@
+#!/usr/bin/env pwsh
+
+#Requires -Version 7.0
+
+[CmdletBinding()]
+param(
+  [Parameter(Mandatory = $true, Position = 0)]
+  [string] $SdkRoot
+)
+
+foreach ($serviceDirectory in (Join-Path $SdkRoot 'sdk' -Resolve | Get-ChildItem -Directory)) {
+  foreach ($packageDirectory in (Get-ChildItem $serviceDirectory -Directory)) {
+    foreach ($projectFile in (Join-Path $packageDirectory 'src' -Resolve -ErrorAction SilentlyContinue | Get-ChildItem -Filter *.csproj)) {
+      $packageName = $projectFile.BaseName.ToLowerInvariant().Replace('.', '_')
+      $existingPackageName = $packageName
+
+      [xml] $projectXml = Get-Content $projectFile
+      $description = (([string]($projectXml.Project.PropertyGroup.Description -join '') -split "`n").Trim() -join ' ').Trim()
+
+      $idx0 = $packageName.Substring(0, 2)
+      $idx1 = $packageName.Substring(2, 2)
+
+      $resp = Invoke-WebRequest "https://index.crates.io/$idx0/$idx1/$packageName" -SkipHttpErrorCheck
+      if ($resp.StatusCode -eq 404) {
+        $existingPackageName = $packageName -replace '_', '-'
+        $resp = Invoke-WebRequest "https://index.crates.io/$idx0/$idx1/$existingPackageName" -SkipHttpErrorCheck
+      }
+
+      $exists = $resp.StatusCode -eq 200
+      if ($exists) {
+        # If we found a package, get the publisher. There is a 1s per request rate limit.
+        Start-Sleep -Seconds 1
+
+        $resp = Invoke-WebRequest "https://crates.io/api/v1/crates/$existingPackageName/owners" -SkipHttpErrorCheck
+        if ($resp.StatusCode -ne 200) {
+          Write-Error "No owner information for existing package $existingPackageName"
+          continue
+        }
+
+        $json = $resp.Content | ConvertFrom-Json
+        [string[]] $owners = $json.users.login
+        $microsoftOwned = $owners -contains 'azure-sdk' -or $owners -contains 'heaths' -or $owners -contains 'github:azure:azure-sdk-publish-rust'
+
+      }
+      else {
+        $existingPackageName = ''
+      }
+
+      [pscustomobject] @{
+        Name                = $packageName
+        Description         = $description
+        Exists              = $exists
+        ExistingPackageName = $existingPackageName
+        MicrosoftOwned      = $microsoftOwned -or $false
+        Owners              = $owners -join ';'
+      }
+    }
+  }
+}
