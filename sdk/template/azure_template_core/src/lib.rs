334e1b74f972c9215a58899be00525b891c7bb3d
Harsha Nalluru <sanallur@microsoft.com>
2025-02-21 22:18:12 +0000

    retain existign sample

--- sdk/template/azure_template_core/src/lib.rs
+++ sdk/template/azure_template_core/src/lib.rs
@@ -17,3 +17,17 @@ pub mod numeric {
 }
 
 pub use numeric::NumericCore;
+
+pub fn add(left: u64, right: u64) -> u64 {
+    left + right
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn it_works() {
+        assert_eq!(add(2, 2), 4);
+    }
+}
