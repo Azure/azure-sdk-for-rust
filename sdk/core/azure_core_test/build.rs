38554b9ec51162e1caccbc5778afbf551f2bef47
Heath Stewart <heaths@microsoft.com>
2025-02-21 13:21:23 -0800

    Optimize rebuild when AZURE_TEST_MODE changes (#2156)
    Doesn't need to be as far down as rebuilding the macros crate.

--- sdk/core/azure_core_test/build.rs
+++ sdk/core/azure_core_test/build.rs
@@ -1,3 +1,6 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
 pub fn main() {
     // Force a rebuild of this package, and thus anything dependent upon it, if AZURE_TEST_MODE changes.
     println!("cargo::rerun-if-env-changed=AZURE_TEST_MODE");
