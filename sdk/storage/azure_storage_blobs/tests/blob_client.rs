6e9087691aba85a9b5374ae76f9f27fec55d3a2b
Vincent Tran <vincenttran@microsoft.com>
2025-02-21 14:29:43 -0800

    Added tests for commit_block_list, stage_block, and get_block_list

--- sdk/storage/azure_storage_blobs/tests/blob_client.rs
+++ sdk/storage/azure_storage_blobs/tests/blob_client.rs
@@ -1,7 +1,7 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 
-use azure_core::{base64, headers::HeaderName, Request, RequestContent, StatusCode};
+use azure_core::{base64, headers::HeaderName, Bytes, Request, RequestContent, StatusCode};
 use azure_core_test::recorded;
 use azure_identity::DefaultAzureCredentialBuilder;
 use azure_storage_blobs::{
@@ -13,7 +13,6 @@ use azure_storage_blobs::{
     },
     BlobClientOptions,
 };
-use serde_json;
 use std::{env, error::Error};
 
 #[recorded::test(live)]
@@ -131,18 +130,13 @@ async fn test_download_blob() -> Result<(), Box<dyn Error>> {
         .await?;
 
     // Assert
-    // assert!(response.is_ok());
     let (status_code, headers, response_body) = response.deconstruct();
     assert!(status_code.is_success());
     assert_eq!(
         "21",
         headers.get_str(&HeaderName::from_static("content-length"))?
     );
-    // TODO: Collect bytes
-    assert_eq!(
-        "test download content",
-        response_body.collect_string().await?
-    );
+    assert_eq!(Bytes::from_static(data), response_body.collect().await?);
 
     container_client.delete_container(None).await?;
     Ok(())
@@ -253,15 +247,15 @@ async fn test_put_block_list() -> Result<(), Box<dyn Error>> {
     // Act
     let container_client = BlobContainerClient::new(
         &endpoint,
-        String::from("testcontainerpbl000"),
+        String::from("testcontainerpbl"),
         credential.clone(),
         None,
     )?;
-    // container_client.create_container(None).await?;
+    container_client.create_container(None).await?;
 
     let blob_client = BlobClient::new(
         &endpoint,
-        String::from("testcontainerpbl000"),
+        String::from("testcontainerpbl"),
         String::from("testblob"),
         credential,
         Some(BlobClientOptions::default()),
@@ -308,22 +302,134 @@ async fn test_put_block_list() -> Result<(), Box<dyn Error>> {
         uncommitted: None,
     };
 
-    let serialized = serde_json::to_vec(&block_lookup_list)?;
-    let rq: RequestContent<BlockLookupList> = RequestContent::from(serialized);
+    let request_content = RequestContent::try_from(block_lookup_list)?;
 
     blob_client
         .commit_block_list(
-            rq,
+            request_content,
             Some(BlobBlockBlobClientCommitBlockListOptions::default()),
         )
         .await?;
 
-    // let response = blob_client
-    //     .get_blob_properties(Some(BlobBlobClientGetPropertiesOptions::default()))
-    //     .await?;
+    let response = blob_client
+        .download_blob(Some(BlobBlobClientDownloadOptions::default()))
+        .await?;
 
-    // println!("{:?}", response);
+    // Assert
+    let (status_code, headers, response_body) = response.deconstruct();
+    assert!(status_code.is_success());
+    assert_eq!(
+        "9",
+        headers.get_str(&HeaderName::from_static("content-length"))?
+    );
+    assert_eq!(
+        Bytes::from_static(b"AAABBBCCC"),
+        response_body.collect().await?
+    );
 
-    // container_client.delete_container(None).await?;
+    container_client.delete_container(None).await?;
+    Ok(())
+}
+
+#[recorded::test(live)]
+async fn test_get_block_list() -> Result<(), Box<dyn Error>> {
+    // Setup
+    let storage_account_name = env::var("AZURE_STORAGE_ACCOUNT_NAME")
+        .expect("Failed to get environment variable: AZURE_STORAGE_ACCOUNT_NAME");
+    let endpoint = format!("https://{}.blob.core.windows.net/", storage_account_name);
+    let credential = DefaultAzureCredentialBuilder::default().build()?;
+
+    // Act
+    let container_client = BlobContainerClient::new(
+        &endpoint,
+        String::from("testcontainergbl"),
+        credential.clone(),
+        None,
+    )?;
+    container_client.create_container(None).await?;
+
+    let blob_client = BlobClient::new(
+        &endpoint,
+        String::from("testcontainergbl"),
+        String::from("testblob"),
+        credential,
+        Some(BlobClientOptions::default()),
+    )?;
+
+    let block_1 = b"AAA";
+    let block_2 = b"BBB";
+    let block_3 = b"CCC";
+
+    blob_client
+        .stage_block(
+            "1",
+            i64::try_from(block_1.len())?,
+            RequestContent::from(block_1.to_vec()),
+            Some(BlobBlockBlobClientStageBlockOptions::default()),
+        )
+        .await?;
+
+    blob_client
+        .stage_block(
+            "2",
+            i64::try_from(block_2.len())?,
+            RequestContent::from(block_2.to_vec()),
+            Some(BlobBlockBlobClientStageBlockOptions::default()),
+        )
+        .await?;
+    blob_client
+        .stage_block(
+            "3",
+            i64::try_from(block_3.len())?,
+            RequestContent::from(block_3.to_vec()),
+            Some(BlobBlockBlobClientStageBlockOptions::default()),
+        )
+        .await?;
+
+    // Three staged blocks
+    let mut block_list = blob_client
+        .get_block_list(
+            BlockListType::All,
+            Some(BlobBlockBlobClientGetBlockListOptions::default()),
+        )
+        .await?
+        .into_body()
+        .await?;
+    assert!(block_list.uncommitted_blocks.is_some());
+    assert!(block_list.committed_blocks.is_none());
+
+    let latest_blocks: Vec<String> = vec![
+        base64::encode("1"),
+        base64::encode("2"),
+        base64::encode("3"),
+    ];
+    let block_lookup_list = BlockLookupList {
+        committed: None,
+        latest: Some(latest_blocks),
+        uncommitted: None,
+    };
+
+    let request_content = RequestContent::try_from(block_lookup_list)?;
+
+    blob_client
+        .commit_block_list(
+            request_content,
+            Some(BlobBlockBlobClientCommitBlockListOptions::default()),
+        )
+        .await?;
+
+    // Three committed blocks
+    block_list = blob_client
+        .get_block_list(
+            BlockListType::All,
+            Some(BlobBlockBlobClientGetBlockListOptions::default()),
+        )
+        .await?
+        .into_body()
+        .await?;
+    assert!(block_list.uncommitted_blocks.is_none());
+    assert!(block_list.committed_blocks.is_some());
+
+    container_client.delete_container(None).await?;
     Ok(())
 }
