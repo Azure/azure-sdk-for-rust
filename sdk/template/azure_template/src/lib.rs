1dd54a3a27a5dfda8906c27f02327b74416f544b
Harsha Nalluru <sanallur@microsoft.com>
2025-02-24 15:45:40 -0800

    Update sdk/template/azure_template/src/lib.rs

--- sdk/template/azure_template/src/lib.rs
+++ sdk/template/azure_template/src/lib.rs
@@ -1,4 +1,5 @@
-//! Azure Template crate demonstrating Rust patterns
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
 
 pub use azure_template_core::NumericCore;
 pub mod configuration;
