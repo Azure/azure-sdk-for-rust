04abead99a2f3c721cbb99a5678f145737ceb9cf
Patrick Hallisey <pahallis@microsoft.com>
2025-03-10 08:51:51 -0700

    Add switches to Get-PackageDependencies

--- eng/scripts/Get-PackageDependencies.ps1
+++ eng/scripts/Get-PackageDependencies.ps1
@@ -2,12 +2,31 @@
 #Requires -Version 7.0
 
 [CmdletBinding()]
+param(
+  [switch]$Publishable,
+  [switch]$NoDev,
+  [switch]$NoReq
+)
 
 $ErrorActionPreference = 'Stop'
 
 $packages = & (Join-Path $PSScriptRoot 'Get-PackageVersions.ps1')
 
-$packages.packageDependencies | Select-Object -Property @(
+if ($Publishable) {
+  $packages = $packages | Where-Object { $_.publish }
+}
+
+$dependencies = $packages | Select-Object -ExpandProperty packageDependencies
+
+if ($NoDev) {
+  $dependencies = $dependencies | Where-Object { $_.kind -ne 'dev' }
+}
+
+if ($NoReq) {
+  $dependencies = $dependencies | Where-Object { $_.req -eq '*' }
+}
+
+$dependencies | Select-Object -Property @(
   @{ Name = 'from'; Expression = { $_.dependant } },
   @{ Name = 'to'; Expression = { $_.name } },
   'kind',
