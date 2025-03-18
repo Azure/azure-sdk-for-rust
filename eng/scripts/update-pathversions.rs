2e2a9e63276ed22704d240c9ac21a98791770c84
Patrick Hallisey <pahallis@microsoft.com>
2025-03-17 17:33:13 -0700

    using toml_edit

--- eng/scripts/update-pathversions.rs
+++ eng/scripts/update-pathversions.rs
@@ -4,12 +4,13 @@
 edition = "2021"
 
 [dependencies]
-toml = "0.8.10"
+toml_edit = "^0.22"
 ---
 
 use std::{env, fs, path::PathBuf};
 use std::io::Write;
-use toml::Table;
+use toml_edit::{DocumentMut, value};
+
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     let script_dir = PathBuf::from(env::var("CARGO_MANIFEST_DIR")?);
@@ -18,12 +19,13 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     // read the Cargo.toml file
     let cargo_toml_path = repo_root.join("Cargo.toml");
     let cargo_toml_content = fs::read_to_string(&cargo_toml_path).expect("Failed to read Cargo.toml");
-    let cargo_toml: Table = cargo_toml_content.parse().expect("Failed to parse Cargo.toml");
+    let mut cargo_toml: DocumentMut = cargo_toml_content.parse().expect("Failed to parse Cargo.toml");
+
+    cargo_toml["workspace"]["dependencies"]["azure_core"]["version"] = value("0.99.0");
 
     // reserialize the Cargo.toml file
     let mut cargo_toml_file = fs::File::create(&cargo_toml_path)?;
-    let mut cargo_toml_content = toml::to_string_pretty(&cargo_toml)?;
-    cargo_toml_content.push_str("\n");
+    let cargo_toml_content = cargo_toml.to_string();
     cargo_toml_file
         .write_all(cargo_toml_content.as_bytes())
         .expect("Failed to write Cargo.toml");
