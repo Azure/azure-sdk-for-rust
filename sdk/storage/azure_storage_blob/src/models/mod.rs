ca59b2ffc69f2850d80fc9edaf144753782cca6d
Heath Stewart <heaths@microsoft.com>
2025-03-25 13:12:31 -0700

    WIP: Storage changes post-refactor

--- sdk/storage/azure_storage_blob/src/models/mod.rs
+++ sdk/storage/azure_storage_blob/src/models/mod.rs
@@ -2,8 +2,8 @@
 // Licensed under the MIT License.
 
 mod blob_properties;
+mod container_properties;
 
-pub use crate::generated::enums::*;
 pub use crate::generated::models::*;
-
 pub use blob_properties::BlobProperties;
+pub use container_properties::ContainerProperties;
