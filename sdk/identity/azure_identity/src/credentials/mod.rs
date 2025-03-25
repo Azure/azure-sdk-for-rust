c8732d52f59a9f2fbdb2cc6227158d479ef6fc01
Charles Lowell <10964656+chlowell@users.noreply.github.com>
2025-03-25 18:27:22 +0000

    delete azure_identity's TokenCache implementation

--- sdk/identity/azure_identity/src/credentials/mod.rs
+++ sdk/identity/azure_identity/src/credentials/mod.rs
@@ -11,7 +11,6 @@
 mod app_service_managed_identity_credential;
 #[cfg(not(target_arch = "wasm32"))]
 mod azure_cli_credentials;
-pub(crate) mod cache;
 mod client_assertion_credentials;
 #[cfg(feature = "client_certificate")]
 mod client_certificate_credentials;
