d8bdbe27b99587fcd3dc775845c0a2866918616d
Patrick Hallisey <pahallis@microsoft.com>
2025-03-26 21:58:47 -0700

    Only update rustup if we're using 'default' toolchain

--- eng/scripts/Use-Rust.ps1
+++ eng/scripts/Use-Rust.ps1
@@ -12,23 +12,23 @@ $ErrorActionPreference = 'Stop'
 Set-StrictMode -Version 2.0
 . "$PSScriptRoot/../common/scripts/common.ps1"
 
-Write-Host $RepoRoot
-
-$rustupVersion = Invoke-LoggedCommand "rustup --version"
-if ($rustupVersion -match 'rustup (\S+)') {
-    $rustupVersion = [AzureEngSemanticVersion]::new($matches[1])
-    if ($rustupVersion -lt [AzureEngSemanticVersion]::new('1.28.0')) {
-        Invoke-LoggedCommand "rustup self update"
-    }
-}
-
 if ($Toolchain -eq 'msrv') {
     Write-Host "Reading MSRV from azure_core"
     $toolchainArg = cargo read-manifest --manifest-path "$RepoRoot/sdk/core/azure_core/Cargo.toml" | ConvertFrom-Json | Select-Object -ExpandProperty rust_version
 }
 elseif ($Toolchain -eq 'default') {
-    # 'rustup default' will install and set the default toolchain, but unlike `rustup install`, it doesn't work
-    # without a toolchain argument.  Even though 'rustup install'
+    # You can't call 'rustup install' without a toolchain before rustup 1.28.0. If know we'll be doing that, make sure
+    # we have the latest rustup installed
+
+    Invoke-LoggedCommand "rustup --version" | Tee-Object -Variable rustupVersion
+
+    if ($rustupVersion -match 'rustup (\S+)') {
+        $rustupVersion = [AzureEngSemanticVersion]::new($matches[1])
+        if ($rustupVersion -lt [AzureEngSemanticVersion]::new('1.28.0')) {
+            Invoke-LoggedCommand "rustup self update"
+        }
+    }
+
     $toolchainArg = ''
 }
 else {
