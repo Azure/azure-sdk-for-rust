4afd715e908b665fea13ce47759026d98495374b
Heath Stewart <heaths@microsoft.com>
2025-03-28 19:54:51 -0700

    Resolve meeting feedback
    * Always add the policy, but support custom policies.
    * For now, caller can add the existing `ClientRequestId` header to `ClientMethodOptions::context`.

--- sdk/core/azure_core/src/http/policies/mod.rs
+++ sdk/core/azure_core/src/http/policies/mod.rs
@@ -4,12 +4,10 @@
 //! HTTP pipeline policies.
 
 mod bearer_token_policy;
-mod request_id;
+mod client_request_id;
 mod telemetry;
-#[cfg(test)]
-mod test;
 
 pub use bearer_token_policy::BearerTokenCredentialPolicy;
-pub use request_id::*;
+pub use client_request_id::*;
 pub use telemetry::*;
 pub use typespec_client_core::http::policies::*;
