10ef74d9bee19d465edbe9550f8708bc0a539e4f
Larry Osterman <larryo@microsoft.com>
2025-04-18 13:47:46 -0700

    Fixed wasm32 build error

--- sdk/core/azure_core/src/task/wasm_spawn.rs
+++ sdk/core/azure_core/src/task/wasm_spawn.rs
@@ -1,24 +1,26 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 
-use super::{TaskFuture, TaskSpawner};
+use super::{SpawnHandleMethods, SpawnHandleT, TaskFuture, TaskSpawner};
+use async_trait::async_trait;
 
 /// A [`TaskSpawner`] using wasm.
 #[derive(Debug)]
 pub struct WasmSpawner;
 
 impl TaskSpawner for WasmSpawner {
-    fn spawn(&self, _f: TaskFuture) -> SpawnHandle {
+    fn spawn(&self, _f: TaskFuture) -> SpawnHandleT<WasmSpawnHandle> {
         unimplemented!()
     }
 }
 
 #[derive(Debug)]
-pub struct SpawnHandle();
+pub struct WasmSpawnHandle();
 
-impl SpawnHandle {
+#[async_trait(?Send)]
+impl SpawnHandleMethods for WasmSpawnHandle {
     /// Wait for the task to complete and return the result.
-    pub async fn wait(&mut self) -> crate::Result<()> {
+    async fn wait(self) -> crate::Result<()> {
         unimplemented!()
     }
 }
