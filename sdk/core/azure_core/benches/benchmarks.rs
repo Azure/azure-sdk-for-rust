7feff468db4949199ed19fd35d8feb8c9e049157
George Arama <gearama@microsoft.com>
2025-05-01 15:50:12 -0700

    Refactor benchmarks to remove unused request creation and header operation tests, streamline HTTP transport tests

--- sdk/core/azure_core/benches/benchmarks.rs
+++ sdk/core/azure_core/benches/benchmarks.rs
@@ -1,10 +1,10 @@
 use azure_core::http::{
-    headers::{HeaderName, HeaderValue, Headers},
-    new_http_client, Method, Request, StatusCode, Url,
+    headers::Headers, response::Response, HttpClient, Method, Request, StatusCode, Url,
 };
-
-use azure_core::error::ErrorKind;
+use azure_core_test::http::MockHttpClient;
 use criterion::{black_box, criterion_group, criterion_main, Criterion};
+use futures::FutureExt;
+use std::sync::Arc;
 
 // how many params to add to the url
 const PARAMS: &[usize] = &[1, 10, 100, 1000];
@@ -25,172 +25,30 @@ fn url_parsing_benchmark(c: &mut Criterion) {
     }
 }
 
-fn http_request_creation_benchmark(c: &mut Criterion) {
-    fn create_request(c: usize) {
-        let mut url = String::new();
-        url.push_str("https://example.com/path/resource?param=value");
-
-        for i in 0..c {
-            url.push_str(&format!("&param{}=value{}", i, i));
-        }
-        let mut req = Request::new(Url::parse(&url).unwrap(), Method::Get);
-        for i in 0..c {
-            let header_name = format!("x-ms-header-{}", i);
-            let header_value = format!("value_{}", i);
-            req.insert_header(
-                HeaderName::from(header_name),
-                HeaderValue::from(header_value),
-            );
-        }
-    }
-
-    // Benchmark creating requests with many headers
-    for num in PARAMS {
-        c.bench_function(&format!("create_request_{}", num), |b| {
-            b.iter(|| {
-                create_request(*num);
-                black_box(());
-            })
-        });
-    }
-
-    fn create_request_with_headers(c: usize) {
-        for i in 0..c {
-            let mut req = Request::new(Url::parse("https://example.com").unwrap(), Method::Get);
-            let header_name = format!("x-ms-header-{}", i);
-            let header_value = format!("value_{}", i);
-            req.insert_header(
-                HeaderName::from(header_name),
-                HeaderValue::from(header_value),
-            );
-        }
-    }
-    // Benchmark creating requests with headers
-    for num in PARAMS {
-        c.bench_function(&format!("create_request_with_headers_{}", num), |b| {
-            b.iter(|| {
-                create_request_with_headers(*num);
-                black_box(());
-            })
-        });
-    }
-
-    // Benchmark with request body
-    for size in PARAMS {
-        let body = "0".repeat(*size);
-        c.bench_function(&format!("create_request_with_body_{}", size), |b| {
-            b.iter(|| {
-                let mut req = Request::new(Url::parse("https://example.com").unwrap(), Method::Put);
-                req.set_body(body.as_bytes().to_vec());
-                black_box(())
-            })
-        });
-    }
-}
-
-fn header_operations_benchmark(c: &mut Criterion) {
-    fn header_operations(c: usize) {
-        let mut headers = Headers::new();
-        for (i, j) in (0..c).zip(0..c) {
-            headers.insert(
-                {
-                    let header_name = format!("x-ms-header-{}", i);
-                    HeaderName::from(header_name)
-                },
-                {
-                    let header_value = format!("value_{}", j);
-                    HeaderValue::from(header_value)
-                },
-            );
-        }
-        for i in 0..c {
-            // Read headers
-            let header_name = format!("x-ms-header-{}", i);
-            let _ = headers.get_str(&HeaderName::from(header_name));
-        }
-    }
-    // Benchmark header operations
-    for num in PARAMS {
-        c.bench_function(&format!("header_operations_{}", num), |b| {
-            b.iter(|| {
-                header_operations(*num);
-                black_box(());
-            })
-        });
-    }
-}
-
 fn http_transport_test(c: &mut Criterion) {
     let rt = tokio::runtime::Runtime::new().unwrap();
 
-    async fn make_get_request() -> azure_core::Result<StatusCode> {
-        let req = Request::new(
-            Url::parse("http://127.0.0.1:5000/Admin/isAlive").unwrap(),
-            Method::Get,
-        );
-        let http_client = new_http_client();
-        let resp = http_client.execute_request(&req).await?;
+    // client to be used in the benchmark
+    let mock_client = Arc::new(MockHttpClient::new(move |_| {
+        async move { Ok(Response::from_bytes(StatusCode::Ok, Headers::new(), vec![])) }.boxed()
+    })) as Arc<dyn HttpClient>;
 
-        if resp.status() != StatusCode::Ok {
-            let status_code = resp.status();
+    // requests to be used in the benchmark
+    let get_req = Request::new(Url::parse("https://localhost").unwrap(), Method::Get);
+    let mut post_req = Request::new(Url::parse("https://localhost").unwrap(), Method::Post);
+    post_req.set_body("test body");
 
-            return Err(azure_core::Error::message(
-                ErrorKind::http_response(
-                    status_code,
-                    Some(status_code.canonical_reason().to_string()),
-                ),
-                format!("{status_code} response from server"),
-            ));
-        }
-
-        let status = resp.status();
-        Ok(status)
-    }
-
-    async fn make_post_request() -> azure_core::Result<StatusCode> {
-        let mut req = Request::new(
-            Url::parse("http://127.0.0.1:5000/Admin/setRecordingOptions").unwrap(),
-            Method::Post,
-        );
-        let body = "{ \"HandleRedirects\": \"true\"}".to_string();
-        req.set_body(body.as_bytes().to_vec());
-        req.insert_header(
-            HeaderName::from("content-type"),
-            HeaderValue::from("application/json"),
-        );
-        let http_client = new_http_client();
-        let resp = http_client.execute_request(&req).await?;
-
-        if resp.status() != StatusCode::Ok {
-            let status_code = resp.status();
-
-            return Err(azure_core::Error::message(
-                ErrorKind::http_response(
-                    status_code,
-                    Some(status_code.canonical_reason().to_string()),
-                ),
-                format!("{status_code} response from server"),
-            ));
-        }
-
-        let status = resp.status();
-        Ok(status)
-    }
     // Benchmark GET and POST requests
     c.bench_function("http_transport_get_async", |b| {
         b.to_async(&rt).iter(|| async {
-            make_get_request().await.unwrap_or_else(|err| {
-                panic!("Failed to make GET request {err}");
-            });
+            let _ = mock_client.execute_request(&get_req).await;
             black_box(());
         });
     });
 
     c.bench_function("http_transport_post_async", |b| {
         b.to_async(&rt).iter(|| async {
-            make_post_request().await.unwrap_or_else(|err| {
-                panic!("Failed to make POST request {err}");
-            });
+            let _ = mock_client.execute_request(&post_req).await;
             black_box(());
         });
     });
@@ -199,9 +57,7 @@ fn http_transport_test(c: &mut Criterion) {
 criterion_group! {
     name = benchmarks;
     config = Criterion::default();
-    targets = url_parsing_benchmark,
-        http_request_creation_benchmark,
-        header_operations_benchmark,
+    targets =url_parsing_benchmark,
         http_transport_test
 }
 criterion_main!(benchmarks);
