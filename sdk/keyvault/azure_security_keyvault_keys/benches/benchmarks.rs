fe40483ad37c5cacbbd5666c6ef06128d91237c8
George Arama <gearama@microsoft.com>
2025-05-02 16:22:39 -0700

    Refactor benchmarks: rename create_key_benchmark to key_operations_benchmark and add get_key benchmark

--- sdk/keyvault/azure_security_keyvault_keys/benches/benchmarks.rs
+++ sdk/keyvault/azure_security_keyvault_keys/benches/benchmarks.rs
@@ -5,9 +5,9 @@ use azure_security_keyvault_keys::{
 };
 use criterion::{black_box, criterion_group, criterion_main, Criterion};
 
-fn create_key_benchmark(c: &mut Criterion) {
+fn key_operations_benchmark(c: &mut Criterion) {
     let rt = tokio::runtime::Runtime::new().unwrap();
-
+    // Setup the KeyClient and the CreateKeyParameters
     async fn setup_key_client() -> (KeyClient, CreateKeyParameters) {
         let keyvault_url: String = std::env::var("AZURE_KEYVAULT_URL")
             .unwrap_or_else(|e| panic!("AZURE_KEYVAULT_URL not set: {}", e));
@@ -23,13 +23,30 @@ fn create_key_benchmark(c: &mut Criterion) {
 
     let (client, body) = rt.block_on(async { setup_key_client().await });
 
-    async fn create_key(
+    const KEY_NAME: &str = "test-key";
+    // prep key in order to run the benchmark in a clean state
+    rt.block_on(async { create_key(KEY_NAME, &client, body.clone()).await }).unwrap();
+    // Create the key
+    async fn create_key(name:&str,
         client: &KeyClient,
         body: CreateKeyParameters,
     ) -> Result<Key, azure_core::Error> {
-        // Create the key
+        // Create a new key version
         let key: Key = client
-            .create_key("key-name", body.try_into()?, None)
+            .create_key(name, body.try_into()?, None)
+            .await?
+            .into_body()
+            .await?;
+        Ok(key)
+    }
+
+    async fn get_key(
+        key_name: &str,
+        client: &KeyClient,
+    ) -> Result<Key, azure_core::Error> {
+        // Get the key
+        let key: Key = client
+            .get_key(key_name, "", None)
             .await?
             .into_body()
             .await?;
@@ -38,9 +55,19 @@ fn create_key_benchmark(c: &mut Criterion) {
     // Benchmark create key
     c.bench_function("create_key", |b| {
         b.to_async(&rt).iter(|| async {
-            create_key(&client, body.clone())
+            create_key(KEY_NAME,&client, body.clone())
                 .await
                 .unwrap_or_else(|e| panic!("Failed to create key {}", e));
+            black_box(());
+        });
+    });
+
+    // Benchmark create key
+    c.bench_function("get_key", |b| {
+        b.to_async(&rt).iter(|| async {
+            get_key(KEY_NAME,&client)
+                .await
+                .unwrap_or_else(|e| panic!("Failed to get key {}", e));
             black_box(());
         });
     });
@@ -49,7 +76,7 @@ fn create_key_benchmark(c: &mut Criterion) {
 criterion_group! {
     name = benchmarks;
     config = Criterion::default().sample_size(10).warm_up_time(std::time::Duration::new(1, 0));
-    targets = create_key_benchmark
+    targets = key_operations_benchmark
 }
 
 criterion_main!(benchmarks);
