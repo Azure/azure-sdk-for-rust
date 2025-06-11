f17b7bb41bb2d5b9b85a0c0e573df37ad2b3e72a
Colin Desmond <colin.desmond@microsoft.com>
2025-06-11 11:01:01 +0000

    Removed unused class

--- sdk/storage/azure_storage_queue/src/clients/mod.rs
+++ sdk/storage/azure_storage_queue/src/clients/mod.rs
@@ -1,8 +1,6 @@
 mod queue_client;
-mod queue_service_client;
 
 pub use queue_client::QueueClient;
-pub use queue_service_client::QueueServiceClient;
 
 pub use crate::generated::clients::{
     AzureQueueStorageClient, AzureQueueStorageClientOptions,
