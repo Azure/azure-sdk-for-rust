b986cf5939af4b97c73233ed34e51ec21c0d7792
Colin Desmond <colin.desmond@microsoft.com>
2025-06-11 15:01:18 +0000

    Tweaks to tests.

--- sdk/storage/azure_storage_queue/tests/queue_client.rs
+++ sdk/storage/azure_storage_queue/tests/queue_client.rs
@@ -3,6 +3,11 @@ use azure_core_test::{recorded, Recording, TestContext};
 use azure_storage_queue::clients::AzureQueueStorageClientOptions;
 use azure_storage_queue::clients::QueueClient;
 use std::option::Option;
+use uuid::Uuid;
+
+use once_cell::sync::Lazy;
+
+static QUEUE_SUFFIX: Lazy<String> = Lazy::new(|| get_random_queue_suffix());
 
 /// Creates a new queue under the given account.
 #[recorded::test]
@@ -10,11 +15,12 @@ async fn test_create_queue(ctx: TestContext) -> Result<()> {
     let recording = ctx.recording();
     let queue_client = get_queue_client(recording).await;
 
-    let response = queue_client?.create("test-queue", None).await?;
+    let queue_name = format!("test-queue-{}", QUEUE_SUFFIX.as_str());
+    let response = queue_client?.create(&queue_name, None).await?;
 
     assert!(
-        response.status() == 201,
-        "Expected status code 201, got {}",
+        response.status().is_success(),
+        "Expected success status code, got {}",
         response.status(),
     );
 
@@ -28,20 +34,19 @@ async fn test_create_queue_if_not_exists(ctx: TestContext) -> Result<()> {
     let queue_client = get_queue_client(recording).await?;
 
     // First, create the queue
-    let response = queue_client.create("test-queue-if-exists", None).await?;
+    let queue_name = String::from(format!("test-queue-if-exists-{}", QUEUE_SUFFIX.as_str()));
+    let response = queue_client.create(&queue_name, None).await?;
     assert!(
-        response.status() == 201,
-        "Expected status code 201, got {}",
+        response.status().is_success(),
+        "Expected success status code, got {}",
         response.status(),
     );
 
     // Now, try to create the same queue again
-    let response = queue_client
-        .create_if_not_exists("test-queue-if-exists", None)
-        .await?;
+    let response = queue_client.create_if_not_exists(&queue_name, None).await?;
     assert!(
-        response.status() == 204,
-        "Expected status code 204, got {}",
+        response.status().is_success(),
+        "Expected success status code, got {}",
         response.status(),
     );
 
@@ -52,15 +57,53 @@ async fn test_create_queue_if_not_exists(ctx: TestContext) -> Result<()> {
 #[recorded::test]
 async fn test_delete_queue(ctx: TestContext) -> Result<()> {
     let recording = ctx.recording();
-    let queue_client = get_queue_client(recording).await;
+    let queue_client = get_queue_client(recording).await?;
+    let queue_name = String::from(format!("test-queue-{}", QUEUE_SUFFIX.as_str()));
 
-    let response = queue_client?.delete("test-queue", None).await?;
+    queue_client.create_if_not_exists(&queue_name, None).await?;
+
+    let response = queue_client.delete(&queue_name, None).await?;
 
     assert!(
         response.status() == 204,
         "Expected status code 204, got {}",
         response.status(),
     );
+    Ok(())
+}
+
+#[recorded::test]
+async fn test_delete_queue_if_exists(ctx: TestContext) -> Result<()> {
+    let recording = ctx.recording();
+    let queue_client = get_queue_client(recording).await?;
+    let queue_name = String::from(format!("test-queue-if-exists-{}", QUEUE_SUFFIX.as_str()));
+
+    // First, create the queue
+    let response = queue_client.create(&queue_name, None).await?;
+    assert!(
+        response.status().is_success(),
+        "Expected success status code, got {}",
+        response.status(),
+    );
+
+    // Now, try to delete the same queue
+    let response = queue_client.delete_if_exists(&queue_name, None).await?;
+    assert!(
+        response.status().is_success(),
+        "Expected success status code, got {}",
+        response.status(),
+    );
+
+    // Try to delete a non-existent queue
+    let non_existent_response = queue_client
+        .delete_if_exists("non-existent-queue", None)
+        .await?;
+    assert!(
+        non_existent_response.status().is_success(),
+        "Expected success status code for non-existent queue, got {}",
+        non_existent_response.status(),
+    );
+
     Ok(())
 }
 
@@ -87,8 +130,14 @@ async fn test_exists(ctx: TestContext) -> Result<()> {
     let queue_client = get_queue_client(recording).await?;
 
     // Check if a queue exists
-    let exists_response = queue_client.exists("test-queue").await?;
+    let queue_name = String::from(format!("test-queue-{}", QUEUE_SUFFIX.as_str()));
+
+    queue_client.create_if_not_exists(&queue_name, None).await?;
+
+    let exists_response = queue_client.exists(&queue_name).await?;
     assert!(exists_response, "Queue should exist");
+
+    queue_client.delete(&queue_name, None).await?;
 
     // Check a non-existent queue
     let non_existent_exists_response = queue_client.exists("non-existent-queue").await?;
@@ -134,4 +183,7 @@ fn recorded_test_setup(recording: &Recording) -> (ClientOptions, String) {
     );
 
     (client_options, endpoint)
+}
+fn get_random_queue_suffix() -> String {
+    format!("{}", Uuid::new_v4())
 }
