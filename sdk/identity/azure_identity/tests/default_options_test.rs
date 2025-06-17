48d541b9dfe0c2050a9878a1ec896e90bfe62224
Charles Lowell <10964656+chlowell@users.noreply.github.com>
2025-06-17 20:14:22 +0000

    fmt

--- sdk/identity/azure_identity/tests/default_options_test.rs
+++ sdk/identity/azure_identity/tests/default_options_test.rs
@@ -1,15 +1,15 @@
 use azure_identity::{AzureCliCredentialOptions, AzureDeveloperCliCredentialOptions};
 
 #[test]
-fn az_credential_options_default() {
-    let _options = AzureCliCredentialOptions{
+fn az_credential_options() {
+    let _options = AzureCliCredentialOptions {
         ..Default::default()
     };
 }
 
 #[test]
-fn azd_credential_options_default() {
-    let _options = AzureDeveloperCliCredentialOptions{
+fn azd_credential_options() {
+    let _options = AzureDeveloperCliCredentialOptions {
         ..Default::default()
     };
 }
