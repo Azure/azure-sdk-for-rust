34eca1eca79d8e350d93f443704c252b2be0b2d6
Vincent Tran <vincenttran@microsoft.com>
2025-07-31 14:33:43 -0700

    Regenerate against adjusted .tsp, ETag -> Etag

--- sdk/storage/azure_storage_blob/tests/blob_container_client.rs
+++ sdk/storage/azure_storage_blob/tests/blob_container_client.rs
@@ -106,9 +106,11 @@ async fn test_list_blobs(ctx: TestContext) -> Result<(), Box<dyn Error>> {
     let blob_list = list_blob_segment_response.segment.blob_items;
     for blob in blob_list {
         let blob_name = blob.name.unwrap().content.unwrap();
-        let blob_type = blob.properties.unwrap().blob_type.unwrap();
+        let blob_type = blob.properties.clone().unwrap().blob_type.unwrap();
+        let etag = blob.properties.unwrap().etag;
         assert!(blob_names.contains(&blob_name));
         assert_eq!(BlobType::BlockBlob, blob_type);
+        assert!(etag.is_some());
     }
 
     container_client.delete_container(None).await?;
