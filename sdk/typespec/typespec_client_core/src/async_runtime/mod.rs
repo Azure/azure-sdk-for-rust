d65a3f16f511a48b5347002fb300f516cdbd11f4
magodo <wztwcy@gmail.com>
2025-08-21 15:16:42 +1000

    update

--- sdk/typespec/typespec_client_core/src/async_runtime/mod.rs
+++ sdk/typespec/typespec_client_core/src/async_runtime/mod.rs
@@ -185,6 +185,11 @@ pub fn set_async_runtime(runtime: Arc<dyn AsyncRuntime>) -> crate::Result<()> {
 }
 
 fn create_async_runtime() -> Arc<dyn AsyncRuntime> {
+    #[cfg(all(feature = "wasm_bindgen", feature = "tokio"))]
+    compile_error!(
+        "feature \"wasm_bindgen\" and feature \"tokio\" cannot be enabled at the same time"
+    );
+
     #[cfg(all(target_arch = "wasm32", feature = "wasm_bindgen"))]
     {
         Arc::new(web_runtime::WasmBindgenRuntime) as Arc<dyn AsyncRuntime>
