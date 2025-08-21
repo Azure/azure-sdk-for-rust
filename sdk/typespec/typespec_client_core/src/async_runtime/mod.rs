2bdb037300cc1b30c44bd28e4da1cfece54495da
magodo <wztwcy@gmail.com>
2025-08-21 14:55:01 +1000

    `create_async_runtime`: Make `cfg` checks exhaustive

--- sdk/typespec/typespec_client_core/src/async_runtime/mod.rs
+++ sdk/typespec/typespec_client_core/src/async_runtime/mod.rs
@@ -187,13 +187,16 @@ pub fn set_async_runtime(runtime: Arc<dyn AsyncRuntime>) -> crate::Result<()> {
 fn create_async_runtime() -> Arc<dyn AsyncRuntime> {
     #[cfg(all(target_arch = "wasm32", feature = "wasm_bindgen"))]
     {
-        Arc::new(web_runtime::WasmBindgenRuntime) as Arc<dyn AsyncRuntime>
+        return Arc::new(web_runtime::WasmBindgenRuntime) as Arc<dyn AsyncRuntime>;
     }
     #[cfg(feature = "tokio")]
     {
-        Arc::new(tokio_runtime::TokioRuntime) as Arc<dyn AsyncRuntime>
+        return Arc::new(tokio_runtime::TokioRuntime) as Arc<dyn AsyncRuntime>;
     }
-    #[cfg(not(any(feature = "tokio", feature = "wasm_bindgen")))]
+    #[cfg(not(any(
+        feature = "tokio",
+        all(target_arch = "wasm32", feature = "wasm_bindgen")
+    )))]
     {
         Arc::new(standard_runtime::StdRuntime) as Arc<dyn AsyncRuntime>
     }
