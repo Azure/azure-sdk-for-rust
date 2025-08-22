46815020601aa91757b7eb6263d9b6c99011036d
Heath Stewart <heaths@microsoft.com>
2025-08-22 11:21:29 -0700

    Add doc_auto_cfg to all crates

--- sdk/keyvault/azure_security_keyvault_secrets/src/lib.rs
+++ sdk/keyvault/azure_security_keyvault_secrets/src/lib.rs
@@ -2,6 +2,7 @@
 // Licensed under the MIT License.
 
 #![doc = include_str!("../README.md")]
+#![cfg_attr(docsrs, feature(doc_auto_cfg))]
 
 #[expect(deprecated, reason = "requires emitter update")]
 mod generated;
