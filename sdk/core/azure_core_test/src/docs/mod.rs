571de3281c385d6715813a91e862a577214ce5ae
Heath Stewart <heaths@microsoft.com>
2025-08-25 22:36:32 -0700

    Make sure the root README is tests

---
+++ sdk/core/azure_core_test/src/docs/mod.rs
@@ -0,0 +1,6 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+//! Tests for other documentation examples in the [Azure/azure-sdk-for-rust](https://github.com/Azure/azure-sdk-for-rust) repository.
+
+mod root_readme;
