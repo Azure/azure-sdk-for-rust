f9f69fc4d618baacb84bd88b0a9f51cd422ba089
Larry Osterman <larryo@microsoft.com>
2025-08-28 13:24:29 -0700

    CSpell fixes

--- sdk/eventhubs/azure_messaging_eventhubs_checkpointstore_blob/examples/checkpoint_store_basic.rs
+++ sdk/eventhubs/azure_messaging_eventhubs_checkpointstore_blob/examples/checkpoint_store_basic.rs
@@ -1,7 +1,7 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 
-// cspell:ignore appender
+// cspell:ignore appender OTLP
 
 //! Basic example showing how to create and use a blob checkpoint store.
 
