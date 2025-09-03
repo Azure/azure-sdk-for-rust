88b67165553230a0cceefce2062ecab7593500c3
Ashley Stanton-Nurse <github@analogrelay.net>
2025-09-03 20:59:32 +0000

    pr feedback

--- sdk/cosmos/azure_data_cosmos_native/src/lib.rs
+++ sdk/cosmos/azure_data_cosmos_native/src/lib.rs
@@ -1,19 +1,19 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 
-use std::ffi::{c_char, CStr, CString};
+use std::ffi::{c_char, CStr};
 
 #[macro_use]
 mod macros;
 
-#[no_mangle] // Necessary to prevent the compiler from stripping it when optimizing
 /// cbindgen:ignore
+#[no_mangle] // Necessary to prevent the compiler from stripping it when optimizing
 pub static BUILD_IDENTIFIER: &CStr = c_str!(env!("BUILD_IDENTIFIER"));
 
 const VERSION: &CStr = c_str!(env!("CARGO_PKG_VERSION"));
 
-#[no_mangle]
 /// Returns a constant C string containing the version of the Cosmos Client library.
+#[no_mangle]
 pub extern "C" fn cosmosclient_version() -> *const c_char {
     VERSION.as_ptr()
 }
