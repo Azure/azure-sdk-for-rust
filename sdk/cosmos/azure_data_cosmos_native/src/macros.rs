58d1a8c24241df275e66335390a5e618b0f0ed8f
Ashley Stanton-Nurse <github@analogrelay.net>
2025-09-04 17:12:50 +0000

    cspell fixes

--- sdk/cosmos/azure_data_cosmos_native/src/macros.rs
+++ sdk/cosmos/azure_data_cosmos_native/src/macros.rs
@@ -8,14 +8,14 @@ macro_rules! c_str {
             // Which ensures the string is generated as a c-str at compile time
             const STR: &str = $s;
             const BYTES: [u8; STR.len() + 1] = const {
-                let mut cstrbuf: [u8; STR.len() + 1] = [0; STR.len() + 1];
+                let mut cstr_buf: [u8; STR.len() + 1] = [0; STR.len() + 1];
                 let mut i = 0;
                 // For loops over ranges don't work in const contexts yet.
                 while i < STR.len() {
-                    cstrbuf[i] = STR.as_bytes()[i];
+                    cstr_buf[i] = STR.as_bytes()[i];
                     i += 1;
                 }
-                cstrbuf
+                cstr_buf
             };
             match CStr::from_bytes_with_nul(&BYTES) {
                 Ok(cstr) => cstr,
