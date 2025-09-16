2e7bf139a561b32f299f79796b625813443b8ceb
Heath Stewart <heaths@microsoft.com>
2025-09-16 13:55:00 -0700

    Fix version-specific test assert

--- sdk/keyvault/azure_security_keyvault_secrets/tests/secret_client.rs
+++ sdk/keyvault/azure_security_keyvault_secrets/tests/secret_client.rs
@@ -275,7 +275,7 @@ async fn round_trip_secret_verify_telemetry(ctx: TestContext) -> Result<()> {
         },
         ExpectedInstrumentation {
             package_name: recording.var("CARGO_PKG_NAME", None),
-            package_version: recording.var("CARGO_PKG_VERSION", None),
+            package_version: env!("CARGO_PKG_VERSION").into(),
             package_namespace: Some("KeyVault"),
             api_calls: vec![
                 ExpectedApiInformation {
