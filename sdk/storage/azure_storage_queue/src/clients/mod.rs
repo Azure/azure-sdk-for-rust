a13b9829b93c6305b54c00c379630d8cf233240a
Likhan Siddiquee <likhan.siddiquee@gmail.com>
2025-09-17 11:11:00 +0200

    Updating client module visibility as per review comments
    Co-authored-by: Heath Stewart <heaths@microsoft.com>

--- sdk/storage/azure_storage_queue/src/clients/mod.rs
+++ sdk/storage/azure_storage_queue/src/clients/mod.rs
@@ -1,7 +1,7 @@
-pub mod queue_client;
+mod queue_client;
 pub use queue_client::QueueClient;
 
-pub mod queue_service_client;
+mod queue_service_client;
 pub use queue_service_client::QueueServiceClient;
 
 pub use crate::generated::clients::{QueueClientOptions, QueueServiceClientOptions};
