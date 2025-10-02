177b88aace9441ca5d51b84129de5c6ef82d61e0
Ashley Stanton-Nurse <github@analogrelay.net>
2025-10-02 19:00:52 +0000

    add container routing map and tests

---
+++ sdk/cosmos/azure_data_cosmos/src/types.rs
@@ -0,0 +1,41 @@
+//! Internal module to define several newtypes used in the SDK.
+
+macro_rules! string_newtype {
+    ($name:ident) => {
+        #[derive(serde::Deserialize, serde::Serialize, Debug, Clone, PartialEq, Eq, PartialOrd, Ord, Hash)]
+        #[serde(transparent)]
+        pub struct $name(String);
+
+        impl $name {
+            pub fn new(value: String) -> Self {
+                Self(value)
+            }
+
+            pub fn value(&self) -> &str {
+                &self.0
+            }
+        }
+
+        impl std::fmt::Display for $name {
+            fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+                write!(f, "{}", self.0)
+            }
+        }
+
+        impl From<&str> for $name {
+            fn from(s: &str) -> Self {
+                Self(s.to_string())
+            }
+        }
+
+        impl From<String> for $name {
+            fn from(s: String) -> Self {
+                Self(s)
+            }
+        }
+    };
+}
+
+string_newtype!(ResourceId);
+string_newtype!(PartitionKeyRangeId);
+string_newtype!(EffectivePartitionKey);
