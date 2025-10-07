f11022e519113d395c415319236ecafc799737ba
Ashley Stanton-Nurse <github@analogrelay.net>
2025-10-07 17:45:05 +0000

    update update-cratenames to get ALL dependency crates

--- eng/scripts/update-cratenames.rs
+++ eng/scripts/update-cratenames.rs
@@ -9,7 +9,7 @@ toml = "0.8.10"
 ---
 
 use cargo_util_schemas::manifest::TomlManifest;
-use std::{ffi::OsStr, fs, io::Write as _, path::PathBuf};
+use std::{collections::HashSet, ffi::OsStr, fs, io::Write as _, path::PathBuf};
 
 fn main() {
     let workspace_root = get_workspace_root();
@@ -29,25 +29,52 @@ fn main() {
         .dependencies
         .as_ref()
         .expect("expected workspace dependencies");
-    let mut crate_names: Vec<String> = dependencies.iter().map(|(name, _)| name.to_string()).collect();
+    let mut crate_names: HashSet<String> = dependencies
+        .iter()
+        .map(|(name, _)| name.to_string())
+        .collect();
 
     // Extract workspace members.
-    for relative_path in workspace_manifest
+    let members = workspace_manifest
         .workspace
         .as_ref()
         .expect("expected workspace")
         .members
         .as_ref()
-        .expect("expected workspace members")
-        .into_iter() {
-            let crate_name = PathBuf::from(relative_path)
-                .file_stem()
-                .and_then(OsStr::to_str)
-                .expect("expected crate name")
-                .to_string();
-            crate_names.push(crate_name);
+        .expect("expected workspace members");
+
+    for relative_path in members.into_iter() {
+        let member_dir_name = PathBuf::from(relative_path)
+            .file_stem()
+            .map(|s| s.to_string_lossy().into_owned())
+            .expect("member directory name");
+        let member_manifest_path = workspace_root.join(relative_path).join("Cargo.toml");
+        eprintln!("Reading member manifest {member_manifest_path:?}");
+        let member_manifest = fs::read_to_string(&member_manifest_path)
+            .unwrap_or_else(|_| panic!("member manifest content: {member_manifest_path:?}"));
+        let member_manifest: TomlManifest = toml::from_str(&member_manifest)
+            .unwrap_or_else(|_| panic!("deserialize member manifest: {member_manifest_path:?}"));
+
+        let sections = [
+            member_manifest.dependencies,
+            member_manifest.dev_dependencies,
+            member_manifest.build_dependencies,
+        ];
+        for section in sections.iter().flatten() {
+            for (name, _) in section.iter() {
+                crate_names.insert(name.to_string());
+            }
         }
 
+        // Add the crate name for this member.
+        if let Some(name) = member_manifest.package.as_ref().map(|p| p.name.as_ref()) {
+            crate_names.insert(name.to_string());
+        } else {
+            crate_names.insert(member_dir_name.to_string());
+        }
+    }
+
+    let mut crate_names: Vec<String> = crate_names.into_iter().collect();
     crate_names.sort();
     let crate_names_path = workspace_root
         .join("eng/dict/crates.txt")
