c1bb71888678ad20ad5c7c8712ee75bb84054a5c
Vincent Tran <vincenttran@microsoft.com>
2025-10-23 17:51:45 -0700

    nit

--- sdk/storage/azure_storage_blob/tests/blob_client.rs
+++ sdk/storage/azure_storage_blob/tests/blob_client.rs
@@ -654,9 +654,8 @@ async fn test_encoding_edge_cases(ctx: TestContext) -> Result<(), Box<dyn Error>
     // Extract all blob names from list_blobs() response
     let listed_blob_names: Vec<String> = blob_items
         .iter()
-        .map(|blob| blob.name.clone().unwrap().try_into())
-        .collect::<Result<Vec<String>, _>>()?;
-
+        .map(|blob| blob.name.clone().unwrap().content.unwrap())
+        .collect();
     // Verify each test case blob name appears in the list (with normalization)
     for blob_name in test_cases {
         let normalized_name = blob_name.replace('\\', "/");
