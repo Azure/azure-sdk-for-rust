caee73be691a820944835a98fe828f1e78cf8046
Vincent Tran <vincenttran@microsoft.com>
2025-10-28 15:45:59 -0700

    deserialize/seralize, XML methods

--- sdk/storage/azure_storage_blob/src/generated/clients/blob_service_client.rs
+++ sdk/storage/azure_storage_blob/src/generated/clients/blob_service_client.rs
@@ -481,7 +481,7 @@ impl BlobServiceClient {
                         )
                         .await?;
                     let (status, headers, body) = rsp.deconstruct();
-                    let res: ListContainersSegmentResponse = xml::from_xml(&body)?;
+                    let res: ListContainersSegmentResponse = xml::read_xml(&body)?;
                     let rsp = RawResponse::from_bytes(status, headers, body).into();
                     Ok(match res.next_marker {
                         Some(next_marker) if !next_marker.is_empty() => PagerResult::More {
