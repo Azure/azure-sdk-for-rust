f6f850978ae8605bc2ab964eb348004dd4be10cc
Ashley Stanton-Nurse <ashleyst@microsoft.com>
2025-10-28 10:02:50 -0700

    cosmos: Allow the Query Pipeline to return an alternative query to execute in each request (#3166)
    For Hybrid Search, as well as ReadMany support, the query engine will
    need to direct the SDK to issue **multiple different queries**. However,
    the Rust SDK's query executor expects to be able to replay the original
    query on each request. This PR changes that expectation to allow (but
    not require) each Query Request coming from the pipeline to provide an
    alternate query. When an alternate query is provided, the pipeline can
    also indicate if the parameters from the original query should be sent.
    Often, the alternate query does NOT use the parameters, but in some
    circumstances (hybrid search) it will.
    
    There are also some small fixes for doc tests and error checking in the
    engine-less cross-partition query tests. Not sure why these are coming
    up here and not in `main`.

--- sdk/cosmos/azure_data_cosmos/src/query/engine.rs
+++ sdk/cosmos/azure_data_cosmos/src/query/engine.rs
@@ -3,14 +3,42 @@ pub struct QueryRequest {
     /// The ID of the partition key range to query.
     pub partition_key_range_id: String,
 
+    /// The index of this request, within the partition key range.
+    ///
+    /// This value will always increase for subsequent requests for the same partition key range.
+    /// It must be provided back to the pipeline when providing data, so that the pipeline can ensure that data is provided in order.
+    pub index: usize,
+
     /// The continuation to use, if any.
     pub continuation: Option<String>,
+
+    /// The query to execute for this partition key range, if different from the original query.
+    pub query: Option<String>,
+
+    /// If a query is specified, this flag indicates if the query parameters should be included with that query.
+    ///
+    /// Sometimes, when an override query is specified, it differs in structure from the original query, and the original parameters are not valid.
+    pub include_parameters: bool,
+
+    /// If specified, indicates that the SDK should IMMEDIATELY drain all remaining results from this partition key range, following continuation tokens, until no more results are available.
+    /// All the data from this partition key range should be provided BEFORE any new items will be made available.
+    ///
+    /// This allows engines to optimize for non-streaming scenarios, where the entire result set must be provided to the engine before it can make progress.
+    pub drain: bool,
 }
 
 /// The request of a single-partition query for a specific partition key range.
 pub struct QueryResult<'a> {
+    /// The ID of the partition key range that was queried.
     pub partition_key_range_id: &'a str,
+
+    /// The index of the [`QueryRequest`] that generated this result.
+    pub request_index: usize,
+
+    /// The continuation token to be used for the next request, if any.
     pub next_continuation: Option<String>,
+
+    /// The raw body of the response from the query.
     pub result: &'a [u8],
 }
 
@@ -38,7 +66,16 @@ pub trait QueryPipeline: Send {
     fn run(&mut self) -> azure_core::Result<PipelineResult>;
 
     /// Provides additional single-partition data to the pipeline.
-    fn provide_data(&mut self, data: QueryResult) -> azure_core::Result<()>;
+    ///
+    /// Data from multiple partition ranges may be provided at once.
+    /// However, each page of data must be provided in order.
+    /// So, for any given partition key range, page n's results must be earlier in the `data` vector than page n+1's results.
+    /// Data from different partition key ranges may be interleaved, as long as each partition key range's pages are in order.
+    ///
+    /// The pipeline will use the [`QueryResult::request_index`] field to validate this.
+    ///
+    /// When providing data from a draining request (i.e. a request with `drain = true`), all pages for that draining request can share the same [`QueryResult::request_index`].
+    fn provide_data(&mut self, data: Vec<QueryResult>) -> azure_core::Result<()>;
 }
 
 /// Provides an interface to a query engine, which constructs query pipelines.
