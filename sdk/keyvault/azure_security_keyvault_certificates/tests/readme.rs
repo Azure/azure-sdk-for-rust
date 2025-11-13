df95d6f464c3329a58e3dccc7fbd476ded08e20c
Heath Stewart <heaths@microsoft.com>
2025-11-12 18:06:00 -0800

    Update Key Vault keys and certificates READMEs

--- sdk/keyvault/azure_security_keyvault_certificates/tests/readme.rs
+++ sdk/keyvault/azure_security_keyvault_certificates/tests/readme.rs
@@ -7,47 +7,53 @@ use azure_core::{
     time::Duration,
 };
 use azure_core_test::{recorded, TestContext, TestMode};
-use azure_security_keyvault_secrets::SecretClient;
+use azure_security_keyvault_certificates::CertificateClient;
 use include_file::include_markdown;
 
 #[recorded::test]
 async fn readme(ctx: TestContext) -> Result<()> {
-    use azure_security_keyvault_secrets::SecretClientOptions;
+    use azure_security_keyvault_certificates::CertificateClientOptions;
 
     let recording = ctx.recording();
 
-    let mut options = SecretClientOptions::default();
+    let mut options = CertificateClientOptions::default();
     recording.instrument(&mut options.client_options);
 
-    let client = SecretClient::new(
+    let client = CertificateClient::new(
         recording.var("AZURE_KEYVAULT_URL", None).as_str(),
         recording.credential(),
         Some(options),
     )?;
 
     // Each macro invocation is in its own block to prevent errors with duplicate imports.
-    println!("Create a secret");
-    include_markdown!("README.md", "create_secret", scope);
+    println!("Create a certificate");
+    include_markdown!("README.md", "create_certificate", scope);
 
-    println!("Get a secret");
-    include_markdown!("README.md", "get_secret", scope);
+    println!("Get a certificate");
+    include_markdown!("README.md", "get_certificate", scope);
 
-    println!("Update a secret");
-    include_markdown!("README.md", "update_secret", scope);
+    println!("Update a certificate");
+    include_markdown!("README.md", "update_certificate", scope);
 
-    println!("List secrets");
-    include_markdown!("README.md", "list_secrets", scope);
+    println!("List certificates");
+    include_markdown!("README.md", "list_certificates", scope);
+
+    println!("Key operations using certificates");
+    include_markdown!("README.md", "key_operations", scope);
 
     println!("Handle errors");
     include_markdown!("README.md", "errors", scope);
 
-    println!("Delete a secret");
-    include_markdown!("README.md", "delete_secret", scope);
+    println!("Delete a certificate");
+    include_markdown!("README.md", "delete_certificate", scope);
 
-    // Make sure the secret gets purged (may not take immediate effect).
-    println!("Purge a secret");
+    // Make sure the certificate gets purged (may not take immediate effect).
+    println!("Purge a certificate");
     for _ in 0..5 {
-        match client.purge_deleted_secret("secret-name", None).await {
+        match client
+            .purge_deleted_certificate("certificate-name", None)
+            .await
+        {
             Ok(_) => break,
             Err(err) if matches!(err.kind(), ErrorKind::HttpResponse { status, .. } if *status == StatusCode::Conflict) => {
                 if recording.test_mode() != TestMode::Playback {
