9ab67fe13d75af1ec3c9b3c91459060027187bc5
Daniel Jurek <djurek@microsoft.com>
2025-11-25 18:38:53 +0000

    Add semver checks

---
+++ eng/scripts/Test-Semver.ps1
@@ -0,0 +1,66 @@
+#!/usr/bin/env pwsh
+
+#Requires -Version 7.0
+[CmdletBinding(DefaultParameterSetName = "none")]
+param(
+  [Parameter(ParameterSetName = 'Named')]
+  [string[]]$PackageNames,
+  [Parameter(ParameterSetName = 'PackageInfo')]
+  [string]$PackageInfoDirectory
+)
+
+. ([System.IO.Path]::Combine($PSScriptRoot, '..', 'common', 'scripts', 'common.ps1'))
+. ([System.IO.Path]::Combine($PSScriptRoot, 'shared', 'Cargo.ps1'))
+
+function Get-OutputPackageNames($workspacePackages) {
+  $packablePackages = $workspacePackages | Where-Object -Property publish -NE -Value @()
+  $packablePackageNames = $packablePackages.name
+
+  $names = @()
+  switch ($PsCmdlet.ParameterSetName) {
+    'Named' {
+      $names = $PackageNames
+    }
+
+    'PackageInfo' {
+      $names = Get-PackageNamesFromPackageInfo $PackageInfoDirectory
+    }
+
+    default {
+      return $packablePackageNames
+    }
+  }
+
+  foreach ($name in $names) {
+    if (-not $packablePackageNames.Contains($name)) {
+      Write-Error "Package '$name' is not in the workspace or does not publish"
+      exit 1
+    }
+  }
+
+  return $names
+}
+
+$packages = Get-CargoPackages
+$outputPackageNames = Get-OutputPackageNames $packages
+
+LogGroupStart "cargo install cargo-semver-checks"
+Write-Host "cargo install cargo-semver-checks"
+cargo install cargo-semver-checks --locked
+LogGroupEnd
+
+$packageParams = @()
+foreach ($packageName in $outputPackageNames) {
+  $packageParams += "--package"
+  $packageParams += $packageName
+}
+
+LogGroupStart "cargo semver-checks $($packageParams -join ' ')"
+Write-Host "cargo semver-checks $($packageParams -join ' ')"
+cargo semver-checks @packageParams
+LogGroupEnd
+
+if ($LASTEXITCODE -ne 0) {
+  LogError "SemVer checks failed"
+  exit $LASTEXITCODE
+}
