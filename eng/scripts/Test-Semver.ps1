42219b02fd531aa4dbad1bcb6a20c18cc37c2d26
Daniel Jurek <djurek@microsoft.com>
2025-12-01 12:23:27 -0800

    Add SemVer checks (#3375)
    * Fixes #1605
    * Check "stable" Rust on Linux only (others add significant time to
    build)
    * Add `eng/cgmanfiest.json` to track installation of a dev tool in the
    build system
    * `Test-Semver.ps1` uses the version specified in `eng/cgmanifest.json`
    
    Tested: 
    
    | Scenario | Result | 
    | -------- | ------ | 
    | No packages changed | | 
    | [Checks SemVer of changed package in
    PR](https://dev.azure.com/azure-sdk/public/_build/results?buildId=5614015&view=logs&j=906b8cf8-77d6-5b97-38eb-78e393929662&t=a7849eae-de43-5ea0-75a4-5b970d68e244)
    | Success (expected) |
    | [Fails on SemVer violation in
    PR](https://dev.azure.com/azure-sdk/public/_build/results?buildId=5614218&view=logs&j=b766ebde-1fdb-5f11-1350-46ddc53b23cf&t=7ef29fa0-f532-5653-3473-16dc04608431)
    | Fail (expected) |
    | [Checks SemVer of packages in
    service](https://dev.azure.com/azure-sdk/internal/_build/results?buildId=5614246&view=logs&j=b766ebde-1fdb-5f11-1350-46ddc53b23cf&t=9494b470-f365-50d2-5008-6394593b39c5)
    | Fail (expected) |
    | [Checks SemVer of packages in release (and blocks
    release)](https://dev.azure.com/azure-sdk/internal/_build/results?buildId=5614282&view=logs&j=b766ebde-1fdb-5f11-1350-46ddc53b23cf&t=9494b470-f365-50d2-5008-6394593b39c5)
    | Fail (expected) |
    
    # Test performance
    
    SemVer checks add a non-trivial amount of time in some OS configurations
    (Windows, MacOS) because `rust-semver-checks` is built.
    
    | Test Job | OS | Time | 
    | -------- | -- | ---- | 
    | Unit | Linux | 1m42s | 
    | Unit | Windows | 5m40s | 
    | Unit | Mac | 7m2s | 
    | Semver | Linux | 6m13s | 
    | Semver | Windows | 15m3s | 
    | Semver | Mac | 21m19s | 
    
    `rust-semver-check` build times (look at logs for builds of stable in
    [this PR
    run](https://dev.azure.com/azure-sdk/public/_build/results?buildId=5613783&view=logs&j=7e382a76-12de-5903-1057-64e59e53ed06&t=ba3464b8-2ae1-508a-306c-badcf96fe2c9)):
    
    | OS | Time | 
    | -- | ---- | 
    | Linux | 4m50s | 
    | Windows | 10m15s | 
    | Mac | 17m12s | 
    
    
    ## Caching considerations
    
    Also discussed here:
    https://github.com/Azure/azure-sdk-for-rust/issues/3158
    
    Azure DevOps caches are scoped according to [several
    factors](https://learn.microsoft.com/en-us/azure/devops/pipelines/release/caching?view=azure-devops&tabs=bundler#cache-isolation-and-security).
    In the case of a PR where speed is most important, we would need a job
    that runs against `main` to populate a cache that PRs could read. We
    don't currently schedule PR pipelines to run. In fact, vcpkg, uses its
    own cache mechanism with relevant rules.
    
    If we needed to semver check across OS quickly, we could use a storage
    account to hold pre-built binaries (similar to vcpkg). The binaries
    would be built in a Rust pipeline and uploaded to the storage account
    with a path prefix like `carge-semver-checks/ubuntu-24/0.45.0/`.
    Something would need to update the cache and cgmanifes.json file and
    proper arrangements would need to be made locally so that calling `cargo
    semver-checks` would use the correct binary.
    
    Similar work could also be done to cache source analysis binaries
    
    # Other considerations
    
    Checking "nightly" and "msrv" versions of Rust is more complicated.
    Generally, [`cargo-semver-checks` supports
    "stable"](https://github.com/obi1kenobi/cargo-semver-checks?tab=readme-ov-file#what-rust-versions-does-cargo-semver-checks-support)
    so SemVer checking focuses there.
    
    PRs should also block on semver-breaking changes. It may be possible
    that future version schemes (e.g. adding `-beta.x` to a version number)
    may not block PRs because semver allows breaking changes in `-beta.x`
    versions. This will need to be investigated as part of the version
    incrementing script. We'd either need to adjust the version scheme or
    update `-beta.x` at CI execution time.

---
+++ eng/scripts/Test-Semver.ps1
@@ -0,0 +1,80 @@
+#!/usr/bin/env pwsh
+
+#Requires -Version 7.0
+[CmdletBinding(DefaultParameterSetName = "none")]
+param(
+  [Parameter(ParameterSetName = 'Named')]
+  [string[]]$PackageNames,
+  [Parameter(ParameterSetName = 'PackageInfo')]
+  [string]$PackageInfoDirectory,
+  [switch]$IgnoreCgManifestVersion
+)
+
+. ([System.IO.Path]::Combine($PSScriptRoot, '..', 'common', 'scripts', 'common.ps1'))
+. ([System.IO.Path]::Combine($PSScriptRoot, 'shared', 'Cargo.ps1'))
+
+function Get-OutputPackageNames($workspacePackages) {
+  $packablePackages = $workspacePackages | Where-Object -Property publish -NE -Value @()
+  $packablePackageNames = $packablePackages.name
+
+  $names = @()
+  switch ($PsCmdlet.ParameterSetName) {
+    'Named' {
+      $names = $PackageNames
+    }
+
+    'PackageInfo' {
+      $names = Get-PackageNamesFromPackageInfo $PackageInfoDirectory
+    }
+
+    default {
+      return $packablePackageNames
+    }
+  }
+
+  foreach ($name in $names) {
+    if (-not $packablePackageNames.Contains($name)) {
+      Write-Error "Package '$name' is not in the workspace or does not publish"
+      exit 1
+    }
+  }
+
+  return $names
+}
+
+$packages = Get-CargoPackages
+$outputPackageNames = Get-OutputPackageNames $packages
+
+# Read version from cgmanifest.json. If ignored the currently installed or
+# "latest" version is used.
+$versionParams = @()
+if (!$IgnoreCgManifestVersion) {
+  $versionParams += '--version'
+  $cgManifest = Get-Content ([System.IO.Path]::Combine($PSScriptRoot, '..', 'cgmanifest.json')) `
+  | ConvertFrom-Json
+  $versionParams += $cgManifest.
+  registrations.
+  Where({ $_.component.type -eq 'cargo' -and $_.component.cargo.name -eq 'cargo-semver-checks' }).
+  component.cargo.version
+}
+
+LogGroupStart "cargo install cargo-semver-checks --locked $($versionParams -join ' ')"
+Write-Host "cargo install cargo-semver-checks --locked $($versionParams -join ' ')"
+cargo install cargo-semver-checks --locked @versionParams
+LogGroupEnd
+
+$packageParams = @()
+foreach ($packageName in $outputPackageNames) {
+  $packageParams += "--package"
+  $packageParams += $packageName
+}
+
+LogGroupStart "cargo semver-checks $($packageParams -join ' ')"
+Write-Host "cargo semver-checks $($packageParams -join ' ')"
+cargo semver-checks @packageParams
+LogGroupEnd
+
+if ($LASTEXITCODE -ne 0) {
+  LogError "SemVer checks failed"
+  exit $LASTEXITCODE
+}
