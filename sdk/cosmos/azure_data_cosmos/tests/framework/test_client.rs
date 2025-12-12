95a22d52eb2930efaf69fd31341a9535f6a0e7e3
Ashley Stanton-Nurse <ashleyst@microsoft.com>
2025-12-12 11:03:42 -0800

    fix spelling issues

--- sdk/cosmos/azure_data_cosmos/tests/framework/test_client.rs
+++ sdk/cosmos/azure_data_cosmos/tests/framework/test_client.rs
@@ -1,4 +1,7 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
+
+// cspell:ignore: TEAMPROJECTID
 
 #![cfg_attr(not(feature = "key_auth"), allow(dead_code))]
 
