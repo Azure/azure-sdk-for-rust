111f132961adac1b4b44daa39846177f616a933f
Jocelyn <jaschrep@microsoft.com>
2025-12-15 10:39:59 -0500

    headers

--- sdk/storage/azure_storage_blob/src/test_extensions/body.rs
+++ sdk/storage/azure_storage_blob/src/test_extensions/body.rs
@@ -1,3 +1,6 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
 use azure_core::http::Body;
 use bytes::{Bytes, BytesMut};
 
--- sdk/storage/azure_storage_blob/src/test_extensions/mod.rs
+++ sdk/storage/azure_storage_blob/src/test_extensions/mod.rs
@@ -1,2 +1,5 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
 pub(crate) mod body;
 pub(crate) mod streams;
