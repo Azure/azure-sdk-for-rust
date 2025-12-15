9df7efab857293f6eb554ea4f442b667f055498a
Ashley Stanton-Nurse <ashleyst@microsoft.com>
2025-12-15 10:07:44 -0800

    cleanup and cspell fixes

--- sdk/cosmos/azure_data_cosmos/Test-Cleanup.ps1
+++ sdk/cosmos/azure_data_cosmos/Test-Cleanup.ps1
@@ -1,10 +1,24 @@
 # Copyright (c) Microsoft Corporation. All rights reserved.
 # Licensed under the MIT License.
+# cSpell:ignore TEAMPROJECTID HOMEDIRECTORY
 
 # Load common ES scripts
 . "$PSScriptRoot\..\..\..\eng\common\scripts\common.ps1"
 
-if (Get-Command docker -ErrorAction SilentlyContinue) {
+$IsAzDo = ($null -ne $env:SYSTEM_TEAMPROJECTID)
+if($IsAzDo) {
+    $AzDoEmulatorPath = Join-Path $env:AGENT_HOMEDIRECTORY "..\..\Program Files\Azure Cosmos DB Emulator\Microsoft.Azure.Cosmos.Emulator.exe"
+}
+
+
+if ($IsWindows) {
+    if (Test-Path $AzDoEmulatorPath) {
+        Write-Host "Detected Azure DevOps Agent environment with Cosmos DB Emulator. Stopping Cosmos DB Emulator."
+        Invoke-LoggedCommand "`"$AzDoEmulatorPath`" /shutdown" | Out-Null
+    } else {
+        Write-Host "Unable to confirm Cosmos DB Emulator location, skipping cleanup."
+    }
+} else if (Get-Command docker -ErrorAction SilentlyContinue) {
     Write-Host "Docker detected. Stopping Cosmos DB Emulator container."
 
     $containerName = "cosmosdb-emulator-test"
