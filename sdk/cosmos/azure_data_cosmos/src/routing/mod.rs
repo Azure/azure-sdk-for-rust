652b7737d5cf69e43340128b859f927b75825ebf
Debdatta Kunda <87335885+kundadebdatta@users.noreply.github.com>
2025-12-22 09:02:52 -0800

    Code changes to update visibility of async_cache.

--- sdk/cosmos/azure_data_cosmos/src/routing/mod.rs
+++ sdk/cosmos/azure_data_cosmos/src/routing/mod.rs
@@ -1,7 +1,7 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 
-pub mod async_cache;
+pub(crate) mod async_cache;
 pub(crate) mod container_cache;
 pub(crate) mod global_endpoint_manager;
 pub(crate) mod location_cache;
