66e387676585eebc0880b2e2c1772351344956bb
tvaron3 <tomas.varon1802@gmail.com>
2026-01-23 14:53:02 -0500

    fix setup script

--- sdk/cosmos/azure_data_cosmos/Test-Setup.ps1
+++ sdk/cosmos/azure_data_cosmos/Test-Setup.ps1
@@ -98,6 +98,3 @@ if ($IsWindows) {
     # We can't run the emulator on the macOS agent, and we don't want to fail local builds because the emulator isn't installed.
     Write-Host "Cosmos DB Emulator is not available on this platform. Skipping test setup."
 }
-
-# Work around a temporary issue where Invoke-LoggedCommand, which calls us, needs LASTEXITCODE to be set
-$global:LASTEXITCODE = 0
