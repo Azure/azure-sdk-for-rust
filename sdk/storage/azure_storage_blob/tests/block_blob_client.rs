7e9b9f26d4d944e2b2a06163164505c5d94feda4
Jocelyn <jaschrep@microsoft.com>
2026-01-27 14:21:55 -0500

    live only
    Future work to make partitioned block blob uploads recordable.

--- sdk/storage/azure_storage_blob/tests/block_blob_client.rs
+++ sdk/storage/azure_storage_blob/tests/block_blob_client.rs
@@ -213,7 +213,7 @@ async fn test_upload_blob_from_url(ctx: TestContext) -> Result<(), Box<dyn Error
     Ok(())
 }
 
-#[recorded::test]
+#[recorded::test(live)]
 async fn managed_upload(ctx: TestContext) -> Result<(), Box<dyn Error>> {
     let stage_block_count = Arc::new(AtomicUsize::new(0));
     let count_policy = Arc::new(TestPolicy::count_requests(
