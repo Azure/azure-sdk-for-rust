3713bea13535e5f9f52a55766f8a93a23661d99c
Simon Moreno <30335873+simorenoh@users.noreply.github.com>
2026-01-28 11:50:32 -0500

    Update mod.rs

--- sdk/cosmos/azure_data_cosmos/tests/framework/mod.rs
+++ sdk/cosmos/azure_data_cosmos/tests/framework/mod.rs
@@ -17,9 +17,6 @@
 pub mod test_client;
 pub mod test_data;
 
-#[cfg(feature = "preview_query_engine")]
-pub mod query_engine;
-
 pub use test_client::{
     TestClient, TestOptions, TestRunContext, DEFAULT_TEST_TIMEOUT, HUB_REGION, SATELLITE_REGION,
 };
