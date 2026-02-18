17d5fa864193e8570541e06c892e0443e098c723
Fabian Meiswinkel <fabianm@microsoft.com>
2026-02-18 04:27:46 +0000

    Update database_client.rs

--- sdk/cosmos/azure_data_cosmos_native/src/clients/database_client.rs
+++ sdk/cosmos/azure_data_cosmos_native/src/clients/database_client.rs
@@ -131,10 +131,8 @@ pub extern "C" fn cosmos_database_create_container(
             parse_cstr(container_id, error::messages::INVALID_CONTAINER_ID)?.to_string();
         let partition_key_path =
             parse_cstr(partition_key_path, error::messages::INVALID_PARTITION_KEY)?.to_string();
-        let properties = ContainerProperties::new(
-            container_id.clone(),
-            partition_key_path.clone().into(),
-        );
+        let properties =
+            ContainerProperties::new(container_id.clone(), partition_key_path.clone().into());
 
         database.create_container(properties, None).await?;
 
