5eae3be3e28364861b3eabdc5830056aef860092
Brian Caswell <bcaswell@microsoft.com>
2026-03-17 22:02:00 +0000

    revert changes to the SDK, as that isn't needed and update the example to demonstrate that.

--- sdk/storage/azure_storage_blob/examples/blob_upload_seekable_stream.rs
+++ sdk/storage/azure_storage_blob/examples/blob_upload_seekable_stream.rs
@@ -18,63 +18,150 @@
 //!   <file-path>
 //! ```
 
-use azure_core::stream::{SeekableStream, DEFAULT_BUFFER_SIZE};
+use azure_core::{
+    http::{Body, NoFormat, RequestContent},
+    stream::{SeekableStream, DEFAULT_BUFFER_SIZE},
+    Bytes,
+};
 use azure_identity::AzureCliCredential;
 use azure_storage_blob::BlobContainerClient;
 use futures::{task::Poll, Future};
 use std::{
-    env, ffi::OsString, fmt, io, io::SeekFrom, path::Path, path::PathBuf, sync::Arc, task::Context,
+    cmp::min, env, ffi::OsString, io, io::SeekFrom, path::Path, path::PathBuf, pin::Pin, sync::Arc,
+    task::Context,
 };
 use tokio::{
     fs::File,
-    io::{AsyncReadExt, AsyncSeekExt},
+    io::{AsyncReadExt, AsyncSeekExt, Take},
     sync::Mutex,
 };
+use tracing::debug;
 
-#[derive(Clone)]
-struct FileStream {
-    handle: Arc<Mutex<File>>,
-    len: usize,
+/// Builds a [`FileStream`].
+#[derive(Debug)]
+pub struct FileStreamBuilder {
+    handle: File,
+    offset: Option<u64>,
+    buffer_size: Option<usize>,
+    block_size: Option<u64>,
+}
+
+impl FileStreamBuilder {
+    pub fn new(handle: File) -> Self {
+        Self {
+            handle,
+            offset: None,
+            buffer_size: None,
+            block_size: None,
+        }
+    }
+
+    /// Offset into the file to start reading from.
+    pub fn offset(self, offset: u64) -> Self {
+        Self {
+            offset: Some(offset),
+            ..self
+        }
+    }
+
+    /// Amount of data to read from the file.
+    pub fn block_size(self, block_size: u64) -> Self {
+        Self {
+            block_size: Some(block_size),
+            ..self
+        }
+    }
+
+    /// Amount of data to buffer in memory during streaming reads.
+    pub fn buffer_size(self, buffer_size: usize) -> Self {
+        Self {
+            buffer_size: Some(buffer_size),
+            ..self
+        }
+    }
+
+    /// Build a [`FileStream`] from this builder.
+    pub async fn build(mut self) -> azure_core::Result<FileStream> {
+        let stream_size = self.handle.metadata().await?.len();
+        let buffer_size = self.buffer_size.unwrap_or(DEFAULT_BUFFER_SIZE);
+
+        let offset = if let Some(offset) = self.offset {
+            self.handle.seek(SeekFrom::Start(offset)).await?;
+            offset
+        } else {
+            0
+        };
+
+        let block_size = if let Some(block_size) = self.block_size {
+            block_size
+        } else {
+            stream_size - offset
+        };
+
+        let handle = Arc::new(Mutex::new(self.handle.take(block_size)));
+
+        Ok(FileStream {
+            handle,
+            stream_size,
+            block_size,
+            buffer_size,
+            offset,
+        })
+    }
+}
+
+#[derive(Debug, Clone)]
+#[pin_project::pin_project]
+pub struct FileStream {
+    #[pin]
+    handle: Arc<Mutex<Take<File>>>,
+    pub stream_size: u64,
+    pub block_size: u64,
     buffer_size: usize,
+    pub offset: u64,
 }
 
 impl FileStream {
-    async fn open(path: impl AsRef<Path>) -> std::io::Result<Self> {
-        let file = File::open(path).await?;
-        let len = file.metadata().await?.len() as usize;
-
-        Ok(Self {
-            handle: Arc::new(Mutex::new(file)),
-            len,
-            buffer_size: DEFAULT_BUFFER_SIZE,
-        })
+    async fn read(&mut self, slice: &mut [u8]) -> std::io::Result<usize> {
+        let mut handle = self.handle.clone().lock_owned().await;
+        handle.read(slice).await
     }
 
-    async fn read(&mut self, buf: &mut [u8]) -> std::io::Result<usize> {
-        let mut handle = self.handle.lock().await;
-        handle.read(buf).await
-    }
-}
-
-impl fmt::Debug for FileStream {
-    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        f.debug_struct("FileStream")
-            .field("len", &self.len)
-            .field("buffer_size", &self.buffer_size)
-            .finish()
+    /// Resets the remaining bytes in this stream window to the configured block size.
+    pub async fn next_block(&mut self) -> azure_core::Result<()> {
+        debug!("setting limit to {}", self.block_size);
+        let mut handle = self.handle.clone().lock_owned().await;
+        {
+            let inner = handle.get_mut();
+            self.offset = inner.stream_position().await?;
+        }
+        handle.set_limit(self.block_size);
+        Ok(())
     }
 }
 
 #[async_trait::async_trait]
 impl SeekableStream for FileStream {
     async fn reset(&mut self) -> azure_core::Result<()> {
-        let mut handle = self.handle.lock().await;
-        handle.seek(SeekFrom::Start(0)).await?;
+        debug!(
+            "resetting stream to offset {} and limit to {}",
+            self.offset, self.block_size
+        );
+        let mut handle = self.handle.clone().lock_owned().await;
+        {
+            let inner = handle.get_mut();
+            inner.seek(SeekFrom::Start(self.offset)).await?;
+        }
+        handle.set_limit(self.block_size);
         Ok(())
     }
 
     fn len(&self) -> usize {
-        self.len
+        debug!(
+            "stream len: {} - {} ... {}",
+            self.stream_size, self.offset, self.block_size
+        );
+        min(self.stream_size - self.offset, self.block_size) as usize
     }
 
     fn buffer_size(&self) -> usize {
@@ -84,11 +171,35 @@ impl SeekableStream for FileStream {
 
 impl futures::io::AsyncRead for FileStream {
     fn poll_read(
-        mut self: std::pin::Pin<&mut Self>,
+        mut self: Pin<&mut Self>,
         cx: &mut Context<'_>,
-        buf: &mut [u8],
+        slice: &mut [u8],
     ) -> Poll<std::io::Result<usize>> {
-        std::pin::pin!(self.read(buf)).poll(cx)
+        std::pin::pin!(self.read(slice)).poll(cx)
+    }
+}
+
+impl From<&FileStream> for Body {
+    fn from(stream: &FileStream) -> Self {
+        Body::SeekableStream(Box::new(stream.clone()))
+    }
+}
+
+impl From<FileStream> for Body {
+    fn from(stream: FileStream) -> Self {
+        Body::SeekableStream(Box::new(stream))
+    }
+}
+
+impl<T, F> From<&FileStream> for RequestContent<T, F> {
+    fn from(stream: &FileStream) -> Self {
+        Body::from(stream).into()
+    }
+}
+
+impl<T, F> From<FileStream> for RequestContent<T, F> {
+    fn from(stream: FileStream) -> Self {
+        Body::from(stream).into()
     }
 }
 
@@ -173,9 +284,14 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
 
     let blob_client = container_client.blob_client(&args.blob_name);
     let expected_content = tokio::fs::read(&args.file_path).await?;
-    let stream = FileStream::open(&args.file_path).await?;
+    let file = File::open(&args.file_path).await?;
+    let stream = FileStreamBuilder::new(file)
+        .buffer_size(512usize)
+        .build()
+        .await?;
+    let content: RequestContent<Bytes, NoFormat> = stream.into();
 
-    blob_client.upload_stream(stream, None).await?;
+    blob_client.upload(content, None).await?;
 
     let response = blob_client.download(None).await?;
     let (_, _, body) = response.deconstruct();
