b5a2816220b8cb72642b0b24de5a320f94abc3e5
vincenttran-msft <101599632+vincenttran-msft@users.noreply.github.com>
2026-04-15 17:33:08 -0700

    force threads to 1

---
+++ sdk/storage/azure_storage_blob/Test-Setup.ps1
@@ -0,0 +1,5 @@
+#!/usr/bin/env pwsh
+# Serialize test execution to rule out concurrency-related hangs in CI.
+# Remove this file once the root cause is identified.
+$env:RUST_TEST_THREADS = '1'
+Write-Host "RUST_TEST_THREADS set to $env:RUST_TEST_THREADS"
