74d2604ef47d333910c9b2b342cd60d20c4328ee
Jocelyn <jaschrep@microsoft.com>
2026-04-28 13:12:41 -0400

    roundtrip test

--- sdk/storage/azure_storage_blob_test/src/stress/data.rs
+++ sdk/storage/azure_storage_blob_test/src/stress/data.rs
@@ -4,6 +4,7 @@
 use std::cmp::min;
 
 use azure_core::stream::SeekableStream;
+use bytes::BufMut;
 use crc_fast::{CrcAlgorithm, Digest};
 use rand::random;
 
@@ -27,4 +28,17 @@ pub fn random_data_stream_with_checksum(
         GeneratedStream::from_iter(src_bytes.into_iter(), len, None),
         digest.finalize(),
     )
+}
+
+pub fn random_data_memory_with_checksum(len: usize, algorithm: CrcAlgorithm) -> (Vec<u8>, u64) {
+    let buf: [u8; 9999] = random();
+    let mut data = Vec::with_capacity(len);
+
+    for i in (0..len).step_by(buf.len()) {
+        data.put(&buf[..min(buf.len(), i - len)]);
+    }
+
+    let mut digest = Digest::new(algorithm);
+    digest.update(&data);
+    (data, digest.finalize())
 }
