a98919388cfb8fc0d1e6e81b421d8c61fc5fd061
Fabian Meiswinkel <fabianm@microsoft.com>
2026-05-04 18:50:50 +0000

    Fixing build issues

--- sdk/cosmos/azure_data_cosmos_driver/src/models/cosmos_operation.rs
+++ sdk/cosmos/azure_data_cosmos_driver/src/models/cosmos_operation.rs
@@ -582,6 +582,7 @@ impl CosmosOperation {
     /// - `x-ms-cosmos-supported-query-features: <feature-list>`
     /// - `Content-Type: application/query+json`
     /// - `x-ms-documentdb-isquery: True`
+    #[allow(dead_code)]
     pub(crate) fn query_plan(container: ContainerReference) -> Self {
         let resource_ref: CosmosResourceReference = CosmosResourceReference::from(container)
             .with_resource_type(ResourceType::Document)
