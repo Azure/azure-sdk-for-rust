65f2b3b6caceb27214572c110f7f55249bb85128
Jocelyn <jaschrep@microsoft.com>
2026-05-12 12:03:36 -0400

    stress framework initial impl

---
+++ sdk/storage/azure_storage_blob_test/src/stress/framework_tests.rs
@@ -0,0 +1,194 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+//! Tests for functioning of the stress test runner.
+//!
+//! These tests cover various scenarios for running the `StressRunner` with different options and measurements.
+//!
+
+use async_trait::async_trait;
+use clap::Args;
+
+use super::*;
+
+#[derive(Debug, Subcommand)]
+enum MockStressTestFactory {
+    TestOne(MockArgsOne),
+    TestTwo(MockArgsTwo),
+}
+
+impl StressTestFactory for MockStressTestFactory {
+    fn get_test(&self) -> Result<Box<dyn StressTest>> {
+        Ok(match self {
+            Self::TestOne(_args) => Box::new(MockStressTestOne {}),
+            Self::TestTwo(_args) => Box::new(MockStressTestTwo {}),
+        })
+    }
+}
+
+struct MockStressTestOne {}
+#[derive(Args, Debug)]
+struct MockArgsOne {}
+#[async_trait]
+impl StressTest for MockStressTestOne {
+    async fn global_setup(&self) -> Result<()> {
+        Ok(())
+    }
+    fn get_operation(&self) -> Result<Box<dyn StressTestOperation>> {
+        Ok(Box::new(MockOperationOne {}))
+    }
+    async fn global_cleanup(&self) -> Result<()> {
+        Ok(())
+    }
+}
+struct MockOperationOne {}
+#[async_trait]
+impl StressTestOperation for MockOperationOne {
+    async fn run(&self, _result_sender: UnboundedSender<StressRunOutput>) {
+        // todo!()
+    }
+}
+
+struct MockStressTestTwo {}
+#[derive(Args, Debug)]
+struct MockArgsTwo {
+    #[arg(short, long)]
+    a_mandatory: usize,
+    #[arg(short, long, default_value_t = 1)]
+    b_has_default: usize,
+    #[arg(short, long)]
+    c_optional: Option<usize>,
+    #[arg(short, long)]
+    d_flag: bool,
+}
+#[async_trait]
+impl StressTest for MockStressTestTwo {
+    async fn global_setup(&self) -> Result<()> {
+        Ok(())
+    }
+    fn get_operation(&self) -> Result<Box<dyn StressTestOperation>> {
+        Ok(Box::new(MockOperationTwo {}))
+    }
+    async fn global_cleanup(&self) -> Result<()> {
+        Ok(())
+    }
+}
+struct MockOperationTwo {}
+#[async_trait]
+impl StressTestOperation for MockOperationTwo {
+    async fn run(&self, _result_sender: UnboundedSender<StressRunOutput>) {
+        // todo!()
+    }
+}
+
+#[tokio::test]
+async fn test_parse_minimum_runner_args() {
+    let args = vec!["stress_test", "--output", "", "test-one"];
+    let result =
+        StressRunner::<MockStressTestFactory>::from_args(env!("CARGO_MANIFEST_DIR"), file!(), args);
+
+    println!("Result: {:?}", result);
+    assert!(result.is_ok());
+}
+
+#[tokio::test]
+async fn test_parse_missing_required_runner_arg() {
+    let args = vec!["stress_test", "test-one"];
+    let result =
+        StressRunner::<MockStressTestFactory>::from_args(env!("CARGO_MANIFEST_DIR"), file!(), args);
+
+    println!("Result: {:?}", result);
+    assert!(result.is_err());
+}
+
+#[tokio::test]
+async fn test_parse_unrecognized_runner_arg() {
+    let args = vec!["stress_test", "-o", "", "--bad-arg", "test-one"];
+    let result =
+        StressRunner::<MockStressTestFactory>::from_args(env!("CARGO_MANIFEST_DIR"), file!(), args);
+
+    println!("Result: {:?}", result);
+    assert!(result.is_err());
+}
+
+#[tokio::test]
+async fn test_parse_all_runner_args() {
+    let args = vec![
+        "stress_test",
+        "--parallel",
+        "5",
+        "--duration",
+        "10",
+        "--timeout",
+        "60",
+        "--output",
+        "",
+        "test-one",
+    ];
+    let result =
+        StressRunner::<MockStressTestFactory>::from_args(env!("CARGO_MANIFEST_DIR"), file!(), args);
+
+    println!("Result: {:?}", result);
+    assert!(result.is_ok());
+}
+
+#[tokio::test]
+async fn test_parse_missing_subcommand() {
+    let args = vec!["stress_test", "--output", ""];
+    let result =
+        StressRunner::<MockStressTestFactory>::from_args(env!("CARGO_MANIFEST_DIR"), file!(), args);
+
+    println!("Result: {:?}", result);
+    assert!(result.is_err());
+}
+
+#[tokio::test]
+async fn test_parse_subcommand_minimum_args() {
+    let args = vec!["stress_test", "--output", "", "test-two", "-a", "5"];
+    let result =
+        StressRunner::<MockStressTestFactory>::from_args(env!("CARGO_MANIFEST_DIR"), file!(), args);
+
+    println!("Result: {:?}", result);
+    assert!(result.is_ok());
+}
+
+#[tokio::test]
+async fn test_parse_subcommand_all_args() {
+    let args = vec![
+        "stress_test",
+        "--output",
+        "",
+        "test-two",
+        "-a",
+        "5",
+        "-b",
+        "10",
+        "-c",
+        "15",
+        "-d",
+    ];
+    let result =
+        StressRunner::<MockStressTestFactory>::from_args(env!("CARGO_MANIFEST_DIR"), file!(), args);
+
+    println!("Result: {:?}", result);
+    assert!(result.is_ok());
+}
+
+#[tokio::test]
+async fn test_parse_subcommand_unrecognized_arg() {
+    let args = vec![
+        "stress_test",
+        "--output",
+        "",
+        "test-two",
+        "-a",
+        "5",
+        "--bad-arg",
+        "foo",
+    ];
+    let result =
+        StressRunner::<MockStressTestFactory>::from_args(env!("CARGO_MANIFEST_DIR"), file!(), args);
+
+    println!("Result: {:?}", result);
+    assert!(result.is_err());
+}
---
+++ sdk/storage/azure_storage_blob_test/src/stress/mod.rs
@@ -0,0 +1,176 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+use azure_core::{
+    async_runtime::get_async_runtime,
+    error::{ErrorKind, ResultExt},
+    Error,
+};
+use clap::{Parser, Subcommand};
+use futures::{
+    channel::mpsc::{self, UnboundedSender},
+    future,
+};
+use serde::Serialize;
+use std::mem;
+
+pub type Result<T> = std::result::Result<T, Error>;
+
+/// A [Subcommand] that is capable of instancing
+pub trait StressTestFactory {
+    fn get_test(&self) -> Result<Box<dyn StressTest>>;
+}
+
+#[async_trait::async_trait]
+pub trait StressTest: Send + Sync {
+    async fn global_setup(&self) -> Result<()>;
+    fn get_operation(&self) -> Result<Box<dyn StressTestOperation>>;
+    async fn global_cleanup(&self) -> Result<()>;
+}
+
+#[async_trait::async_trait]
+pub trait StressTestOperation: Send + Sync {
+    async fn run(&self, result_sender: UnboundedSender<StressRunOutput>);
+}
+
+#[derive(Debug, Clone, Parser)]
+struct StressRunnerOptions<T: StressTestFactory + Subcommand> {
+    /// Parallel operations to run.
+    #[arg(long, default_value_t = 1)]
+    parallel: usize,
+
+    /// Run duration in seconds.
+    #[arg(long, value_name = "SECONDS", default_value_t = 60)]
+    duration: u64,
+
+    /// Optional timeout in seconds for individual operations.
+    #[arg(long, value_name = "SECONDS")]
+    timeout: Option<u64>,
+
+    /// Results output destination.
+    #[arg(short = 'o', long = "output", value_name = "FILE")]
+    results_file: String,
+
+    #[command(subcommand)]
+    command: T,
+}
+
+#[derive(Debug, Clone, Default, Serialize)]
+struct StressRunCounts {
+    pub total_loops: usize,
+    pub loops_success: usize,
+    pub loops_graceful_error: usize,
+    pub loops_timeout: usize,
+    pub loops_panic: usize,
+    pub loops_data_corruption: usize,
+}
+
+pub enum StressRunOutput {
+    Success,
+    GracefulError(Error),
+    Timeout,
+    Panic(String),
+    DataCorruption,
+}
+
+/// Context information required by performance tests.
+#[derive(Debug, Clone)]
+#[allow(dead_code)]
+pub struct StressRunner<T: StressTestFactory + Subcommand> {
+    options: StressRunnerOptions<T>,
+    package_dir: &'static str,
+    module_name: &'static str,
+}
+
+impl<T: StressTestFactory + Subcommand> StressRunner<T> {
+    /// Construct a stress test runner using one of the provided `tests` based on [`env::args_os`].
+    ///
+    ///
+    /// # Arguments
+    ///
+    /// * package_dir - The directory containing the package with the tests. Typically `env!("CARGO_PACKAGE_DIR")`
+    /// * module_name - The name of the module containing the test, typically `file!()`
+    /// * tests - A set of test definitions.
+    ///
+    pub fn new(package_dir: &'static str, module_name: &'static str) -> Result<Self> {
+        let options = StressRunnerOptions::<T>::try_parse()
+            .with_context(ErrorKind::Other, "Failed to parse command line arguments.")?;
+        Ok(Self {
+            options,
+            package_dir,
+            module_name,
+        })
+    }
+
+    /// Construct a stress test runner using one of the provided `tests` based on provided `args`.
+    pub fn from_args(
+        package_dir: &'static str,
+        module_name: &'static str,
+        args: Vec<&str>,
+    ) -> azure_core::Result<Self> {
+        let options = StressRunnerOptions::<T>::try_parse_from(args)
+            .with_context(ErrorKind::Other, "Failed to parse command line arguments.")?;
+        Ok(Self {
+            options,
+            package_dir,
+            module_name,
+        })
+    }
+
+    pub async fn run(&self) -> azure_core::Result<()> {
+        let stress_test = self.options.command.get_test()?;
+
+        // println!("Test Configuration: {:#}", self.options);
+
+        println!("========== Starting global setup ==========");
+        stress_test.global_setup().await?;
+
+        let mut totals = StressRunCounts::default();
+        let mut join_handles = Vec::with_capacity(self.options.parallel);
+        let (tx, mut rx) = mpsc::unbounded();
+        for iteration in 1.. {
+            println!("========== Starting test run {} ==========", iteration);
+
+            let operation = stress_test.get_operation()?;
+            let tx_clone = tx.clone();
+            join_handles.push(get_async_runtime().spawn(Box::pin(async move {
+                operation.run(tx_clone).await;
+            })));
+
+            // block until free parallel slot
+            while join_handles.len() >= self.options.parallel {
+                let join_result;
+                (join_result, _, join_handles) =
+                    future::select_all(mem::take(&mut join_handles)).await;
+                if let Err(_join_error) = join_result {
+                    todo!("Handle error joining task")
+                }
+            }
+
+            // non-blocking process run result(s)
+            while let Ok(msg) = rx.try_recv() {
+                totals.total_loops += 1;
+                match msg {
+                    StressRunOutput::Success => totals.loops_success += 1,
+                    StressRunOutput::GracefulError(_error) => totals.loops_graceful_error += 1,
+                    StressRunOutput::Timeout => totals.loops_timeout += 1,
+                    StressRunOutput::Panic(_panic_msg) => totals.loops_panic += 1,
+                    StressRunOutput::DataCorruption => totals.loops_data_corruption += 1,
+                }
+                println!(
+                    "{}",
+                    serde_json::to_string_pretty(&totals).with_context(
+                        ErrorKind::DataConversion,
+                        "Failed to serialize test results to JSON.",
+                    )?
+                );
+            }
+        }
+        println!("========== Starting test cleanup ==========");
+        stress_test.global_cleanup().await?;
+        Ok(())
+    }
+}
+
+#[cfg(test)]
+mod framework_tests;
