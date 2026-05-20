3e7f248bbf5a147ce934180efcd3aa692fd28578
Heath Stewart <heaths@microsoft.com>
2026-05-19 18:03:04 -0700

    Use GA releases in root sample projects
    Resolves #4397

--- samples/list_blobs_native_tls/src/main.rs
+++ samples/list_blobs_native_tls/src/main.rs
@@ -1,6 +1,7 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 
+use azure_core::http::Url;
 use azure_identity::DeveloperToolsCredential;
 use azure_storage_blob::BlobContainerClient;
 use clap::Parser;
@@ -11,17 +12,20 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let args = Args::parse();
 
     let credential = DeveloperToolsCredential::new(None)?;
-    let endpoint = format!("https://{}.blob.core.windows.net/", args.account_name);
+    let endpoint: Url = format!(
+        "https://{}.blob.core.windows.net/{}",
+        args.account_name, args.container_name
+    )
+    .parse()?;
 
     // The reqwest/native-tls feature automatically configures native-tls as the TLS provider.
-    let container_client =
-        BlobContainerClient::new(&endpoint, &args.container_name, Some(credential), None)?;
+    let container_client = BlobContainerClient::new(endpoint, Some(credential), None)?;
 
     // Iterate through all pages of blobs in the container.
     let mut pager = container_client.list_blobs(None)?.into_pages();
     while let Some(page) = pager.try_next().await? {
         let response = page.into_model()?;
-        for blob in &response.segment.blob_items {
+        for blob in &response.blob_items {
             if let Some(name) = &blob.name {
                 println!("{name}");
             }
