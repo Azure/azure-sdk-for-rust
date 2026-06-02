862fdf14e134edb5a3d7978993d021041b9b3927
Simon Moreno <30335873+simorenoh@users.noreply.github.com>
2026-06-02 13:12:36 -0400

    add sdk-level multi-region tests for validation

---
+++ sdk/cosmos/azure_data_cosmos/tests/multi_region_tests/mod.rs
@@ -0,0 +1,6 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+mod cosmos_multi_region_fallback;
+
+#[path = "../framework/mod.rs"]
+mod framework;
